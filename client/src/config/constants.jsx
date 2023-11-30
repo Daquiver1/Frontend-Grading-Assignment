4cedc4debfe299e1c9909acef5d35b4d0f79f25e
CodeSageJ Jew <jklarbi_danquah@st.ug.edu.gh>
2023-11-30 18:57:14 +0000

    hero section done

---
+++ client/src/config/constants.jsx
@@ -0,0 +1,11 @@
+//auth
+export const SIGNIN = "/sign-in";
+
+// for main pages
+export const HOME = "/";
+export const MAIN_LAYOUT = "/";
+export const ABOUT = "/about";
+export const OUR_TEAM = "/our-team";
+export const GROUPS = "/groups";
+export const DOCUMENTS = '/documents'
+export const CONTACT = '/contact'
--- client/src/config/router-config.jsx
+++ client/src/config/router-config.jsx
@@ -1,13 +1,22 @@
 import {Route, createBrowserRouter,
     createRoutesFromElements,} from "react-router-dom"
-import SignIn from "../pages.jsx/auth/signin"
 
+//pages
+import SignIn from "../pages/auth/signin"
+import HomeLayout from "../pages/home/homeLayout"
+
+//paths
+import { HOME,SIGNIN } from "./constants"
+import NotFound from "../pages/404"
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <>
-          <Route path="/sign-in" element={<SignIn />}/>
-          <Route path="*" element={<SignIn />}/>
+          <Route path={SIGNIN} element={<SignIn />}/>
+          <Route path={HOME} element={<HomeLayout />}>
+             
+          </Route>
+          <Route path="*" element={<NotFound />}/>
         </>
     )
 )
