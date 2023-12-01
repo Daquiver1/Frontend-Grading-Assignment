5c1a8013d69220adddcf7bd1962060be07a96c46
heisqueenson9 <150591005+heisqueenson9@users.noreply.github.com>
2023-12-01 06:57:49 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+
+function App() {
+  return (
+    <>
+    <div className="fullPage">
+      <div className="sideNav">
+        <div className="webName">Student Report</div>
+      </div>
+      <div className="pageOutput">
+
+      </div>
+    </div>
+    </>
+  );
+}
+
+export default App;
