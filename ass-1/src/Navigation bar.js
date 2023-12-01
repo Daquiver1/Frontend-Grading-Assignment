d18953f3833a9b59ec93d06704202aabb2ee13e3
Ed-Hans <ekoffi_hans@st.ug.edu.gh>
2023-12-01 20:57:48 +0000

    Installed boostrap

---
+++ ass-1/src/Navigation bar.js
@@ -0,0 +1,17 @@
+const NavBar = () => {
+  return (
+    <div className="Navigation">
+      <nav>
+        <a href="HomePage">Home</a>
+        <a href="LoginPage">Login</a>
+        <a href="DashboardPage">Dashboard</a>
+        <a href="GradeReportPage">Report Grade</a>
+        <a href="MissingGradeFormPage">Missing Grade Form Page</a>
+        <a href="InstructorContactPage">Contact Us</a>
+        <a href="Help and Support Page">Help </a>
+      </nav>
+    </div>
+  );
+};
+
+export default NavBar;
