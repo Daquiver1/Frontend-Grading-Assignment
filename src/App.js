1b0d5a689fc9d71882f2b7a179b1ad102ab3bdcd
steveAzo -Git <stephenazongo77@gmail.com>
2023-12-02 03:38:29 +0000

    I coded the landing page and the login page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LandingPage from './components/LandingPage';
+import LoginPage from './components/LoginPage';
+import DashboardPage from './components/DashboardPage';
+import GradeReportPage from './components/GradeReportPage';
+import MissingGradeFormPage from './components/MissingGradeFormPage';
+import InstructorContactPage from './components/InstructorContactPage';
+import HelpAndSupportPage from './components/HelpAndSupportPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<LandingPage/>} />
+
+        <Route path="/login" element={<LoginPage/>} />
+
+        <Route path="/DashboardPage" element={<DashboardPage/>} />
+
+        <Route path="/grade-report" element={<GradeReportPage/>} />
+
+        <Route path="/missing-grade-form" element={<MissingGradeFormPage/>} />
+
+        <Route path="/instructor-contact" element={<InstructorContactPage/>} />
+
+        <Route path="/help-and-support" element={<HelpAndSupportPage/>} />
+      </Routes>
+        
+    </Router>
   );
 }
 
---
+++ src/components/LandingPage.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './LandingPage.css';
+
+
+const LandingPage = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <div className='container'>
+        <h1>Welcome to the Missing Grade Reporting System</h1>
+        <p>
+          Empowering students to track and report missing grades for a seamless academic experience.
+        </p>
+
+        <Link to="/login">
+          <button>Get Started</button>
+        </Link>
+      </div>
+
+      <footer>
+        <p>&copy; 2023 Missing Grade Reporting System</p>
+      </footer>
+    </div>
+  );
+};
+
+export default LandingPage;
