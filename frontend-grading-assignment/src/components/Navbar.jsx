ae4b4ef29203522b3f1328a9b45dfb097b376efc
Richmond-Andoh <richmondandoh00@gmail.com>
2023-12-02 10:01:51 +0000

    Worked on Landing Page

--- frontend-grading-assignment/src/components/Navbar.jsx
+++ frontend-grading-assignment/src/components/Navbar.jsx
@@ -1,40 +1,57 @@
+import "./CustomNavbar.css";
+import React, { useState, useEffect } from 'react';
+
 const Navbar = () => {
+    const [scrollY, setScrollY] = useState(0);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setScrollY(window.scrollY)
+        }
+
+        //Attach the event listener when the component is mounted
+        window.addEventListener("scroll", handleScroll);
+
+        //remove the event listener when the compnent is unmounted
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, [])
+    
     return (
 
         <div>
-            <nav class="navbar navbar-expand-lg bg-dark fixed-top">
+            <nav className={ scrollY >= 10 ? "navbar navbar-expand-lg fixed-top scrolled" : "navbar navbar-expand-lg fixed-top"}>
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="#">
-                      <i class="bi bi-mortarboard-fill text-white fs-1"></i>
+                    <a className="navbar-brand" href="#">
+                      <i className="bi bi-mortarboard-fill text-primary fs-1"></i>
                     </a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                            <a class="nav-link active m-2 text-white" aria-current="page" href="#">Home</a>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                            <a className="nav-link active m-2 text-white" aria-current="page" href="#">Home</a>
                             </li>
-                            <li class="nav-item">
-                            <a class="nav-link m-2 text-white" href="#">Login</a>
+                            <li className="nav-item">
+                            <a className="nav-link m-2 text-white" href="#">Login</a>
                             </li>
                             
-                            <li class="nav-item">
-                               <a class="nav-link m-2 text-white" aria-disabled="true">Dashboard</a>
+                            <li className="nav-item">
+                               <a className="nav-link m-2 text-white" href="#" aria-disabled="true">Dashboard</a>
                             </li>
-                            <li class="nav-item">
-                               <a class="nav-link m-2 text-white" aria-disabled="true">Report Grade</a>
+                            <li className="nav-item">
+                               <a className="nav-link m-2 text-white" href="#" aria-disabled="true">Report Grade</a>
                             </li>
-                            <li class="nav-item">
-                              <a class="nav-link m-2 text-white" aria-disabled="true">Missing Grade</a>
-                            </li>
-
-                            <li class="nav-item">
-                               <a class="nav-link m-2 text-white" aria-disabled="true">Contact</a>
+                            <li className="nav-item">
+                              <a className="nav-link m-2 text-white" href="#" aria-disabled="true">Missing Grade</a>
                             </li>
 
-                            <li class="nav-item">
-                              <a class="nav-link m-2 text-white" aria-disabled="true">Help & Support</a>
+                            <li className="nav-item">
+                               <a className="nav-link m-2 text-white" href="#" aria-disabled="true">Contact</a>
+                            </li>
+
+                            <li className="nav-item">
+                              <a className="nav-link m-2 text-white" href="#" aria-disabled="true">Help & Support</a>
                             </li>
                         </ul>
                         {/* <form class="d-flex" role="search">
--- frontend-grading-assignment/src/routes/Home.jsx
+++ frontend-grading-assignment/src/routes/Home.jsx
@@ -1,9 +1,50 @@
+import "./CustomHome.css"
 import Navbar from "../components/Navbar"
+import Footer from "../components/Footer";
 
 const Home = () => {
     return (
         <div>
             <Navbar />
+            <div className="container-fluid pt-5 hero-session">
+                <div className="container hero-content text-white">
+                    <h1>
+                        Student Grading System
+                    </h1>
+                    <button type="button" class="btn btn-outline-light mt-2 rounded">
+                        Read More
+                    </button>
+
+                </div>
+            </div>
+
+            <section>
+                <div className="container mt-5">
+                    <div className="row">
+                        <div className="col col-12 col-md-6 col-sm-12 col-xs-12">
+                        <div className="content mt-5 p-5">
+                            <h2 className="mb-3 text-primary">Our Mission</h2>
+                            <p>
+                                To empower students by providing a transparent and efficient
+                                platform for tracking and reporting grades in real-time. With 
+                                a user-friendly interface, the system facilitates seamless 
+                                interactions between students and instructors, enabling the 
+                                submission of missing grades and fostering open communication channels. The mission encompasses
+                                ensuring data security, promoting educational equity, and offering academic support services. 
+                            </p>
+                            <button type="button" className="btn btn-primary text-white btn-sm py-2 px-3 mt-2">Read More</button>
+                        </div>
+                        </div>
+                        <div className="col col-12 col-md-6 col-sm-12 col-xs-12">
+                            <div className="img mt-5 p-3">
+                            <img src="https://www.mystudentplan.ca/_images/student-plan-landing.png" className="img-fluid" alt="student image" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+           <Footer />
         </div>
     )
 }
