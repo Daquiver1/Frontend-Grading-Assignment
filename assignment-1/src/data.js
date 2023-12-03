e162057bda28ef656bc8d46d3591e3262c3ef152
kodditor <kobbyowusudarko@gmail.com>
2023-12-03 13:19:25 +0000

    'Database'

---
+++ assignment-1/src/data.js
@@ -0,0 +1,129 @@
+
+export const reasons = [
+    {
+        title: 'Centralized Grade Tracking',
+        body: 'Say goodbye to the frustration of missing grades! GradeRepo consolidates all your academic achievements in one secure place. Now, you can effortlessly keep track of every assignment, quiz, and project, ensuring you never miss a grade again'
+    },
+    {
+        title: 'Uncover Hidden Achievements',
+        body: 'Ever feel like your true academic potential isn\'t fully represented in your official records? GradeRepo allows you to report grades that may have been missed by your institution, giving you the recognition you deserve for your hard work.'
+    },
+    {
+        title: 'Seamless Integration',
+        body: 'GradeRepo seamlessly integrates with your existing academic profiles and systems, providing a user-friendly interface that makes it easy to update and manage your grades. No more struggling with outdated and complicated platforms!'
+    },
+    {
+        title: 'Personalized Reports',
+        body: 'Generate personalized reports showcasing your academic journey. Whether you need it for scholarship applications, job interviews, or personal satisfaction, GradeRepo ensures you have a comprehensive and accurate representation of your achievements.'
+    }  
+]
+
+export const grades = [
+    {
+        id: 'DCIT101',
+        title: 'Introduction to Computer Science',
+        semester: 1,
+        accYear: '2022/23',
+        grade: 'A'
+    },
+    {
+        id: 'DCIT102',
+        title: 'Computer Hardware Fund. and Circuits',
+        semester: 2,
+        accYear: '2022/23',
+        grade: 'A'
+    },
+    {
+        id: 'DCIT103',
+        title: 'Office Productivity Tools',
+        semester: 1,
+        accYear: '2022/23',
+        grade: 'B+'
+    },
+    {
+        id: 'DCIT104',
+        title: 'Programming Fundamentals',
+        semester: 2,
+        accYear: '2022/23',
+        grade: 'B'
+    },
+    {
+        id: 'MATH121',
+        title: 'Algebra and Trigonometry',
+        semester: 1,
+        accYear: '2022/23',
+        grade: 'A'
+    },
+    {
+        id: 'MATH123',
+        title: 'Vectors and Geometry',
+        semester: 1,
+        accYear: '2022/23',
+        grade: 'B'
+    },
+    {
+        id: 'DCIT201',
+        title: 'Programming I',
+        semester: 1,
+        accYear: '2023/24',
+        grade: ''
+    },
+    {
+        id: 'DCIT203',
+        title: 'Digital and Logic Systems Design',
+        semester: 1,
+        accYear: '2023/24',
+        grade: ''
+    },
+    {
+        id: 'DCIT205',
+        title: 'Multi Media and Web Design',
+        semester: 1,
+        accYear: '2023/24',
+        grade: 'A'
+    }   
+]
+
+export const instructors = [
+    {
+        name: 'Michael Agbo Tettey Soli',
+        title: 'Lecturer',
+        courseID: 'DCIT205',
+        email: 'mats@gmail.com'
+    },
+    {
+        name: 'Christian Abrokwa',
+        title: 'Teaching Assistant',
+        courseID: 'DCIT205',
+        email: 'ca@gmail.com'
+    },
+    {
+        name: 'Phillipa Bennett-Eghan',
+        title: 'Teaching Assistant',
+        courseID: 'DCIT205',
+        email: 'pba@gmail.com'
+    },
+]
+
+export const QAs = [
+    {
+        question: 'What is GradeRepo, and how does it work?',
+        answer: 'GradeRepo is a centralized system designed to help students track and report grades not recorded in their academic profiles. It works by allowing lecturers to input and manage grades in one secure place, providing a comprehensive view of their academic achievements.'
+    },
+    {
+        question: 'Is GradeRepo compatible with my existing academic profiles?',
+        answer: 'Absolutely! GradeRepo seamlessly integrates with your current academic systems, making it easy to update and manage your grades without any hassle.'
+    },
+    {
+        question: 'Can I report grades that were missed or not officially recorded?',
+        answer: 'Yes, one of GradeRepo\'s key features is the ability to report grades that may have been missed by your institution. It ensures that your academic profile truly reflects your hard work and accomplishments'
+    },
+    {
+        question: 'How secure is my academic information on GradeRepo?',
+        answer: 'Security is our top priority. GradeRepo employs state-of-the-art security measures to safeguard your data, ensuring the privacy of your academic information.'
+    },
+    {
+        question: 'Is GradeRepo only for students, or can educators use it too?',
+        answer: 'GradeRepo is primarily designed for students, but educators can also benefit from its features. They can use it to provide students with a centralized platform to track and manage their grades.'
+    }
+]
