2e9270b738ece1dcc0c3944f98dd9bfedfb75229
Michael Perry Tettey <michaelperryt97@gmail.com>
2023-12-03 22:44:45 +0000

    Created Header

--- dcit-205-assignment/src/components/header.js
+++ dcit-205-assignment/src/components/header.js
@@ -1,16 +1,81 @@
-import logo from '../logo.svg'
+import logo from './logo.svg'
 export const Header = () => {
     return (
-      <header>
-        <nav className="flex flex-row justify-between p-3 items-center fixed top-0 left-0 right-0 bg-red-700">
-          <div className="">
-            <img src={logo} alt={"this is a logo"} className="w-3/12" />
+      <header className="fixed top-0 left-0 right-0 bg-gray-100">
+        <nav
+          class=" mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+          aria-label="Global"
+        >
+          <div class="flex lg:flex-1">
+            <a href="/about" rel="noopener" class="-m-1.5 p-1.5">
+              <span class="sr-only">UG | Course Complaint</span>
+              <img class="h-8 w-auto" src={logo} alt="" />
+            </a>
           </div>
-          <button className="py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opac ml-4"
-          >
-            Hello
-          </button>
-          <button className="py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opac ml-4">Show</button>
+          <div class="flex lg:hidden">
+            <button
+              type="button"
+              class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            >
+              <span class="sr-only">Open main menu</span>
+              <svg
+                class="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                />
+              </svg>
+            </button>
+          </div>
+          <div class="hidden lg:flex lg:gap-x-12">
+            <div class="relative">
+              <button
+                type="button"
+                class="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900"
+                aria-expanded="false"
+              >
+                Home
+              </button>
+            </div>
+
+            <a
+              href="/about"
+              rel="noopener"
+              class="text-sm font-semibold leading-6 text-gray-900"
+            >
+              About
+            </a>
+            <a
+              href="/about"
+              rel="noopener"
+              class="text-sm font-semibold leading-6 text-gray-900"
+            >
+              Course Check
+            </a>
+            <a
+              href="/about"
+              rel="noopener"
+              class="text-sm font-semibold leading-6 text-gray-900"
+            >
+              Contact
+            </a>
+          </div>
+          <div class="hidden lg:flex lg:flex-1 lg:justify-end">
+            <a
+              href="/about"
+              rel="noopener"
+              class="text-sm font-semibold leading-6 text-gray-900"
+            >
+              Log in <span aria-hidden="true">&rarr;</span>
+            </a>
+          </div>
         </nav>
       </header>
     );
