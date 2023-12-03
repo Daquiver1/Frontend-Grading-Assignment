b479b459b8293f4064716acb6312df30cae79c31
Michael Perry Tettey <michaelperryt97@gmail.com>
2023-12-03 22:44:13 +0000

    Created footer

--- dcit-205-assignment/src/components/footer.js
+++ dcit-205-assignment/src/components/footer.js
@@ -1,17 +1,36 @@
 export const Footer = () => {
-    return(
-        <footer classNAme="footer w-auto sticky bottom-0 left-0 right-0">
-            <div classNAme="flex flex-col ">
-                <input id="submit" placeholder="Write your email here" className="p-1 border-emerald-100 border-solid border-4 focus:border-red-100
-                focus:outline-none hover:cursor-pointer"></input>
-                <label for="submit">Submit</label>
-                
-            </div>
-            <div className="bg-red-400 inline-flex justify-center m-auto gap-2">
-                <p>Time</p>
-                <p>Date</p>
-            </div>
-        </footer>
+    return (
+      <footer className="sticky bottom-0 left-0 z-20 w-full p-4 bg-white border-none border-white shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-300 dark:border-gray-600">
+        <span className="text-sm text-gray-700 sm:text-center dark:text-gray-700">
+          © 2023{" "}
+          <a href="https://flowbite.com/" className="hover:underline">
+            CourseSues.io™
+          </a>
+          . All Rights Reserved.
+        </span>
+        <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-700 sm:mt-0">
+          <li>
+            <a href="#" className="hover:underline me-4 md:me-6">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#" className="hover:underline me-4 md:me-6">
+              Course Check
+            </a>
+          </li>
+          <li>
+            <a href="#" className="hover:underline me-4 md:me-6">
+              Apply
+            </a>
+          </li>
+          <li>
+            <a href="#" className="hover:underline">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </footer>
     );
 };
 export default Footer;
