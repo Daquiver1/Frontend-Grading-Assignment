12283a0705a1f0cabcd51cfface960f6f3d4ead7
oswaldamoah <oswaldamoah@gmail.com>
2023-12-04 23:25:15 +0000

    GradeReportPage jsx file

---
+++ src/components/GradeReportPage.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Header2 from './Header2';
+import Sidebar from './Sidebar';
+import Footer from './Footer';
+
+
+const GradeReportPage = () => {
+  return (
+    <div>
+      <Header2 />
+      <Sidebar />
+      {/* Your Page Content Goes Here */}
+      <Footer />
+    </div>
+  );
+};
+
+export default GradeReportPage;
+
+
