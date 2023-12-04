c7792384f7d18d872bb45f9935a6efd5adae4971
oswaldamoah <oswaldamoah@gmail.com>
2023-12-04 23:24:22 +0000

    DashboardPage jsx file

---
+++ src/components/DashboardPage.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header2 from './Header2';
+import Sidebar from './Sidebar';
+import Footer from './Footer';
+
+const DashboardPage = () => {
+  return (
+    <div>
+      <Header2 />
+      <Sidebar />
+      {/* Your Page Content Goes Here */}
+      <Footer />
+    </div>
+  );
+};
+
+export default DashboardPage;
+
+
