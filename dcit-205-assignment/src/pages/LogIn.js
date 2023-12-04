5f557b98de3e2ee25161a4978eef6525f97a7919
Michael Perry Tettey <michaelperryt97@gmail.com>
2023-12-04 15:12:45 +0000

    Created Login Page

--- dcit-205-assignment/src/pages/LogIn.js
+++ dcit-205-assignment/src/pages/LogIn.js
@@ -1,3 +1,4 @@
+import logo from "../components/logo.svg"
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { Link } from "react-router-dom";
@@ -5,27 +6,66 @@ export default function LogIn() {
   return (
     <>
       <Header />
-      <main className="mx-auto flex min-h-screen w-full items-center justify-center  text-white bg-white">
+      <main className="mx-auto flex min-h-screen w-full items-center justify-center  text-white bg-white pt-10">
         {/* component */}
         <section className="flex w-[30rem] flex-col space-y-10">
-          <div className="text-center text-4xl font-medium text-black">Log In</div>
-          <div className="w-full transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-indigo-500">
+          <div className="flex flex-col justify-center items-center">
+            <div className="w-1/6">
+              <img className="w-full" src={logo} alt="" />
+            </div>
+            <div className="text-4xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+              Log In
+            </div>
+          </div>
+
+          <div>
+            <label
+              htmlFor="username"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Username
+            </label>
             <input
               type="text"
-              placeholder="Email or Username"
-              className="w-full border-none bg-transparent outline-none placeholder:bold focus:outline-none text-black"
+              id="username"
+              name="username"
+              className="mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300 text-black"
             />
           </div>
-          <div className="w-full transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-indigo-500">
+          <div>
+            <label
+              htmlFor="text"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Student Id
+            </label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              className="mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300 text-black"
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Password
+            </label>
             <input
               type="password"
-              placeholder="Password"
-              className="w-full border-none bg-transparent outline-none placeholder:bold focus:outline-none text-black"
+              id="username"
+              name="username"
+              className="mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300 text-black"
             />
           </div>
-          <button className="transform rounded-sm bg-indigo-600 py-2 font-bold duration-300 hover:bg-indigo-400">
-            LOG IN
-          </button>
+          <Link
+            to="/dashboard"
+            className="transform rounded-lg bg-indigo-600 py-3 font-bold duration-300 hover:bg-indigo-400 text-center"
+          >
+            LOGIN
+          </Link>
           <a
             href="#"
             className="transform text-center font-semibold text-gray-500 duration-300 hover:text-gray-300"
