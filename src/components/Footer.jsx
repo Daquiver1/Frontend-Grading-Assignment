9fb57591306276d782e9a9908b31b9ce4eab7b79
oswaldamoah <oswaldamoah@gmail.com>
2023-12-04 23:24:00 +0000

    Footer jsx file

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>&copy; 2023, University of Ghana</p>
+      <p>Contact Us: ug@st.ug.edu.gh</p>
+    </footer>
+  );
+};
+
+export default Footer;
