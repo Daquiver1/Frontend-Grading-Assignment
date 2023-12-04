4e511e96209626d3dca5bddf73822e57e6d09934
Michael Perry Tettey <michaelperryt97@gmail.com>
2023-12-04 01:54:06 +0000

    Modified index.js, added dom routing

--- dcit-205-assignment/src/index.js
+++ dcit-205-assignment/src/index.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import SignUp from "./pages/SignUP";
+import LogIn from "./pages/LogIn";
+import reportWebVitals from "./reportWebVitals";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<LogIn />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
