99a302901845b56fa4d349fe9701b9559c4d5366
oswaldamoah <oswaldamoah@gmail.com>
2023-12-04 23:25:45 +0000

    added HelpAndSupportPage jsx file

---
+++ src/components/HelpAndSupportPage.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Header2 from './Header2';
+import Sidebar from './Sidebar';
+import Footer from './Footer';
+
+
+const HelpAndSupportPage = () => {
+  return (
+    <div>
+      <Header2 />
+      <Sidebar />
+      {/* Your Page Content Goes Here */}
+      <Footer />
+    </div>
+  );
+};
+
+export default HelpAndSupportPage;
+
+
