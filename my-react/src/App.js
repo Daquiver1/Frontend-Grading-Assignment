4b2d1e93881a791e1ac2a04d57d771447ef6ccce
NanaOji <akpaabe@gmail.com>
2023-12-04 10:45:05 +0000

    First Commit

---
+++ my-react/src/App.js
@@ -0,0 +1,21 @@
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+
+
+      <div className="main">
+       <Routes>
+       <Route exact path="/" element={<Home/>}/>
+       <Route  path="/dashboard" element={<Dashboard/>}/>
+       </Routes>
+       </div>
+
+      </Router>
+  
+    </div>
+  );
+}
+
+export default App;-
