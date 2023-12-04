889361c227c9d893f871313698f0b71603cd6144
Michael Perry Tettey <michaelperryt97@gmail.com>
2023-12-04 15:14:09 +0000

    Created a student object

---
+++ dcit-205-assignment/src/scripts/student-data.js
@@ -0,0 +1,8 @@
+export const Student = {
+  name: "Michael Perry Tettey",
+  major: "Bsc.Computer Science",
+  role: "Student",
+  image:
+    "https://pbs.twimg.com/profile_images/1728364037228933121/-yM3FBA5_400x400.jpg",
+};
+export default Student;
