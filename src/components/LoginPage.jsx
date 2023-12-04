8d66fa5b8167a1289b3542e1e07c9b95d1167b2e
oswaldamoah <oswaldamoah@gmail.com>
2023-12-04 23:24:53 +0000

    LoginPage jsx file

---
+++ src/components/LoginPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header1 from './Header1';
+
+
+const LoginPage = () => {
+  return (
+    <div>
+      <Header1 />
+      {/* Your Login Page Content Goes Here */}
+    </div>
+  );
+};
+
+export default LoginPage;
