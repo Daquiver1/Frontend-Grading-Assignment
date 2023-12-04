6534f0f0748082037ba2da68dbc41320cac45190
DylanGates <Solomonafoh@gmail.com>
2023-12-04 03:40:28 +0000

    Initial Commit

---
+++ my-app/src/components/NavBar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './NavBar.css';
+
+const NavBar = () => {
+  return (
+    <div className="fixed top-0 flex flex-row w-16 w-screen m-0 text-white bg-gray-900 shadow-lg">
+      <ul>
+        <li>A</li>
+        <li>B</li>
+        <li>C</li>
+        <li>D</li>
+        <li>E</li>
+        <li>F</li>
+      </ul>
+    </div>
+  );
+};
+
+export default NavBar;
---
+++ my-app/src/pages/login.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+
+const LoginPage = () => {
+  const [id, setId] = useState('');
+  const [pin, setPin] = useState('');
+  const [error, setError] = useState('');
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    if (id === '123456' && pin === '7890') {
+      alert('Login successful!');
+      setError('');
+    } else {
+      setError('Invalid ID or PIN');
+    }
+
+    // Clear input fields after login attempt
+    setId('');
+    setPin('');
+  };
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-200">
+      <div className="p-8 bg-white rounded shadow">
+        <h2 className="mb-4 text-2xl font-bold">Login</h2>
+        {error && <p className="mb-4 text-red-500">{error}</p>}
+        <form onSubmit={handleLogin}>
+          <div className="mb-4">
+            <label className="block mb-2 text-sm font-bold text-gray-700" htmlFor="id">
+              ID
+            </label>
+            <input
+              className="w-full px-3 py-2 leading-tight text-gray-700 border rounded appearance-none focus:outline-none focus:shadow-outline"
+              id="id"
+              type="text"
+              placeholder="Enter your ID"
+              value={id}
+              onChange={(e) => setId(e.target.value)}
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block mb-2 text-sm font-bold text-gray-700" htmlFor="pin">
+              PIN
+            </label>
+            <input
+              className="w-full px-3 py-2 leading-tight text-gray-700 border rounded appearance-none focus:outline-none focus:shadow-outline"
+              id="pin"
+              type="password"
+              placeholder="Enter your PIN"
+              value={pin}
+              onChange={(e) => setPin(e.target.value)}
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-600 focus:outline-none focus:shadow-outline"
+              type="submit"
+            >
+              Login
+            </button>
+            <a
+              className="inline-block text-sm font-bold text-blue-500 align-baseline hover:text-blue-800"
+              href="#"
+            >
+              Forgot PIN?
+            </a>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
