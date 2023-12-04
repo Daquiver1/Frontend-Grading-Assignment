b83cf20bb98804fb5575a56f99edc6b89054b82f
Bless Adeti <blessadeti7@gmail.com>
2023-12-04 17:00:19 +0000

    Started Landing Page

--- dcit205/src/App.js
+++ dcit205/src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <nav className="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+            <div className="container">
+              <a className="navbar-brand js-scroll-trigger" href="#page-top">Start Bootstrap</a>
+              <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                Menu
+                <i className="fa fa-bars"></i>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarResponsive">
+                <ul className="navbar-nav text-uppercase ml-auto">
+                  <li className="nav-item">
+                    <a className="nav-link js-scroll-trigger" href="#services">Home</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link js-scroll-trigger" href="#portfolio">Login</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link js-scroll-trigger" href="#about">Dashboard</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link js-scroll-trigger" href="#team">Missing Grade Form</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link js-scroll-trigger" href="#contact">Contact Us</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+        </nav>
+      </div>
   );
 }
 
