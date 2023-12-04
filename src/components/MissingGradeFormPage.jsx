6f78f5f7af69bc66cbc642f3824539266c8e59b2
oswaldamoah <oswaldamoah@gmail.com>
2023-12-04 23:26:19 +0000

    added MissingGradeFormPage jsx file

---
+++ src/components/MissingGradeFormPage.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Header2 from './Header2';
+import Sidebar from './Sidebar';
+import Footer from './Footer';
+
+
+const MissingGradeFormPage = () => {
+  return (
+    <div>
+      <Header2 />
+      <Sidebar />
+      {/* Your Page Content Goes Here */}
+      <Footer />
+    </div>
+  );
+};
+
+export default MissingGradeFormPage;
+
+
