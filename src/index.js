5620d9560d9032d40441065b76c476d9bd818310
oswaldamoah <oswaldamoah@gmail.com>
2023-12-04 23:22:06 +0000

    index.js file for react

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'; // Assuming you have an App.js file
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
