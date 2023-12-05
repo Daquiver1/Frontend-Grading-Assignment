74c07698f375327bb39ab1a91bf0592a72e6c43b
Davelar1789 <dnalaryea003@st.ug.edu.gh>
2023-12-05 15:35:58 +0000

    fifth commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-// src/App.js
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Landingpage from './components/Landingpage';
-import LoginPage from './components/LoginPage';
-import DashboardPage from './components/DashBoard';
-import GradeReportPage from './components/GradeReport';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <Router>
-      <Route path="/" exact component={Landingpage} />
-      <Route path="/login" component={LoginPage} />
-      <Route path="/dashboard" component={DashBoard} />
-      <Route path="/grade-report" component={GradeReport} />
-      {/* Add more routes for other pages */}
-    </Router>
-  );
+    <div>
+      <h1>Hello</h1>
+    </div>
+  )
 }
 
-export default App;
+export default App
