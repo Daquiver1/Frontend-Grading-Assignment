5f254ae4fccf5cf0900e4cec115c6fb35ea9a3d3
bennydan7 <owusub243@gmail.com>
2023-12-05 14:44:36 +0000

    modified main page

--- frontend-grading-assignment/src/App.js
+++ frontend-grading-assignment/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./routes/home";
+import About from "./routes/About";
+import Service from "./routes/Service";
+import Contact from "./routes/Contact";
+import Login from "./routes/Login";
+import { Component, useState } from "react";
+import Dashboard from "./components/Dashboard/Dashboard";
 
 function App() {
+  // Using the useState hook to manage the route state
+  const [route, setRoute] = useState("login");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Conditional rendering based on the 'route' state */}
+      <Routes>
+        <Route path="/Dashboard" element={<Dashboard />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/service" element={<Service />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
