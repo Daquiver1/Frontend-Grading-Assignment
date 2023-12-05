96cde36c1e7ce022626021b31f924a2b80d852c3
bello abdul samed <blayonstance@blayonstance.bello.com>
2023-12-05 15:27:30 -0600

    card added adjustment to navbar also

--- my-web/src/components/appmain.js
+++ my-web/src/components/appmain.js
@@ -7,7 +7,21 @@ function Main(){
     <><div className="custom-div">
         <img className="imag" src={myImage} alt="Landing Image" />
       </div>
-      <h1 style={{ color: 'black' }}>Welcome to Gradesk</h1></>
+      <h1 style={{ color: '#123456' }}>Welcome to Gradesk</h1>
+      <main class="cards">
+        <section className="card contact">
+            <div className="icon">
+                <img src="#" alt="Contact us."/>
+            </div>
+            <h3>Contact us.</h3>
+            <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
+            <button>Learn More</button>
+        </section>
+        </main>
+      
+      
+      
+      </>
 
     );
     
--- my-web/src/components/navbar.js
+++ my-web/src/components/navbar.js
@@ -1,33 +1,32 @@
 import { useRef } from "react";
-import {FaBars,FaHtml5,FaTimes} from "react-icons/fa";
+import { FaBars, FaTimes } from "react-icons/fa";
 import "../styles/main.css";
 
-function Navbar(){
-    const navRef = useRef();
-    const showNavbar = () => {
-        navRef.current.classList.toggle("responsive_nav");
-    }
-    return(
-       <header>
-        <h3>Gradesk</h3>
-        <nav className="navy" ref={navRef}>
-                <a href="/#">Home</a>
-                <a href="/#">Help/Support</a>
-                <a href="/#">go</a>
-                <a href="/#">Home</a>
-                <button className="log-btn">
-                <a href="#" target="_blank">
-                <text className="tt-button">Log In</text> 
-                </a>
-                </button>
-                <button className="nav-btn nav-close-btn" onClick={showNavbar}>
-                    <FaTimes/>
-                </button>  
-        </nav>
-        <button className="nav-btn " onClick={showNavbar}>
-            <FaBars/>
-        </button>
-       </header> 
-    );
+function Navbar() {
+  const navRef = useRef();
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  };
+
+  return(
+    <header>
+     <h3>Gradesk</h3>
+     <nav className="navy" ref={navRef}>
+             
+             <button className="log-btn">
+             <a href="#" target="_blank">
+             <text className="tt-button">Log In</text> 
+             </a>
+             </button>
+             <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+                 <FaTimes/>
+             </button>  
+     </nav>
+     <button className="nav-btn " onClick={showNavbar}>
+         <FaBars/>
+     </button>
+    </header> 
+ );
 }
+
 export default Navbar;
