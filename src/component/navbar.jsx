9ee93d9bfdc35b5b87eedc7786990a625ac7fdfd
Endykay23 <offeibeaendurance1@gmail.com>
2023-12-05 16:47:48 +0000

    working on the navbar

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -1,63 +1,55 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import LOGO from "../uglogo.png"
+import React from 'react';
+import { Link } from 'react-router-dom';
+import LOGO from '../uglogo.png';
 
 const Navbar = () => {
-    
-    return (
-        <div className="w-screen h-[84px] flex items-center justify-around  relative bg-slate-50">
-          <img src={LOGO} alt='logo' className='w-24' />
-          <div>
-          <div className="left-[425.50px] top-[30px] justify-center items-center gap-[50px] inline-flex">
-            <div className="justify-start items-center gap-2.5 flex">
-            <Link to = '/'>
-                <div className="text-zinc-900 text-base font-medium font-['Inter'] leading-normal">Home</div>
-            </Link>
-            </div>
-            <div className="justify-start items-center gap-2.5 flex">
-            <Link to = '/dashboard'>
-                <div className="text-zinc-900 text-base font-normal font-['Inter'] leading-normal">Dashboard</div>
-            </Link>
-            </div>
-            <div className="justify-start items-center gap-2.5 flex">
-            <Link to = "/report">
-              <div className="text-zinc-900 text-base font-normal font-['Inter'] leading-normal">Grade Report</div>
-            </Link>
-            </div>
-            <div className="justify-start items-center gap-2.5 flex">
-              <Link to = "/missin-grade">
-                <div className="text-zinc-900 text-base font-normal font-['Inter'] leading-normal">Missing Grade</div>
-              </Link>
-            </div>
-            <div className="justify-start items-center gap-2.5 flex">
-              <Link to = "/contact">
-                <div className="text-zinc-900 text-base font-normal font-['Inter'] leading-normal">Contact</div>
-              </Link>
-            </div>
-            <div className="justify-start items-center gap-2.5 flex">
-              <Link to = "/help">
-                <div className="text-zinc-900 text-base font-normal font-['Inter'] leading-normal">Help</div>
-              </Link>
-            </div>
+  return (
+    <div className="w-screen h-[84px] flex flex-col sm:flex-row items-center justify-between relative bg-slate-50 p-4">
+      <div className="flex items-center">
+        <img src={LOGO} alt="logo" className="w-24" />
+      </div>
+
+      <div className="flex flex-col sm:flex-row mt-4 sm:mt-0">
+        <div className="flex flex-col sm:flex-row justify-center items-center gap-2.5 sm:gap-5">
+          <Link to="/">
+            <div className="text-zinc-900 text-base font-medium font-['Inter'] leading-normal">Home</div>
+          </Link>
+          <Link to="/dashboard">
+            <div className="text-zinc-900 text-base font-normal font-['Inter'] leading-normal">Dashboard</div>
+          </Link>
+          <Link to="/report">
+            <div className="text-zinc-900 text-base font-normal font-['Inter'] leading-normal">Grade Report</div>
+          </Link>
+          <Link to="/missin-grade">
+            <div className="text-zinc-900 text-base font-normal font-['Inter'] leading-normal">Missing Grade</div>
+          </Link>
+          <Link to="/contact">
+            <div className="text-zinc-900 text-base font-normal font-['Inter'] leading-normal">Contact</div>
+          </Link>
+          <Link to="/help">
+            <div className="text-zinc-900 text-base font-normal font-['Inter'] leading-normal">Help</div>
+          </Link>
         </div>
-        <div className="left-[105px] top-[30px]  justify-start items-center gap-2 inline-flex">
-            <div className="w-[35px] h-6 relative" />
+
+        <div className="mt-4 sm:mt-0 ml-0 sm:ml-5 flex items-center gap-2">
+          <div className="w-[35px] h-6 relative" />
         </div>
-        <div className="left-[1138px] top-[22px]  justify-start items-start gap-3.5 inline-flex">
-            <div className="px-5 py-2.5 bg-slate-50 rounded-md justify-start items-center gap-2.5 flex">
-              <Link to = "/login">
-                <div className="text-center text-blue-500 text-sm font-medium font-['Inter'] leading-tight">Login</div>
-              </Link>
-            </div>
-            <div className="px-5 py-2.5 bg-blue-500 rounded-md justify-start items-center gap-2.5 flex">
-            <Link to = "sign">
+
+        <div className="mt-4 sm:mt-0 ml-auto flex items-center gap-2">
+          <div className="px-5 py-2.5 bg-slate-50 rounded-md">
+            <Link to="/login">
+              <div className="text-center text-blue-500 text-sm font-medium font-['Inter'] leading-tight">Login</div>
+            </Link>
+          </div>
+          <div className="px-5 py-2.5 bg-blue-500 rounded-md">
+            <Link to="sign">
               <div className="text-center text-white text-sm font-medium font-['Inter'] leading-tight">Sign up</div>
             </Link>
-            </div>
-        </div>
           </div>
         </div>
-)
-}
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
