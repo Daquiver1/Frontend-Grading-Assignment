d95219a6b371d4cb5ca199600b7a64d351d5e778
Nana-king-netizen <otengnanayaw354@gmail.com>
2023-12-05 16:52:35 +0000

    Modified Footer and created empty pages

--- assignment/src/components/Footer.jsx
+++ assignment/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import Container from "../Container";
 const Footer = () => {
-    return <footer className="bg-gray-400 text-white">
+    return <footer className="bg-gray-400 text-white align-middle leading-8 ">
         <div className="md:flex md:justify-between md:items-center sm:px-12 px-4 bg-blue-700 py-1">
         <h1 className="lg:text-4xl text-3xl md:mb-0 mb-6 lg:leading-normal font-semibold md:w-2/5"><span className="text-yellow-500">3 Steps </span>away from a rectified Grade</h1>
         </div>
--- assignment/src/components/Items.jsx
+++ assignment/src/components/Items.jsx
@@ -1,10 +1,35 @@
 const Items = () => {
     return ( 
-     <div>
-        <p>
-        {'\u00a9'} 2023 Missing Grade Report System | All Rights Reserved
-        </p>
-        
+     <div className="flex justify-between p-5">
+        <ul>
+            <li>{'\u00a9'} 2023 Missing Grade Report System | All Rights Reserved</li>
+            <li>For technical support please contact:</li>
+
+            <li><span className="text-blue-700 hover:cursor-pointer hover:text-yellow-500 duration-75">support@missinggradesystem.com </span>| (+233)549-467-625</li>
+            <li>Follow us on social media:</li>
+
+            <li className="text-blue-700 py-1 align-middle"> <span className="mr-1 align-middle ">
+            <ion-icon name="logo-facebook"></ion-icon>
+            </span>
+            Facebook|  
+             <span className="ml-1 mr-1 align-middle" >
+            <ion-icon name="logo-linkedin"></ion-icon>
+            </span>
+            LinkedIn | 
+            <span className="ml-1 mr-1 pt-[6px]">
+            <ion-icon name="logo-twitter"></ion-icon>
+            </span>
+            Twitter</li>
+            
+        </ul>
+        <div className="p-1">
+            <ul>
+                <li>Address: LG 25 ,Legon,Accra</li>
+                <li className="hover: text-blue-700 cursor-pointer">Privacy Policy| Terms of Service | Accessibility </li>
+                <li>This system is designed and maintained by: <br /><span className="text-blue-700">Department of Computer Science|UG</span></li>
+            </ul>
+        </div>
+
      </div>
      );
 }
