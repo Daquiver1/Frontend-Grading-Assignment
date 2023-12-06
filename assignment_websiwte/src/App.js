026381c3600ef465e7602078b9d95a62f6051580
kurlerprudent <149204888+kurlerprudent@users.noreply.github.com>
2023-12-06 09:27:18 +0000

    Added react to the project

---
+++ assignment_websiwte/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Home from './Home';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <Home/>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ assignment_websiwte/src/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from './Header'
+
+export default function Home(){
+    return(
+        <>
+        <Header/>
+        <h2>This is the home page</h2>
+        </>
+    )
+}
