2ab93d2cdb9cce2c8a086a895d9c2bfea2bedfb9
oswaldamoah <oswaldamoah@gmail.com>
2023-12-06 00:31:03 +0000

    updated the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,18 @@
+// Footer.jsx
 import React from 'react';
-
+import '../index.css';
 
 const Footer = () => {
   return (
-    <footer>
-      <p>&copy; 2023, University of Ghana</p>
-      <p>Contact Us: ug@st.ug.edu.gh</p>
+    <footer className="footer-container">
+      <div className="footer-content">
+        <div className="footer-left">
+          <p>&copy; 2023 UGCS</p>
+        </div>
+        <div className="footer-right">
+          <p>Contact Us: <a href="mailto:frontdesk@ug.edu.gh">frontdesk@ug.edu.gh</a></p>
+        </div>
+      </div>
     </footer>
   );
 };
