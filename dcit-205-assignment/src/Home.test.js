fdc1d040d7b66c2773eb5617c0f073a4c3d0cf9d
Michael Perry Tettey <michaelperryt97@gmail.com>
2023-12-06 10:47:02 +0000

    Wrote test for home page

---
+++ dcit-205-assignment/src/Home.test.js
@@ -0,0 +1,18 @@
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import Home from './Home'
+
+const homeLinks = [
+  { text: "Apply Here", location: "/login" },
+  { text: "Get started", location: "/login" },
+];
+
+test.each(homeLinks)("Check if Home Links are working",(link) => {
+  render(
+    <BrowserRouter>
+    <Home />
+    </BrowserRouter>
+  );
+  const homeDOM = screen.getByText(link.text);
+  expect(homeDOM).toHaveAttribute("href", link.location);
+});
