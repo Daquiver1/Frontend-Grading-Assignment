c95cfe1e6bdbad0b2e1299496db8d5cdefa9d79f
kodditor <kobbyowusudarko@gmail.com>
2023-12-06 22:29:01 +0000

    Changed grids to flexboxes and responsiveness

--- assignment-1/src/App.js
+++ assignment-1/src/App.js
@@ -26,12 +26,12 @@ function App() {
       <Header />
       <main>
         <section>
-          <div className='grid grid-cols-[1.5fr,1fr] mt-16'>
+          <div className='flex flex-col-reverse gap-8 md:gap-0 md:grid grid-cols-[1.5fr,1fr] mt-16'>
             <span>
-              <h3 className=' leading-7 w-3/4'><span className='pulsing-text'>Introducing GradeRepo</span> Elevate Your Academic Experience! Uncover hidden achievements, 
+              <h3 className=' leading-7 text-center md:text-left md:w-3/4 mb-4 md:mb-0'><span className='pulsing-text'>Introducing GradeRepo</span> Elevate Your Academic Experience! Uncover hidden achievements, 
                 track every grade, and take control of your academic narrative with our user-friendly system. 
                 Login in now and experience a new level of grade transparency.</h3>
-              <h1 className='text-ugBlue'>Take Control of Your Academic Narrative.</h1>
+              <h1 className='text-ugBlue text-center md:text-left'>Take Control of Your Academic Narrative.</h1>
             </span>
             <span>
               <img src='/img/Macbook Pro.png' />
@@ -39,18 +39,18 @@ function App() {
           </div>
         </section>
         <section>
-          <div className='bg-ugBlue text-white rounded-3xl p-10 '>
-            <h2 className='text-white font-light text-5xl tracking-[3px]'>Why GradeRepo?</h2>
-            <div className='flex gap-2 place-items-center mt-4 h-[200px] '>
-              <span className='w-2/5'>
+          <div className='bg-ugBlue text-white rounded-3xl p-6 md:p-10 '>
+            <h2 className='text-white font-light text-5xl tracking-[3px] text-center md:text-left'>Why GradeRepo?</h2>
+            <div className='flex flex-col md:flex-row gap-8  md:gap-2 place-items-center mt-4 md:h-[200px] '>
+              <span className='w-full overflow-auto text-center md:text-left mt-2 md:mt-0 md:w-2/5'>
                 { reasons.map((reasonObj, idx) =>{
                     return (
                       <h3 key={idx} onClick={(e)=>{handleReasonChange(e, reasonObj)}} className={`mb-4 cursor-pointer hover:text-white duration-150 text-xl last:mb-0 ${(reasonObj === reason) ? 'text-white font-bold':'text-gray-400'}`}>{reasonObj.title}</h3>
                     )
                 })}
               </span>
-              <span className='w-3/5'>
-                <p id='reason' className='text-2xl duration-150'>{reason.body}</p>
+              <span className='w-full md:w-3/5'>
+                <p id='reason' className='text-2xl text-center md:text-left duration-150'>{reason.body}</p>
               </span>
             </div>
           </div>
