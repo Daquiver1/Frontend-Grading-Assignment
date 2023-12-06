b205058c9411b056fdcfb1c77ee470f66ff91bbf
kodditor <kobbyowusudarko@gmail.com>
2023-12-06 22:27:43 +0000

    Added custom grid templates for mobile

--- assignment-1/tailwind.config.js
+++ assignment-1/tailwind.config.js
@@ -11,6 +11,8 @@ module.exports = {
       gridTemplateColumns: {
         grades: '150px 1fr 250px 70px',
         instruct: '50px, 1fr 170px 90px',
+        gradesMobile: '90px 200px 0px 70px',
+        instructMobile: '50px 1fr 0px 100px'
       }
     },
   },
