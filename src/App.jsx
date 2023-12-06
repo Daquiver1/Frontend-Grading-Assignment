c049cc678e448544046931bbdbe586fd790b841f
ADK <adk.kda10@gmail.com>
2023-12-06 19:25:23 +0000

    Project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <h1 className="text-4xl font-semibold text-green-700 underline">
+      Hello world!
+    </h1>
+  );
+};
+
+export default App;
