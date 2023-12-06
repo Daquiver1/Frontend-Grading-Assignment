ba8fd3936cacda495d764303fc0cf64426bfc9a4
kev-69 <bismarkobuobi19@gmail.com>
2023-12-06 17:31:09 +0000

    #

--- missing-grade-reporting-system/src/App.js
+++ missing-grade-reporting-system/src/App.js
@@ -1,11 +1,11 @@
-
-import './App.css';
 import Login from './Components/Login-Page/login.jsx';
+import Dashboard from './Components/Dashboard-Page/dashboardPage.jsx';
 
 function App() {
   return (
     <div>
       <Login />
+      <Dashboard />
     </div>
   );
 }
