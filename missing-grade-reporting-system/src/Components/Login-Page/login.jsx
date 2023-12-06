7d268736f6e4d2baeebb0fabf1368904018e0e79
kev-69 <bismarkobuobi19@gmail.com>
2023-12-06 11:34:21 +0000

    Applied a script that allow users to only enter number in the inputs field and also limit the number of inputs to 9 for ID number and 5 for PIN

---
+++ missing-grade-reporting-system/src/Components/Login-Page/login.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from 'react'
+import './login.css'
+
+import user_icon from '../Assests/user-pin_.png'
+import pin_icon from '../Assests/lock-password-unlocked-linear_.png'
+
+const Login = () => {
+    const [action, setAction] = useState('Sign In');
+    const [studentId, setStudentId] = useState('');
+    const [pin, setPin] = useState('');
+
+    const handleStudentIdChange = (e) => {
+        // Allow only numbers and limit to 9 digits
+        const sanitizedValue = e.target.value.replace(/\D/g, '').slice(0, 9);
+        setStudentId(sanitizedValue);
+      };
+
+    const handlePinChange = (e) => {
+        // Allow only numbers and limit to 5 digits
+        const sanitizedValue = e.target.value.replace(/\D/g, '').slice(0, 5);
+        setPin(sanitizedValue);
+      };
+
+
+  return (
+
+    <div className='container'>
+
+      <div className='header'>
+            <div className='text'><h2>{action}</h2></div>
+            <div className='underline'></div>
+      </div>
+
+      <div className='inputs'>
+        <div className='input'>
+          <img src={user_icon} alt='' className='img-size' />
+          <input
+            type='text'
+            placeholder='ID Number'
+            className='f-input'
+            value={studentId}
+            onChange={handleStudentIdChange}
+          />
+        </div>
+        
+        <div className='input'>
+          <img src={pin_icon} alt='' className='img-size' />
+          <input
+            type='password'
+            placeholder='* * * * *'
+            className='f-input'
+            value={pin}
+            onChange={handlePinChange}
+          />
+        </div>
+       </div>
+    <div>
+        <div className="forgot">
+            <p>Forgot Pin or Request Pin Change? <a href="index.html">Click Here!</a></p>
+        </div>
+
+        <div className='submit-container'>
+            <div className={action==="Sign Up"?"submit gray":"login-button"} onClick={() => {setAction("Sign In")}}>
+                Sign In
+            </div>
+        </div>
+    </div>
+    </div>
+
+  );
+};
+
+export default Login
