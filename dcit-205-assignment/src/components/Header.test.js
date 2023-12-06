16dd1c70a96414148b26b75eed60e38a65c6700a
Michael Perry Tettey <michaelperryt97@gmail.com>
2023-12-06 10:46:05 +0000

    Wrote test for header

---
+++ dcit-205-assignment/src/components/Header.test.js
@@ -0,0 +1,30 @@
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import Header from "./Header";
+
+const links = [
+  { text: "Home", location: "/home" },
+  { text: "Log in", location: "/login" },
+  { text: "Course Check", location: "/" },
+];
+
+test.each(links)("Check if the Nav Bar Has Links.", (link) => {
+  render(
+    <BrowserRouter>
+      <Header />
+    </BrowserRouter>
+  );
+  const linkDom = screen.getByText(link.text);
+  expect(linkDom).toHaveAttribute("href", link.location);
+});
+
+test("Check if have the logo has a link to the home page", () => {
+  render(
+    <BrowserRouter>
+      <Header />
+    </BrowserRouter>
+  );
+  const logoDom = screen.getByTestId(/logo/);
+  expect(logoDom).toHaveAttribute("href", "/");
+  expect(screen.getByAltText(/logo/)).toBeInTheDocument();
+});
