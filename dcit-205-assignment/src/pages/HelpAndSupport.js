bb292211ce18ed8175d82232f8f85c461850f975
Michael Perry Tettey <michaelperryt97@gmail.com>
2023-12-06 10:56:51 +0000

    Created Help and Support Page

---
+++ dcit-205-assignment/src/pages/HelpAndSupport.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+export default function HelpAndSupport() {
+    return(
+        <>
+        <Header />
+
+        <Footer />
+        </>
+    );
+}
