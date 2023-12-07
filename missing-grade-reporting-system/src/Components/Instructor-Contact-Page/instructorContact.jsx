156146cc95abaa606171b971ca4a9b53ff3f657f
kev-69 <bismarkobuobi19@gmail.com>
2023-12-07 21:49:06 +0000

    New component for Instuctor contact page

---
+++ missing-grade-reporting-system/src/Components/Instructor-Contact-Page/instructorContact.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react';
+import './instructorContact.css';
+
+const Contacts = () => {
+  useEffect(() => {
+    document.title = 'Contact Instructor';
+  }, []);
+
+  return (
+    <div>
+      {/* Your component content goes here */}
+    </div>
+  );
+};
+
+export default Contacts;
