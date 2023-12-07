d9fec2a8cb053624773b8a2810258781ec7c7f60
mighty808 <effahaboagye888@gmail.com>
2023-12-07 19:10:40 +0000

    first commit

---
+++ src/Home.jsx
@@ -0,0 +1,195 @@
+import React from 'react'
+import 
+{ BsFillArchiveFill, BsFillGrid3X3GapFill, BsPeopleFill, BsFillBellFill}
+ from 'react-icons/bs'
+ import 
+ { ComposedChart, BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, LineChart, Line, Area, Scatter, PieChart, Pie, Sector, } 
+ from 'recharts';
+
+function Home() {
+
+    const data = [
+        {
+          name: 'Page A',
+          uv: 4000,
+          pv: 2400,
+          amt: 2400,
+        },
+        {
+          name: 'Page B',
+          uv: 3000,
+          pv: 1398,
+          amt: 2210,
+        },
+        {
+          name: 'Page C',
+          uv: 2000,
+          pv: 9800,
+          amt: 2290,
+        },
+        {
+          name: 'Page D',
+          uv: 2780,
+          pv: 3908,
+          amt: 2000,
+        },
+        {
+          name: 'Page E',
+          uv: 1890,
+          pv: 4800,
+          amt: 2181,
+        },
+        {
+          name: 'Page F',
+          uv: 2390,
+          pv: 3800,
+          amt: 2500,
+        },
+        {
+          name: 'Page G',
+          uv: 3490,
+          pv: 4300,
+          amt: 2100,
+        },
+      ];
+     
+      const data01 = [
+        { name: 'Group A', value: 400 },
+        { name: 'Group B', value: 300 },
+        { name: 'Group C', value: 300 },
+        { name: 'Group D', value: 200 },
+      ];
+      const data02 = [
+        { name: 'A1', value: 100 },
+        { name: 'A2', value: 300 },
+        { name: 'B1', value: 100 },
+        { name: 'B2', value: 80 },
+        { name: 'B3', value: 40 },
+        { name: 'B4', value: 30 },
+        { name: 'B5', value: 50 },
+        { name: 'C1', value: 100 },
+        { name: 'C2', value: 200 },
+        { name: 'D1', value: 150 },
+        { name: 'D2', value: 50 },
+      ];
+      
+
+  return (
+    <main className='main-container'>
+        <div className='main-title'>
+            <h3>DASHBOARD</h3>
+        </div>
+
+        <div className='main-cards'>
+            <div className='card'>
+                <div className='card-inner'>
+                    <h3>CURRENT GRADE</h3>
+                    <BsFillArchiveFill className='card_icon'/>
+                </div>
+                <h1>A</h1>
+            </div>
+            <div className='card'>
+                <div className='card-inner'>
+                    <h3>COURSES</h3>
+                    <BsFillGrid3X3GapFill className='card_icon'/>
+                </div>
+                <h1>7</h1>
+            </div>
+            <div className='card'>
+                <div className='card-inner'>
+                    <h3>STUDENTS</h3>
+                    <BsPeopleFill className='card_icon'/>
+                </div>
+                <h1>1,448</h1>
+            </div>
+            <div className='card'>
+                <div className='card-inner'>
+                    <h3>ALERTS</h3>
+                    <BsFillBellFill className='card_icon'/>
+                </div>
+                <h1>42</h1>
+            </div>
+        </div>
+
+        <div className='charts'>
+            <ResponsiveContainer width="100%" height="100%">
+            <BarChart
+            width={500}
+            height={300}
+            data={data}
+            margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+            }}
+            >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Bar dataKey="pv" fill="#8884d8" />
+                <Bar dataKey="uv" fill="#82ca9d" />
+                </BarChart>
+            </ResponsiveContainer>
+
+            <ResponsiveContainer width="100%" height="100%">
+                <LineChart
+                width={500}
+                height={300}
+                data={data}
+                margin={{
+                    top: 5,
+                    right: 30,
+                    left: 20,
+                    bottom: 5,
+                }}
+                >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+                <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+                </LineChart>
+            </ResponsiveContainer>
+
+            <ResponsiveContainer width="100%" height="100%">
+        <ComposedChart
+          width={500}
+          height={400}
+          data={data}
+          margin={{
+            top: 20,
+            right: 20,
+            bottom: 20,
+            left: 20,
+          }}
+        >
+          <CartesianGrid stroke="#f5f5f5" />
+          <XAxis dataKey="name" scale="band" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Area type="monotone" dataKey="amt" fill="#8884d8" stroke="#8884d8" />
+          <Bar dataKey="pv" barSize={20} fill="#413ea0" />
+          <Line type="monotone" dataKey="uv" stroke="#ff7300" />
+          <Scatter dataKey="cnt" fill="red" />
+        </ComposedChart>
+      </ResponsiveContainer>
+
+      <ResponsiveContainer width="100%" height="100%">
+        <PieChart width={400} height={400}>
+          <Pie data={data01} dataKey="value" cx="50%" cy="50%" outerRadius={60} fill="#8884d8" />
+          <Pie data={data02} dataKey="value" cx="50%" cy="50%" innerRadius={70} outerRadius={90} fill="#82ca9d" label />
+        </PieChart>
+      </ResponsiveContainer>
+
+        </div>
+    </main>
+  )
+}
+
+export default Home
