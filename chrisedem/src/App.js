a09857b274df7f74838c31c49e4ae35eeabda4a3
Christian22-cmyk <chriswedem20@gmail.com>
2023-12-07 13:31:51 +0000

    worked on the front page of my website, added some css styles and color to my navigation links

--- chrisedem/src/App.js
+++ chrisedem/src/App.js
@@ -9,13 +9,7 @@ function App() {
   <div className='App'>
     <Navbar></Navbar>
     <div className='content'>
-      <a href='/'>Home</a>
-      <a href='Login'>Login</a>
-      <a href='Dashboard'>Dashboard</a>
-      <a href='Grade report'>Grade Report</a>
-      <a href='Mising Grade form'>Missing Grade Form</a>
-      <a href='Instructor Contact Page'>Instructor Contact Page</a>
-      <a href='Help and Support Page'>Help and Support Page</a>
+      
       
 
       <Home/>
--- chrisedem/src/Navbar.js
+++ chrisedem/src/Navbar.js
@@ -1,15 +1,21 @@
-import React from 'react';
-
-
 const Navbar = () => {
-  return (  
-    <nav className="Mybars">
-      <h1> Scholar Grade Support</h1>
+  return (
+    <nav className="navbar">
+      <h1>Scholar Grade Support</h1>
       <div className="links">
+        <a href="/">Home</a>
+        <a href="Login">Login</a>
+        <a href="Dashboard">Dashboard</a>
+        <a href="Grade Report page">Grade Report</a>
         
+        <a href="/create" style={{ 
+          color: 'white', 
+          backgroundColor: '#f1356d',
+          borderRadius: '8px' 
+        }}>New Blog</a>
       </div>
     </nav>
   );
 }
  
-export default Navbar; 
+export default Navbar;
