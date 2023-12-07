8dd9773adaeb31d941898e7962a8b05a7e498554
Levinkrumah <152205122+Levinkrumah@users.noreply.github.com>
2023-12-07 16:46:49 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import LandingPage from './pages/LandingPage';
+import LoginPage from './pages/LoginPage';
+import DashboardPage from './pages/DashboardPage';
+import GradeReportPage from './pages/GradeReportPage';
+import MissingGradeFormPage from './pages/MissingGradeFormPage';
+import InstructorContactPage from './pages/InstructorContactPage';
+import HelpSupportPage from './pages/HelpSupportPage';
+
+function App() {
+  const appStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    minHeight: '100vh',
+  };
+
+  return (
+    <Router>
+      <div style={appStyle}>
+        <Navbar />
+        <Switch>
+          {/* Routes go here */}
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
