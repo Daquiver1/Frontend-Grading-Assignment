787c3e9ed43f5ad816a501a050ae5340fa9bae82
Davelar1789 <dnalaryea003@st.ug.edu.gh>
2023-12-07 16:04:41 +0000

    eighth commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import './App.css';
+import Login from './Login';
 
 const App = () => {
   return (
-    <div>
-      <button className="login-button" onClick={() => window.location.href = '/login'}>
-        Login
-      </button>
+    <Router>
+      <div>
+        <Link className="login-button" to="/login">
+          Login
+        </Link>
 
-      <div className="card">
-        <p><strong>Missing Grade Reporting System</strong></p>
-        <p className="card-footer">This website is used to enable students to view their grades, report missing grades and communicate with their instructors or academic administration</p>
+        <Switch>
+          <Route path="/login" exact>
+            <Login />
+          </Route>
+          <Route path="/">
+            <div>
+              <div className="card">
+                <p><strong>Missing Grade Reporting System</strong></p>
+                <p className="card-footer">This website is used to enable students to view their grades, report missing grades and communicate with their instructors or academic administration</p>
+              </div>
+            </div>
+          </Route>
+        </Switch>
       </div>
-    </div>
+    </Router>
   );
 };
 
