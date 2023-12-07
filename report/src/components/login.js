abea01b15f05a9c4ad65ea2da342a580aee01e09
Dwamenachrist <136202826+Dwamenachrist@users.noreply.github.com>
2023-12-07 04:26:33 +0000

    God really loves us

--- report/src/components/login.js
+++ report/src/components/login.js
@@ -15,12 +15,8 @@ function AppLogin() {
         <MDBCol col='6' className="mb-5">
           <div className="d-flex flex-column ms-5">
             <div className="text-center">
-              <img
-                src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/lotus.webp"
-                style={{ width: '185px' }}
-                alt="logo"
-              />
-              <h4 className="mt-1 mb-5 pb-1">We are The Lotus Team</h4>
+            <img width="96" height="96" src="https://img.icons8.com/external-tal-revivo-shadow-tal-revivo/96/external-targeting-a-good-grades-in-high-school-exams-school-shadow-tal-revivo.png" alt="external-targeting-a-good-grades-in-high-school-exams-school-shadow-tal-revivo"/>
+              <h3 className="mt-1 mb-5 pb-1">Your Path to Academic Success</h3>
             </div>
             <p>Please login to your account</p>
             <MDBInput wrapperClass='mb-4' label='Email address' id='form1' type='email' />
