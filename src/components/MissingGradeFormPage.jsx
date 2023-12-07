3d23d612b7fb57dd8d3d60ad7f0f4e34e2269e2c
oswaldamoah <oswaldamoah@gmail.com>
2023-12-07 02:41:20 +0000

    updated MissingGradeFormPage jsx file

--- src/components/MissingGradeFormPage.jsx
+++ src/components/MissingGradeFormPage.jsx
@@ -2,9 +2,21 @@ import React from 'react';
 
 const MissingGradeFormPage = () => {
   return (
-    <div>
-      {/* Page Content */}
-    </div>
+    <body>
+    <div className="grade-report-page">
+      <h2 className="banner-title">Missing Grade Form</h2>
+
+      <div className="banner-title">
+        <iframe
+          src="https://forms.gle/hexgzae2UgtJiPyP8"
+          title="Missing Grade Form"
+          className="form-iframe"
+          width={800}
+          height={500}
+        ></iframe>
+      </div>
+    </div>    
+    </body>
   );
 };
 
