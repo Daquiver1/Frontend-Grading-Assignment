73709cfec0b092636af0879d91f6bbd87a8b2707
oswaldamoah <oswaldamoah@gmail.com>
2023-12-07 02:40:56 +0000

    restructured footer contents

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,12 +6,12 @@ const Footer = () => {
   return (
     <footer className="footer-container">
       <div className="footer-content">
-        <div className="footer-left">
+        <div className="footer-items">
+        <p>Contact Us: <a href="mailto:frontdesk@ug.edu.gh">frontdesk@ug.edu.gh</a></p>
           <p>&copy; 2023 UGCS</p>
+          
         </div>
-        <div className="footer-right">
-          <p>Contact Us: <a href="mailto:frontdesk@ug.edu.gh">frontdesk@ug.edu.gh</a></p>
-        </div>
+        
       </div>
     </footer>
   );
