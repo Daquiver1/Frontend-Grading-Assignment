136d8ec7ad9b7f10c02bc8eba1db4fc901efb0eb
NanaAmpomah <nanaampomaharhin@gmail.com>
2023-12-07 22:54:28 +0100

    created the content page of the login page - add the inputs for the student login details

---
+++ grading-website/src/pages/Login.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import HeroSection from '../components/HeroSection';
+import LeftSidebar from '../components/LeftSidebar';
+
+function Login() {
+  const backgroundImage=(
+    <>
+    <img src={process.env.PUBLIC_URL + './images/login-image.jpg'} />
+    </>
+  )
+  const pageContent=(
+    <>
+    <div className='student-login-details'>
+<div className='student-id-container'>
+    <input type='text' placeholder={'Student ID '} required='required' />
+    <div>Please fill out this field</div>
+   </div>
+   <div className='student-pin-container'>
+    <input type='text'placeholder='Pin' required='required' />
+    <div>Please fill out this field</div>
+   </div>
+    </div>
+    <div className='submit-button-container'>
+      <button type='submit'>Login</button>
+    </div>
+    </>
+   
+  )
+  return (
+    <>
+ <Navbar />
+    <HeroSection
+    backgroundImage={backgroundImage}  pageContent={pageContent}
+     />
+     <LeftSidebar />
+     
+    </>
+  )
+}
+
+export default Login
