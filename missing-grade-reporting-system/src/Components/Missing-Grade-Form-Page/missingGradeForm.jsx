1b05b4b7c7b3f7fcf151c58409a36e52aec94cd7
kev-69 <bismarkobuobi19@gmail.com>
2023-12-07 21:48:14 +0000

    New component for Missing Grade form

---
+++ missing-grade-reporting-system/src/Components/Missing-Grade-Form-Page/missingGradeForm.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react';
+import './gradeReport.css';
+
+const GradeReport = () => {
+  useEffect(() => {
+    document.title = 'Missing Grades Form';
+  }, []);
+
+  return (
+    <div>
+      {/* Your component content goes here */}
+    </div>
+  );
+};
+
+export default GradeReport;
