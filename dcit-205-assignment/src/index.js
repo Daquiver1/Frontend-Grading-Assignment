0fe2866440b3611054a12910d78f22b874534e57
Michael Perry Tettey <michaelperryt97@gmail.com>
2023-12-08 14:31:58 +0000

    Added routes to new pages

--- dcit-205-assignment/src/index.js
+++ dcit-205-assignment/src/index.js
@@ -6,6 +6,8 @@ import LogIn from "./pages/LogIn";
 import Dashboard from "./pages/Dashboard";
 import MissingGrades from "./pages/MissingGradeForm";
 import GradeReport from "./pages/GradeReport";
+import InstuctorContact from "./pages/InstructorConctact.Js";
+import HelpAndSupport from "./pages/HelpAndSupport";
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
 
@@ -20,6 +22,8 @@ root.render(
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/missing-grades" element={<MissingGrades />} />
         <Route path="/grade_report" element={<GradeReport />} />
+        <Route path="/instructor_contacts" element={<InstuctorContact />} />
+        <Route path="/help" element={<HelpAndSupport />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
