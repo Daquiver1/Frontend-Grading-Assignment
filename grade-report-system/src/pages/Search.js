c3c8f2fcc2e9d0b8d7e7e995737a0b5974f2433f
Bayat Osman <bayatosman827@gmail.com>
2023-12-08 20:11:25 +0000

    started working on my Help and support page

--- grade-report-system/src/pages/Search.js
+++ grade-report-system/src/pages/Search.js
@@ -5,6 +5,13 @@ import { Link } from "react-router-dom";
 
 const Search = () => {
   const [open, setOpen]= useState(true);
+  const [formData, setFormData] = useState({
+    name1: "",
+    emailAddress: "",
+    instructorName: "",
+    instructorEmail: "",
+    message: "",
+  });
 
   const Menus =[
     {title: "Dashboard",src: "Chart_fill", path:"/dashboard"},
@@ -16,6 +23,18 @@ const Search = () => {
   const handleMenuClick = (title) => {
     setOpen(!open);
   };
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    alert("Handling form submission");
+    alert(`Simulating email sending: ${formData.message}`, formData);
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+  
+
   return (
     <div className="flex">
         <div className={`${open? "w-72" :"w-20"} duration-300 h-screen bg-[#000435] relative`}>
@@ -39,12 +58,42 @@ const Search = () => {
               </li>)}
         </ul>
         </div>
-            <div className="p-7 text-2x1 font-semibold flex-1 h-screen bg-[#ece3e9]">
-             Search
-            <div>
-                <p>how are you</p>
+        <div className="p-7 text-2x1 font-semibold flex-1 h-screen bg-[#ece3e9]">
+        <div className="antialiased">
+          <div className="flex w-full min-h-screen justify-center items-center">
+            <div className="flex flex-col space-y-6 bg-cyan-700 w-full max-w-4xl p-14 rounded-xl shadow-lg text-white">
+              <div className="flex flex-col justify-between"> 
+                <div>
+                  <h1 className='font-bold text-4xl tracking-wide'>Missing Grade Form</h1>
+                </div>
+              </div>
+              <div>
+                <div className="bg-white rounded-xl shadow-lg p-8 text-gray-600 pt-0">
+                  <form action="" className="flex flex-col space-y-4" onSubmit={handleFormSubmit}>
+                    <div>
+                      <label for="" className="text-sm">Course name</label>
+                      <input type="text" value={formData.name1}  name="name" onChange={handleInputChange} placeholder="Your name" className="ring-1 ring-gray-300 w-full rounded-md px-4 py-2 outline-none focus:ring-2 focus:ring-teal-300"/>
+                    </div>
+                    <div>
+                      <label for="" className="text-sm">Instructor name</label>
+                      <input type="text" value={formData.instructorName} name="instructorName" onChange={handleInputChange} placeholder="Your name" className="ring-1 ring-gray-300 w-full rounded-md px-4 py-2 outline-none focus:ring-2 focus:ring-teal-300"/>
+                    </div>
+                    <div>
+                      <label for="" className="text-sm">Expected grade</label>
+                      <input type="email"  value={formData.message} name="message" onChange={handleInputChange} placeholder="Grade" className="ring-1 mt-2 ring-gray-300 w-full rounded-md px-4 py-2 outline-none focus:ring-2 focus:ring-teal-300"/>
+                    </div>
+                    <div>
+                      <label for="" className="text-sm">Explanation field</label>
+                      <textarea type="Message"  value={formData.message}  name="message" onChange={handleInputChange} rows="4" placeholder="Message" className="ring-1 mt-2 ring-gray-300 w-full rounded-md px-4 py-2 outline-none focus:ring-2 focus:ring-teal-300"></textarea>
+                    </div>
+                    <button type="submit" class="inline-block self-end bg-cyan-700 text-white font-bold rounded-lg px-6 py-2 uppercase test-sm">Submit</button>
+                  </form>
+                </div>
+              </div>
             </div>
-       </div>
+          </div>
+        </div>
+        </div>
     </div>
   )
 }
--- grade-report-system/src/pages/Support.js
+++ grade-report-system/src/pages/Support.js
@@ -14,8 +14,27 @@ const Support = () => {
   const handleMenuClick = (title) => {
     setOpen(!open);
   };
+  const [formData, setFormData] = useState({
+    name1: "",
+    emailAddress: "",
+    instructorName: "",
+    instructorEmail: "",
+    message: "",
+  });
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    alert("Handling form submission");
+    alert(`Simulating email sending: ${formData.message}`, formData);
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+
   return (
-    <div className="flex">
+    <div className="flex overflow-hidden">
     <div className={`${open? "w-72" :"w-20"} duration-300 h-screen bg-[#000435] relative`}>
       <img src={require("../assets/control.png")} 
       className={`absolute cursor-pointer rounded-full -right-3 top-9 w-7 border-2 border-[#000435] ${!open && "rotate-180"}`}
@@ -38,11 +57,61 @@ const Support = () => {
       </ul>
     </div>
     <div className="p-7 text-2x1 font-semibold flex-1 h-screen bg-[#ece3e9]">
-       Support
-      <div>
-        <p>how are you</p>
+    <div>
+    <section className="bg-gray-200 p-15 mb-15 rounded-md shadow-md mb-[30px] text-2xl">
+        <p>How do I Register and Pay for my Re-sit</p>
+      </section>
+
+      <section className="bg-gray-200 p-15 mb-15 rounded-md shadow-md mb-[30px] text-2xl">
+        <p>Can I pay for School Fees Online</p>
+      </section>
+      <section className="bg-gray-200 p-15 mb-15 rounded-md shadow-md mb-[30px] text-2xl">
+        <p>Can I view my Registered Courses and Exams Results on UGMobile</p>
+      </section>
+
+      <section className="bg-gray-200 p-15 mb-15 rounded-md shadow-md mb-[30px] text-2xl">
+        <p>How Can I connect my device to the University WIFI</p>
+     
+      </section>
+      <section className="bg-gray-200 p-15 mb-15 rounded-md shadow-md mb-[30px] text-2xl">
+        <p>How do I reset MIS Web Password</p>
+      </section>
+
+      <section className="bg-gray-200 p-15 mb-15 rounded-md shadow-md mb-[30px] text-2xl">
+        <p>How do I defer my course of study or programme</p>
+      </section>
+      <section className="bg-gray-200 p-15 mb-15 rounded-md shadow-md mb-[30px] text-2xl">
+        <p>Can I change my programme in the course of my study</p>
+      </section>
+
+      <div className="antialiased">
+        <div className="flex w-full min-h-screen justify-center items-center pb-[300px]">
+          <div className="flex flex-col space-y-6 bg-cyan-700 w-full max-w-4xl p-14 rounded-xl shadow-lg text-white">
+            <div className="flex flex-col justify-between"> 
+              <div>
+                <h1 className='font-bold text-4xl tracking-wide'>Missing Grade Form</h1>
+              </div>
+            </div>
+            <div>
+              <div className="bg-white rounded-xl shadow-lg p-8 text-gray-600 pt-0">
+                <form action="" className="flex flex-col space-y-4" onSubmit={handleFormSubmit}>
+                  <div>
+                    <label for="" className="text-sm">Course name</label>
+                    <input type="text" value={formData.name1}  name="name" onChange={handleInputChange} placeholder="Your name" className="ring-1 ring-gray-300 w-full rounded-md px-4 py-2 outline-none focus:ring-2 focus:ring-teal-300"/>
+                  </div>
+                  <div>
+                    <label for="" className="text-sm">Instructor name</label>
+                    <input type="text" value={formData.instructorName} name="instructorName" onChange={handleInputChange} placeholder="Your name" className="ring-1 ring-gray-300 w-full rounded-md px-4 py-2 outline-none focus:ring-2 focus:ring-teal-300"/>
+                  </div>
+                  <button type="submit" class="inline-block self-end bg-cyan-700 text-white font-bold rounded-lg px-6 py-2 uppercase test-sm">Submit</button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
+   </div>
   </div>
   )
 }
