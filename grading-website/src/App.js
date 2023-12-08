ed7f260a8daf89c68d56742d69b5ca9e4db1be40
NanaAmpomah <nanaampomaharhin@gmail.com>
2023-12-08 08:45:15 +0100

    imported the help and support page created and added its route

--- grading-website/src/App.js
+++ grading-website/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import HelpAndSupport from './pages/HelpAndSupport';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         < Route path='/home' element={<Home />} />
         < Route path='/login' element={<Login/>} />
+        < Route path='/help' element={<HelpAndSupport/>} />
       </Routes>
     </Router>
     
