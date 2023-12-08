b350af3f8a42c89d34db72af3128e2d93a1dcd14
bennydan7 <owusub243@gmail.com>
2023-12-08 14:05:00 +0000

    added path for tailwind

--- frontend-grading-assignment/tailwind.config.js
+++ frontend-grading-assignment/tailwind.config.js
@@ -1,5 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/components/Dashboard/Dashboard.js"],
-  // ...
+  content: [
+    "./src/components/Dashboard/Dashboard.js",
+    "./src/routes/HelpFAQs.js",
+    "./src/components/FAQ/FAQ.js",
+    "./src/components/Grades/Grades.js",
+  ],
 };
