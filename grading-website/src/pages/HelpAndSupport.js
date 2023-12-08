5aa516eefd3c9b59c8069c8e96dd6521ca52747d
NanaAmpomah <nanaampomaharhin@gmail.com>
2023-12-08 06:21:16 +0100

    added the help and support page

---
+++ grading-website/src/pages/HelpAndSupport.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import HeroSection from '../components/HeroSection';
+import LeftSidebar from '../components/LeftSidebar';
+import Footer from '../components/Footer';
