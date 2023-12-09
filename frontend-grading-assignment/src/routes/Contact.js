13760ffff58838c1f3cda0bda064a7785bb8f4dc
bennydan7 <owusub243@gmail.com>
2023-12-09 10:27:40 +0000

    added instructor page

--- frontend-grading-assignment/src/routes/Contact.js
+++ frontend-grading-assignment/src/routes/Contact.js
@@ -2,6 +2,7 @@ import Hero from "../components/Hero/Hero";
 import Navbar from "../components/Navigation/Navbar";
 import AboutImg from "../assets/2.jpg";
 import Footer from "../components/Footer/Footer";
+import Instructors from "../components/Instructors/Instructors";
 
 function Contact() {
   return (
@@ -14,6 +15,7 @@ function Contact() {
         url="/"
         btnClass="hide"
       />
+      <Instructors />
       <Footer />
     </>
   );
