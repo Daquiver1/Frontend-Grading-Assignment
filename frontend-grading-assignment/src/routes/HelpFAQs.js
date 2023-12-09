3c6f3326ca69bb9e572bd2dcdf2b3195e936ef19
bennydan7 <owusub243@gmail.com>
2023-12-09 10:29:31 +0000

    added help and questions page

---
+++ frontend-grading-assignment/src/routes/HelpFAQs.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { questions } from "../components/FAQ/FAQData";
+import Hero from "../components/Hero/Hero";
+import Navbar from "../components/Navigation/Navbar";
+import FAQImg from "../assets/3.jpg";
+import Footer from "../components/Footer/Footer";
+
+import FAQ from "../components/FAQ/FAQ";
+
+function HelpFAQs() {
+  const [cards] = useState(questions);
+  return (
+    <>
+      <Navbar />
+      <Hero
+        cName="hero-abt"
+        heroImg={FAQImg}
+        title="Help and FAQs"
+        url="/"
+        btnClass="hide"
+      />
+      <section className=" max-w-xl mx-auto  py-20 px-4 ">
+        <h1 className="text-center uppercase tracking-widest font-bold mb-8">
+          Challenge FAQs
+        </h1>
+        <section className="grid grid-cols-1 gap-8">
+          {cards.map((card, index) => (
+            <FAQ {...card} key={index} />
+          ))}
+        </section>
+      </section>
+      <Footer />
+    </>
+  );
+}
+
+export default HelpFAQs;
