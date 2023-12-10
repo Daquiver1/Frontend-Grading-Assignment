9a3746b8ae78480daa7c5b6a0f09a58c67453fd9
Emmanuel Obodai <emmanuelcrash945@gmail.com>
2023-12-10 04:30:12 +0000

    Created the footer

---
+++ src/Components/footer.js
@@ -0,0 +1,11 @@
+export default function Footer() {
+    return(
+        <>    
+            <footer class="bg-dark text-light text-center py-3" id="footer">
+                <div class="container">
+                    <p>2023 Your Website Name</p>
+                </div>
+            </footer>
+        </>
+    ) 
+}
