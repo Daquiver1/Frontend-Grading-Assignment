ceae0083db7cf8a0ff1cd240787f4a4228c70b6d
MartinSalz54 <martinsalzberg54@gmail.com>
2023-12-09 17:17:01 -0800

    modification of images for landingpage

--- my/src/pages/home.js
+++ my/src/pages/home.js
@@ -1,31 +1,31 @@
 
 export default function Home() {
-    return (
-      <div>
-        <h1 className="left-align">
-          Stay informed about your academic progress with real-time updates on your
-          grades. Our system ensures that you have instant access to your latest
-          scores and gives youu feedback.
-        </h1>
-        <h2 className="left-align-small">
-          For Instructors:
-          <br />
-          Our grading system simplifies the grading process for instructors,
-          allowing them to provide timely and constructive feedback to students.
-          The intuitive interface streamlines the grading workflow, making it
-          easier for educators to focus on what matters most – fostering a
-          positive learning environment.
-        </h2>
-  
-        <div className="image-container">
-          <img className="image1" src="image1.jpg" alt="Image 1" />
-          <img className="image2" src="image2.jpg" alt="Image 2" />
-          <img className="image3" src="image3.jpg" alt="Image 3" />
-          <img className="image4" src="image4.jpg" alt="Image 4" />
+  return (
+    <div>
+      <h1 className="left-align">
+        Stay informed about your academic progress with real-time updates on your
+        grades. Our system ensures that you have instant access to your latest
+        scores and gives youu feedback.
+      </h1>
+      <h2 className="left-align-small">
+        For Instructors:
+        <br />
+        Our grading system simplifies the grading process for instructors,
+        allowing them to provide timely and constructive feedback to students.
+        The intuitive interface streamlines the grading workflow, making it
+        easier for educators to focus on what matters most – fostering a
+        positive learning environment.
+      </h2>
+
+      <div className="col">
+        <div className="card card1"></div>
+        <div className="card card2"></div>
+        <div className="card card3"></div>
+        <div className="card card4"></div>
         </div>
-      </div>
-    );
-  }
-  
-  
-  
+    </div>
+  );
+}
+
+
+
