a51513863f93bb22cbdef136018aa9c37b26f03a
kwekumorgan <morganmiguel.30@gmail.com>
2023-12-10 02:42:21 +0000

    deleted some unwanted

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-         My React HEEEEEEE
-        </a>
-      </header>
-    </div>
-  );
+class NewComponent extends React.Component {
+  hideSidebar() {
+    // Implement the logic to hide the sidebar
+    console.log('Sidebar hidden');
+  }
+
+  render() {
+    return (
+      <ul className="sidebar">
+        <li onClick={this.hideSidebar}><a href="#"><svg xmlns="http://www.w3.org/2000/svg" height={24} viewBox="0 -960 960 960" width={24}><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" /></svg></a></li>
+        <li><a href="aa">Home</a></li>
+        <li><a href="bb">Academics</a></li>
+        <li><a href="cc">About</a></li>
+        <li><a href="dd">Undergraduate Programmes</a></li>
+        <li><a href="ee">PhD Programmes</a></li>
+        <li><a href="ff">Compssa</a></li>
+        <li><a href="gg">Catalog</a></li>
+        <li><a href="hh">Faculty</a></li>
+        <li><a href="qq">Contact Us</a></li>
+      </ul>
+    );
+  }
 }
 
-export default App;
+export default NewComponent;
