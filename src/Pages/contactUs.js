894cc6236730ccf3a40d1ee49b727e99f4c3d22f
Emmanuel Obodai <emmanuelcrash945@gmail.com>
2023-12-10 04:28:30 +0000

    Created the contact us page

---
+++ src/Pages/contactUs.js
@@ -0,0 +1,12 @@
+import Header from "../Components/header"
+import Footer from "../Components/footer"
+
+export default function ContactUs() {
+    return(
+        <>
+        <Header/>
+        <h1>Contact Us</h1>
+        <Footer/>
+        </>
+    )    
+}
