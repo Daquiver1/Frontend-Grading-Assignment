3c4b4de86ebad671540d4e9b563c84f0ac774931
Emmanuel Obodai <emmanuelcrash945@gmail.com>
2023-12-10 04:25:48 +0000

    Created the help and support page

---
+++ src/Pages/helpAndSupport.js
@@ -0,0 +1,12 @@
+import Footer from "../Components/footer";
+import Header from "../Components/header"
+
+export default function HelpAndSupport() {
+    return(
+        <>
+        <Header/>
+        <h1>Help And Support</h1>
+        <Footer/>
+        </>
+    ) ;   
+}
