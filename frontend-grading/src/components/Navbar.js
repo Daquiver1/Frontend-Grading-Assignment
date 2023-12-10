6fb6c409711f26059020f9cf8aab1996675c02d2
KTTfx <nsromansarko@gmail.com>
2023-12-10 11:57:19 +0000

    Navbar build

---
+++ frontend-grading/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Logo from '../assets/ug logo.png';
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+        <div className='leftSide'>
+            <img src={Logo} />
+        </div>
+        <div className='rightSide'></div>
+    </div>
+  )
+}
+
+export default Navbar
