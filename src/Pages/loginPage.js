b4ba80a033ceed05660ec2f30e4029062d36aaf0
Emmanuel Obodai <emmanuelcrash945@gmail.com>
2023-12-10 04:24:16 +0000

    Created and finished the login page

---
+++ src/Pages/loginPage.js
@@ -0,0 +1,14 @@
+
+import Footer from "../Components/footer";
+import Header from "../Components/header"
+import LoginPageBody from "../Components/loginPageBody";
+
+export default function LoginPage() {
+    return(
+        <>
+
+        <LoginPageBody/>
+        <Footer/>
+        </>
+    ) ;   
+}
