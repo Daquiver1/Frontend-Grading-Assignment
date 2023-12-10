149a671ef10929df610f3dd7a5971b4668b7e3a5
Jan Phandoh <janphandoh@gmail.com>
2023-12-10 14:30:46 +0000

    Edited navbar links

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,22 +3,25 @@ import "../Styles/Navbar.css";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-  const[menuOpen,setMenuOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
     <nav>
       <div className="logo">
         <img className="logo-img" src={require("../Images/logo.png")} alt="" />
       </div>
-      <div className="menu" onClick={()=>{
-        setMenuOpen(!menuOpen);
-      }}>
+      <div
+        className="menu"
+        onClick={() => {
+          setMenuOpen(!menuOpen);
+        }}
+      >
         <span></span>
         <span></span>
         <span></span>
       </div>
       <div className="nav-list">
-        <ul className={menuOpen? "open" : ""}>
+        <ul className={menuOpen ? "open" : ""}>
           <li>
             <Link className="link" to="/">
               Home
@@ -35,13 +38,13 @@ export const Navbar = () => {
             </Link>
           </li>
           <li>
-            <Link className="link" to="/instructorContact">
-              Instructor-contact
+            <Link className="link" to="/missing-grade">
+              Missing-Grade
             </Link>
           </li>
           <li>
-            <Link className="link" to="/missing-grade">
-              Missing-Grade
+            <Link className="link" to="/instructorContact">
+              Instructor-contact
             </Link>
           </li>
           <li>
