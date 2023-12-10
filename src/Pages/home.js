44bbe243af1c2fc8104a1b7137ed422a94a2f490
Emmanuel Obodai <emmanuelcrash945@gmail.com>
2023-12-10 04:22:30 +0000

    Finished the home page

---
+++ src/Pages/home.js
@@ -0,0 +1,14 @@
+import Header from "../Components/header"
+import Footer from "../Components/footer"
+import HomeBody from "../Components/homeBody"
+
+
+export default function Home() {
+    return(
+        <>
+        <Header/>
+        <HomeBody/>
+        <Footer/>
+        </>
+    )    
+}
