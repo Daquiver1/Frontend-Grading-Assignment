fad2f36ae065dd8fcdb849a8f061acef264876bd
NanaAmpomah <nanaampomaharhin@gmail.com>
2023-12-11 16:29:19 +0100

    imported the instructors contact page and added a route to the page

--- grading-website/src/App.js
+++ grading-website/src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import HelpAndSupport from './pages/HelpAndSupport';
-import MissingGradeFormPage from './pages/MissingGradeFormPage';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import HelpAndSupport from "./pages/HelpAndSupport";
+import MissingGradeFormPage from "./pages/MissingGradeFormPage";
+import InstructorContact from "./pages/InstructorContact";
 
 function App() {
   return (
-    
-    
     <Router>
       <Routes>
-        < Route path='/home' element={<Home />} />
-        < Route path='/login' element={<Login/>} />
-        < Route path='/help' element={<HelpAndSupport/>} />
-        < Route path='/missing' element={<MissingGradeFormPage/>} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/help" element={<HelpAndSupport />} />
+        <Route path="/missing" element={<MissingGradeFormPage />} />
+        <Route path="/instructors" element={<InstructorContact />} />
       </Routes>
     </Router>
-    
   );
 }
 
