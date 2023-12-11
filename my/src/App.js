b18cb3ba14d1794439f321741c29a929c8d2a36a
MartinSalz54 <martinsalzberg54@gmail.com>
2023-12-10 16:53:55 -0800

    modified courses

--- my/src/App.js
+++ my/src/App.js
@@ -5,6 +5,8 @@ import Home from "./pages/home";
 import Login from "./pages/login";
 
 
+
+
 function App() {
   let component;
 
--- my/src/pages/login.js
+++ my/src/pages/login.js
@@ -1,36 +1,102 @@
 import React, { useState } from 'react';
 
 const fakeUserData = {
-  username: 'student123',
-  password: 'password123',
+  username: 'Macbeth',
+  password: 'macbeth123',
 };
 
 const fakeGradesData = [
-  { course: 'Math', grade: 'A' },
-  { course: 'English', grade: 'B' },
-  { course: 'Science', grade: 'Missing' },
-  // Add more courses as needed
+  // ... (your existing fakeGradesData)
+  { course: 'INTRODUCTION TO COMPUTER SCIENCE', grade: 'A', semester: 'FIRST SEMESTER', academicYear: '2022' },
+  { course: 'OFFICE PRODUCTIVITY TOOLS', grade: 'F', semester: 'FIRST SEMESTER', academicYear: '2022' },
+  { course: 'INTRODUCTION TO ECONOMICS I', grade: 'F', semester: 'Spring', academicYear: '2023' },
+  { course: 'STATISTICS AND PROBABILITY I', grade: 'F', semester: 'Spring', academicYear: '2023' },
+  { course: 'CRITICAL THINKING AND PRACTICAL REASONING', grade: 'F', semester: 'FIRST SEMESTER', academicYear: '2023' },
+  // Add more courses as needed with semester and academicYear properties
 ];
 
-const GradeReport = ({ grades }) => (
-  <div>
-    <h2>Grade Report</h2>
-    <ul>
-      {grades.map((grade, index) => (
-        <li key={index}>
-          {`${grade.course}: ${grade.grade}`}
-        </li>
-      ))}
-    </ul>
-  </div>
-);
+const initialGradesData = [
+  // ... (your existing gradesData)
+  { course: 'Science', grade: 'B', semester: 'Fall', academicYear: '2023' },
+  { course: 'Art History', grade: 'A', semester: 'Spring', academicYear: '2022' },
+  { course: 'Literature and Writing', grade: 'B', semester: 'Spring', academicYear: '2022' },
+  { course: 'Environmental Science', grade: 'A-', semester: 'Fall', academicYear: '2022' },
+  { course: 'Music Theory', grade: 'B+', semester: 'Fall', academicYear: '2022' },
+  { course: 'Psychology', grade: 'A', semester: 'Spring', academicYear: '2023' },
+  // Add more courses of your choice
+  { course: 'Chemistry', grade: 'B', semester: 'Spring', academicYear: '2023' },
+  { course: 'Political Science', grade: 'A-', semester: 'Fall', academicYear: '2022' },
+  { course: 'History of Art and Architecture', grade: 'A', semester: 'Fall', academicYear: '2022' },
+  { course: 'Digital Marketing', grade: 'B+', semester: 'Spring', academicYear: '2023' },
+  { course: 'Human Anatomy', grade: 'A+', semester: 'Spring', academicYear: '2023' },
+];
 
-const MissingGradeForm = () => (
-  <div>
-    <h2>Missing Grade Form</h2>
-    {/* Add your form elements here */}
-  </div>
-);
+const GradeReport = ({ grades, filter, setFilter }) => {
+  // Get unique semesters and academic years from grades
+  const semesters = Array.from(new Set(grades.map((grade) => grade.semester)));
+  const academicYears = Array.from(new Set(grades.map((grade) => grade.academicYear)));
+
+  // Filter grades based on selected semester or academic year
+  const filteredGrades = grades.filter(
+    (grade) => filter === 'All' || grade.semester === filter || grade.academicYear === filter
+  );
+
+  return (
+    <div>
+      <h2>Grade Report</h2>
+      <div>
+        <label>
+          Filter by Semester or Academic Year:
+          <select value={filter} onChange={(e) => setFilter(e.target.value)}>
+            <option value="All">All</option>
+            {semesters.map((semester, index) => (
+              <option key={`semester-${index}`} value={semester}>
+                {`Semester ${semester}`}
+              </option>
+            ))}
+            {academicYears.map((year, index) => (
+              <option key={`year-${index}`} value={year}>
+                {`Academic Year ${year}`}
+              </option>
+            ))}
+          </select>
+        </label>
+      </div>
+      <ul>
+        {filteredGrades.map((grade, index) => (
+          <li key={index}>
+            {`${grade.course}: ${grade.grade} - ${grade.semester}, ${grade.academicYear}`}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const MissingGradeForm = ({ onClose }) => {
+  const [missingCourse, setMissingCourse] = useState('');
+
+  const handleSubmit = () => {
+    // Add logic to handle missing grade submission
+    console.log(`Missing grade reported for ${missingCourse}`);
+    // Reset form field
+    setMissingCourse('');
+    // Close the form
+    onClose();
+  };
+
+  return (
+    <div>
+      <h2>Missing Grade Form</h2>
+      <label>
+        Course:
+        <input type="text" value={missingCourse} onChange={(e) => setMissingCourse(e.target.value)} />
+      </label>
+      <br />
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+  );
+};
 
 const InstructorContact = () => (
   <div>
@@ -44,12 +110,15 @@ const App = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [grades, setGrades] = useState([]);
+  const [showMissingGradeForm, setShowMissingGradeForm] = useState(false);
+  const [showGradeReport, setShowGradeReport] = useState(false);
+  const [filter, setFilter] = useState('All');
 
   const handleLogin = () => {
     // Simulate authentication
     if (username === fakeUserData.username && password === fakeUserData.password) {
       setLoggedIn(true);
-      setGrades(fakeGradesData);
+      setGrades(initialGradesData);
     } else {
       alert('Invalid credentials');
     }
@@ -60,11 +129,33 @@ const App = () => {
     setGrades([]);
     setUsername('');
     setPassword('');
+    setShowMissingGradeForm(false);
+    setShowGradeReport(false);
   };
 
   const checkMissingGrades = () => {
     return grades.some((grade) => grade.grade === 'Missing');
   };
+
+  const handleMissingGradeClick = () => {
+    setShowMissingGradeForm(true);
+    setShowGradeReport(false);
+  };
+
+  const handleGradeReportClick = () => {
+    setShowGradeReport(true);
+    setShowMissingGradeForm(false);
+  };
+
+  const handleCloseForm = () => {
+    setShowMissingGradeForm(false);
+    setShowGradeReport(false);
+  };
+
+  // New list for filtered courses based on semester or academic year
+  const filteredCourses = grades.filter(
+    (course) => showGradeReport && (course.semester === filter || course.academicYear === filter)
+  );
 
   return (
     <div>
@@ -85,7 +176,7 @@ const App = () => {
         </div>
       ) : (
         <div>
-          <h2>Welcome, {username}!</h2>
+          <h2>WELCOME TO YOUR DASHBOARD, {username}!</h2>
           {checkMissingGrades() && (
             <div style={{ color: 'red' }}>
               <p>Alert: You have missing grades. Please check your grades!</p>
@@ -101,16 +192,29 @@ const App = () => {
             ))}
           </ul>
 
+          <h3>Filtered Courses</h3>
+          <ul>
+            {filteredCourses.map((course, index) => (
+              <li key={index}>
+                {`${course.course}: ${course.grade} - ${course.semester}, ${course.academicYear}`}
+              </li>
+            ))}
+          </ul>
+
           <button onClick={handleLogout}>Logout</button>
 
           {/* Navigation links */}
           <nav>
             <ul>
               <li>
-                <a href="#">Grade Report</a>
+                <a href="#" onClick={handleGradeReportClick}>
+                  Grade Report
+                </a>
               </li>
               <li>
-                <a href="#">Missing Grade Form</a>
+                <a href="#" onClick={handleMissingGradeClick}>
+                  Missing Grade Form
+                </a>
               </li>
               <li>
                 <a href="#">Instructor Contact</a>
@@ -119,9 +223,8 @@ const App = () => {
           </nav>
 
           {/* Content based on navigation */}
-          {/* You can use a switch or conditional rendering based on the selected page */}
-          {/* For simplicity, I'll show GradeReport by default */}
-          <GradeReport grades={grades} />
+          {showGradeReport && <GradeReport grades={grades} filter={filter} setFilter={setFilter} />}
+          {showMissingGradeForm && <MissingGradeForm onClose={handleCloseForm} />}
         </div>
       )}
     </div>
