df9283daabe865b15342bcacd5c940aa86f53ee4
NanaAmpomah <nanaampomaharhin@gmail.com>
2023-12-11 19:05:14 +0100

    removed the import statement for the left sidebar

--- grading-website/src/pages/Login.js
+++ grading-website/src/pages/Login.js
@@ -1,44 +1,40 @@
-import React from 'react';
-import Navbar from '../components/Navbar';
-import HeroSection from '../components/HeroSection';
-import LeftSidebar from '../components/LeftSidebar';
+import React from "react";
+import Navbar from "../components/Navbar";
+import HeroSection from "../components/HeroSection";
 
 function Login() {
- 
-  const backgroundImage=(
+  const backgroundImage = (
     <>
-    <img src={process.env.PUBLIC_URL + './images/login-image.jpg'} />
+      <img src={process.env.PUBLIC_URL + "./images/login-image.jpg"} />
     </>
-  )
- 
-  const pageContent=(
+  );
+
+  const pageContent = (
     <>
-    <div className='student-login-details'>
-<div className='student-id-container'>
-    <input type='text' placeholder='Student ID ' required='required' />
-    <div>Please fill out this field</div>
-   </div>
-   <div className='student-pin-container'>
-    <input type='text'placeholder='Pin' required='required'  />
-    <div>Please fill out this field</div>
-   </div>
-    </div>
-    <div className='submit-button-container'>
-      <button type='submit'>Login</button>
-    </div>
+      <div className="student-login-details">
+        <div className="student-id-container">
+          <input type="text" placeholder="Student ID " required="required" />
+          <div>Please fill out this field</div>
+        </div>
+        <div className="student-pin-container">
+          <input type="text" placeholder="Pin" required="required" />
+          <div>Please fill out this field</div>
+        </div>
+      </div>
+      <div className="submit-button-container">
+        <button type="submit">Login</button>
+      </div>
     </>
-   
-  )
+  );
   return (
     <>
- <Navbar />
-    <HeroSection
-    backgroundImage={backgroundImage}  pageContent={pageContent}
-     />
-     <LeftSidebar />
-     
+      <Navbar />
+      <HeroSection
+        backgroundImage={backgroundImage}
+        pageContent={pageContent}
+      />
     </>
-  )
+  );
 }
 
-export default Login
+export default Login;
