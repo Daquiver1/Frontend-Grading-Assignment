5509ab9474ffb20cc41955dfbc6a79c5880ca036
Nana-king-netizen <otengnanayaw354@gmail.com>
2023-12-12 23:25:44 +0000

    update on contacts page

--- assignment/src/components/Contacts.jsx
+++ assignment/src/components/Contacts.jsx
@@ -1,29 +1,6 @@
 
 const Contact = () => {
 
-const lecturers =[
-  {
-   course:"DCIT201",
-   name:"Mr.Peter Tackie",
-   email:"petertk@gmail.com"},
-
-   {
-   course:"DCIT203",
-   name:"Mr.Abdullai Aziz",
-   email:""
-  },
-
-  {
-   course:"DCIT207",
-   name:"Peter Sasu",
-   email:"psasu542@gmail.com"},
-
-  {
-    course:"DCIT209",
-    name:"Bernard Agyekum",
-    email:"agyekumb@gmail.com"
-  }
-];
 
     return(
       <>
@@ -36,7 +13,8 @@ const lecturers =[
        
         </div>
 
-        <div className="flex flex-row">
+        <div className="flex flex-row justify-between">
+
         <div className=" bg-gray-300 text-blue-700 font-sans text-semibold rounded-lg shadow-xl min-h-[50px] ml-5 mt-10">
       <ul className="font-bold p-[15px] align-middle">
         <li className="text-center">DCIT 201</li>
@@ -44,8 +22,9 @@ const lecturers =[
         <li>Email: <span className="text-black">parmah54@gmail.com </span> </li>
         <li>Tel: <span className="text-black">(+233) 248-147-225 </span> </li>
       </ul>
-
+     
         </div>
+
         <div className=" bg-gray-300 text-blue-700 font-sans text-semibold rounded-lg shadow-xl min-h-[50px] mt-10 ml-5">
         <ul className="font-bold p-[15px] align-middle">
         <li className="text-center">DCIT 203</li>
@@ -54,6 +33,44 @@ const lecturers =[
         <li>Tel: <span className="text-black">(+233) 530-242-985 </span> </li>
       </ul>
         </div>
+
+        <div className=" bg-gray-300 text-blue-700 font-sans text-semibold rounded-lg shadow-xl min-h-[50px] mt-10 ml-5 mr-5">
+        <ul className="font-bold p-[15px] align-middle">
+        <li className="text-center">DCIT 205</li>
+        <li>Instructor: <span className="text-black">Mr.Michael Soli</span></li>
+        <li>Email: <span className="text-black">michaela450@gmail.com </span> </li>
+        <li>Tel: <span className="text-black">(+233) 241-638-173 </span> </li>
+      </ul>
+        </div>
+
+        
+
+        
+        </div>
+
+        <div className="flex justify-between">
+        <ul className="font-bold p-[15px] bg-gray-300 text-blue-700 font-sans text-semibold rounded-lg shadow-xl min-h-[50px] ml-5 mt-20">
+        <li className="text-center">DCIT 207</li>
+        <li>Instructor: <span className="text-black">Mr.Amoah Danso</span></li>
+        <li>Email: <span className="text-black">damoah81@gmail.com </span> </li>
+        <li>Tel: <span className="text-black">(+233) 241-638-173 </span> </li>
+      </ul>
+
+      <ul className="font-bold p-[15px] bg-gray-300 text-blue-700 font-sans text-semibold rounded-lg shadow-xl min-h-[50px] ml-5 mt-20">
+        <li className="text-center">DCIT 209</li>
+        <li>Instructor: <span className="text-black">Mr.Bernard Gyansah</span></li>
+        <li>Email: <span className="text-black">bernardgya11@gmail.com </span> </li>
+        <li>Tel: <span className="text-black">(+233) 512-054-371 </span> </li>
+      </ul>
+
+      <ul className="font-bold p-[15px] bg-gray-300 text-blue-700 font-sans text-semibold rounded-lg shadow-xl min-h-[50px] ml-5 mr-5 mt-20">
+        <li className="text-center">CBAS 210</li>
+        <li>Instructor: <span className="text-black">Mr.Prince Sakyi</span></li>
+        <li>Email: <span className="text-black">sakyipri137@gmail.com </span> </li>
+        <li>Tel: <span className="text-black">(+233) 243-816-003 </span> </li>
+      </ul>
+
+
         </div>
 
 
