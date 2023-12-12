7dfdc1e6ea24cfcddb16697952ff9f7c81e7a991
NanaOji <akpaabe@gmail.com>
2023-12-12 01:33:32 +0000

    loginstart

--- my-react/src/Components/Login/Login.js
+++ my-react/src/Components/Login/Login.js
@@ -1,12 +1,40 @@
-import login_icon from '../images/blacklogin.jpg'
+import backgroundimage from '../images/Legon University, Accra - Ghana.jpeg'
 
 
 const Login = () => {
     return (
-
-        <div>
-            login here
+        
+<div>
+<div className=' h-[100vh] flex justify-center items-center bg-cover '>
+<div>
+        <div className=''>
+            <h1>Login</h1>
+            <div>
+            <input type='email' />
+            <label htmlFor=''> Your Email</label>
         </div>
+       
+        <div className=''>
+            <input type='email' />
+            <label htmlFor=''> Your Email</label>
+        </div>
+        </div>
+        <div className=''>
+            <input type='checkbox' name="" id="" />
+            <label htmlFor=''> Your Email</label>
+        </div>
+        <span>Forgot Password</span>
+
+
+        
+        </div>
+</div>
+
+
+ <div className=''  >
+      <img className='bg-cover bg-no-repeat bg-center h-full w-full' src={backgroundimage} alt="" /> 
+        </div>
+</div>
      );
 }
  
