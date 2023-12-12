87e208aeecf6d2e70437ca58f85f3203ab1b24c1
Caleb Tetteh <calebtetteh65@gmail.com>
2023-12-12 01:16:35 +0000

    made more changes

--- assignment_work/src/App.js
+++ assignment_work/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import {Route, Routes} from 'react-router-dom';
 import LoginPage from './Components/LoginPage/LoginPage';
 import Homepage from './Components/homepage/homepage';
+import Form from './Components/missing_grade/form';
 function App() {
   return (
     <Routes>
       <Route>
       <Route path="*" element={<Homepage />} />
       <Route path="/loginpage" element={<LoginPage />} />
+      <Route path="/form" element={<Form />} />
       </Route>
     </Routes>
   );
