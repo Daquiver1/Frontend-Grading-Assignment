05a6d126b6fc86d71ae2c6cc225be6d5b453543f
KTTfx <nsromansarko@gmail.com>
2023-12-12 23:42:13 +0000

    building the instructor contact page

--- frontend-grading/src/App.js
+++ frontend-grading/src/App.js
@@ -5,6 +5,8 @@ import LandingPage from './components/LandingPage';
 import LoginPage from './components/LoginPage';
 import DashboardPage from './components/Dashboard';
 import GradeReportPage from './components/GradeReport';
+import ContactInstructor from './components/ContactInstructor';
+import MissingGradeForm from './components/MissingGradeForm';
 
 const App = () => {
   return (
@@ -14,6 +16,8 @@ const App = () => {
         <Route path="/login" component={LoginPage} />
         <Route path="/dashboard" component={DashboardPage} />
         <Route path="/grade-report" component={GradeReportPage} />
+        <Route path="/contact-instructor" component={ContactInstructor} />
+        <Route path="/missing-grade-form" component={MissingGradeForm} />
         {/* Add routes for other pages */}
       </Switch>
     </Router>
--- frontend-grading/src/components/ContactInstructor.jsx
+++ frontend-grading/src/components/ContactInstructor.jsx
@@ -1,28 +1,77 @@
-// ContactInstructor.js
-import React from 'react';
+// ContactInstructor.jsx
+import React, { useState } from 'react';
+import './ContactInstructor.css';
 
 const ContactInstructor = () => {
-  // Sample data (replace with actual data)
-  const instructors = [
-    { name: 'Dr. Smith', email: 'drsmith@example.com', officeHours: 'Monday 2-4 PM' },
-    { name: 'Prof. Johnson', email: 'profjohnson@example.com', officeHours: 'Wednesday 10 AM-12 PM' },
-    // Add more instructors as needed
-  ];
+  const [instructors] = useState([
+    { id: 1, name: 'Dr. Smith', email: 'dr.smith@example.com', workingTime: 'Mon-Fri, 9 AM - 5 PM' },
+    { id: 2, name: 'Prof. Johnson', email: 'prof.johnson@example.com', workingTime: 'Tue-Sat, 10 AM - 6 PM' },
+    { id: 3, name: 'Dr. Brown', email: 'dr.brown@example.com', workingTime: 'Mon-Wed, 8 AM - 4 PM' },
+    { id: 4, name: 'Prof. Davis', email: 'prof.davis@example.com', workingTime: 'Thu-Sat, 11 AM - 7 PM' },
+  ]);
+
+  const [message, setMessage] = useState('');
+  const [selectedInstructor, setSelectedInstructor] = useState(null);
+
+  const handleContact = () => {
+    if (selectedInstructor) {
+      // Implement your logic to handle contacting the instructor
+      console.log(`Contacting ${selectedInstructor.name} at ${selectedInstructor.email}`);
+      console.log(`Message: ${message}`);
+      // Reset form fields after submitting the message
+      setSelectedInstructor(null);
+      setMessage('');
+    }
+  };
 
   return (
     <div className="contact-instructor-container">
-      <h1>Contact Instructor</h1>
-      <div className="instructor-list">
-        {instructors.map((instructor, index) => (
-          <div key={index} className="instructor-info">
-            <h2>{instructor.name}</h2>
-            <p>Email: {instructor.email}</p>
-            <p>Office Hours: {instructor.officeHours}</p>
-            {/* Add course information or other details if needed */}
-          </div>
-        ))}
+      <h1>Contact Instructors</h1>
+      <div className="instructors-table-container">
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Working Time</th>
+            </tr>
+          </thead>
+          <tbody>
+            {instructors.map((instructor) => (
+              <tr key={instructor.id} onClick={() => setSelectedInstructor(instructor)}>
+                <td>{instructor.id}</td>
+                <td>{instructor.name}</td>
+                <td>{instructor.email}</td>
+                <td>{instructor.workingTime}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
-      {/* Add email form and other elements as needed */}
+
+      {selectedInstructor && (
+        <div className="message-form">
+          <h2>Send a Message to {selectedInstructor.name}</h2>
+          <form>
+            <label>
+              Your Message:
+              <textarea
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                placeholder="Type your message here..."
+              />
+            </label>
+            <button type="button" onClick={handleContact}>
+              Send Message
+            </button>
+          </form>
+        </div>
+      )}
+
+      <div className="additional-content">
+  
+      </div>
     </div>
   );
 };
