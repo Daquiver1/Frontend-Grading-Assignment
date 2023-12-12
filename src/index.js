5a78c8e6e66165553a26e7c3e09448d9c684abe7
Gabby-Techhub <gkaquainoo@st.ug.edu.gh>
2023-12-12 14:06:57 +0000

    Created the component header and footer and updated the index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// Bootstrap Bundle Css
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
