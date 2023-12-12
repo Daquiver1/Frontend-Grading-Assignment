2e9a79ec4ec36aad4ccae9bad65c321d3c02699d
Afful Nana Kwasi <affulnana04@gmail.com>
2023-12-12 13:02:40 +0000

    added tailwind to project

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,10 +1,17 @@
 
 
 function App() {
-
+  const name = 'Nana kwasi';
+  const age = '18';
   return (
     <>
-      <h1>hello world!</h1>      
+      <h1>hello world!</h1>   
+      <p>{name} is {age} years old</p>   
+     
+      <h1 className="text-3xl text-gray-400 font-bold underline">
+        Hello world!
+      </h1>
+
     </>
   )
 }
