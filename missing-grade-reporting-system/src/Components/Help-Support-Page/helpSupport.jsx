f553573b4b04db710e4cb07545e01957b7769f64
kev-69 <bismarkobuobi19@gmail.com>
2023-12-12 20:00:46 +0000

    Updates in the Help-Support page

--- missing-grade-reporting-system/src/Components/Help-Support-Page/helpSupport.jsx
+++ missing-grade-reporting-system/src/Components/Help-Support-Page/helpSupport.jsx
@@ -1,14 +1,43 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './helpSupport.css'
 
+import UserInfo from '../User-Info/userInfo'; 
+import HelpSupportNavBar from './supportNavBar.jsx';
+import Footer from '../Footer/footer';
+
 const HelpSupport = () => {
+  const [grades, setGrades] = useState([]); 
+  const [userData, setUserData] = useState({
+    studentName: 'Bismark Obuobi',
+    programOffering: 'BSc Information Technology',
+    level: '200',
+    studentId: '11146250',
+    missingGradesCount: 3,
+  });
+
   useEffect(() => {
     document.title = 'Help & Support';
   }, []);
 
   return (
     <div>
-      {/* Your component content goes here */}
+      <HelpSupportNavBar />
+      <UserInfo
+        studentName={userData.studentName}
+        programOffering={userData.programOffering}
+        studentId={userData.studentId}
+        level={userData.level}
+        missingGradesCount={userData.missingGradesCount}
+      />
+
+      <section id='support-section'>
+        <div>
+          <h1>Hello my name is Kevlar</h1>
+        </div>
+
+      </section>
+
+      <Footer />
     </div>
   )
 }
