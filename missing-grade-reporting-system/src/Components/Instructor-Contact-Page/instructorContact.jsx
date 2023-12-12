d44b4cf68702dc3b2d7d787c333120c1ed3bb757
kev-69 <bismarkobuobi19@gmail.com>
2023-12-12 20:02:53 +0000

    minnor edits

--- missing-grade-reporting-system/src/Components/Instructor-Contact-Page/instructorContact.jsx
+++ missing-grade-reporting-system/src/Components/Instructor-Contact-Page/instructorContact.jsx
@@ -1,14 +1,37 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './instructorContact.css';
+import ContactNavBar from './contactNavBar.jsx';
+
+import UserInfo from '../User-Info/userInfo'; 
+import Footer from '../Footer/footer';
 
 const Contacts = () => {
-  useEffect(() => {
+  const [grades, setGrades] = useState([]); 
+  const [userData, setUserData] = useState({
+    studentName: 'Bismark Obuobi',
+    programOffering: 'BSc Information Technology',
+    level: '200',
+    studentId: '11146250',
+    missingGradesCount: 3,
+  });
+
+  useEffect(() => { 
     document.title = 'Contact Instructor';
   }, []);
 
   return (
     <div>
+      <ContactNavBar />
+      <UserInfo
+        studentName={userData.studentName}
+        programOffering={userData.programOffering}
+        studentId={userData.studentId}
+        level={userData.level}
+        missingGradesCount={userData.missingGradesCount}
+      />
+
       {/* Your component content goes here */}
+      <Footer />
     </div>
   );
 };
