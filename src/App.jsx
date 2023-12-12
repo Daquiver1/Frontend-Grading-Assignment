6e0cf987b72dac9a98fd246082948f16d43c34ab
joyAj2 <joyejeh0@gmail.com>
2023-12-12 20:45:48 +0000

    App.jsx file done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+// Import Router................
+import Router from './Router/Router'
+
+export default function App() {
+  return (
+    <>
+     <Router />
+    </>
+  )
+}
+
