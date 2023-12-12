5621395b6d5c3a079013e4673e640a2cad3db5bb
Mubarak Wumpini Abdul-Rashid <mubarakrashid101@gmail.com>
2023-12-12 13:01:36 +0000

    git files

--- missing-grades/src/App.js
+++ missing-grades/src/App.js
@@ -2,17 +2,14 @@ import './App.css';
 import landingpage from './landingpage';
 
 function App() {
-  return <div className="App">Hello world</div>
 
 
 
-  /*
   return (
     <div className="App">
       <landingpage />
     </div>
   );
-  */
 }
 
 export default App;
