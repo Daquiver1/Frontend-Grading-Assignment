a089b800368b545b6a8023755e1c692edbbc6fe4
Mubarak Wumpini Abdul-Rashid <mubarakrashid101@gmail.com>
2023-12-12 05:55:20 +0000

    created new web app with one page for now

---
+++ missing-grades/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import landingpage from './landingpage';
+
+function App() {
+  return <div className="App">Hello world</div>
+
+
+
+  /*
+  return (
+    <div className="App">
+      <landingpage />
+    </div>
+  );
+  */
+}
+
+export default App;
