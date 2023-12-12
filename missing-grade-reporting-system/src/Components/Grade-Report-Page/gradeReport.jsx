03c907698134cd2a4cdcde66cf0a74202d33a25c
kev-69 <bismarkobuobi19@gmail.com>
2023-12-12 19:58:24 +0000

    Minnor changes in the page

--- missing-grade-reporting-system/src/Components/Grade-Report-Page/gradeReport.jsx
+++ missing-grade-reporting-system/src/Components/Grade-Report-Page/gradeReport.jsx
@@ -1,14 +1,42 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './gradeReport.css';
 
+import GradeReportNavBar from './gradeReportNavBar.jsx';
+import UserInfo from '../User-Info/userInfo'; 
+import Footer from '../Footer/footer';
+
+
 const GradeReport = () => {
+  const [grades, setGrades] = useState([]); 
+  const [userData, setUserData] = useState({
+    studentName: 'Bismark Obuobi',
+    programOffering: 'BSc Information Technology',
+    level: '200',
+    studentId: '11146250',
+    missingGradesCount: 3,
+  });
+
   useEffect(() => {
     document.title = 'Grade Report Page';
   }, []);
 
   return (
     <div>
-      {/* Your component content goes here */}
+      <GradeReportNavBar />
+      <UserInfo
+        studentName={userData.studentName}
+        programOffering={userData.programOffering}
+        studentId={userData.studentId}
+        level={userData.level}
+        missingGradesCount={userData.missingGradesCount}
+      />
+      <section>
+        <div></div>
+        <div></div>
+        <div></div>
+      </section>
+
+      <Footer />
     </div>
   );
 };
