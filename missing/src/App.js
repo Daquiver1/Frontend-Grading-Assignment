9d5afd19642b3da8bd1d8f2aa421693f2d873afd
Nana-Kay11 <knarthur002@st.ug.edu.gh>
2023-12-12 18:36:56 +0000

    Initialize React app

---
+++ missing/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from "./Pages-And-Components/Navigation-Bar/navbar";
+
+function App() {
+     return (
+      <div>
+        <Navbar />
+      </div>
+     );
+}
+
+export default App;
