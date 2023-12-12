ddcb8bf4a36d805a7e97123336678cd2af46fd58
kev-69 <bismarkobuobi19@gmail.com>
2023-12-12 20:27:42 +0000

    Tested out the code

--- missing-grade-reporting-system/src/App.js
+++ missing-grade-reporting-system/src/App.js
@@ -1,17 +1,22 @@
+import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route, } from 'react-router-dom';
+
 import Overview from './Components/Overview-Page/overviewPage.jsx';
 import Login from './Components/Login-Page/login.jsx';
-import NavBar from './Components/Nav-Bar/navBar.jsx';
 import Dashboard from './Components/Dashboard-Page/dashboardPage.jsx';
-import GradesReport from './Components/Grade-Report-Page/gradeReport.jsx';
+import MissingGradeForm from './Components/Missing-Grade-Form-Page/missingGradeForm.jsx';
+import GradeReport from './Components/Grade-Report-Page/gradeReport.jsx';
 import HelpAndSupport from './Components/Help-Support-Page/helpSupport.jsx';
 import Contacts from './Components/Instructor-Contact-Page/instructorContact.jsx';
-import Footer from './Components/Footer/footer.jsx';
 
 function App() {
+  
   return (
+
     <div>
-      <Overview />
+      <Dashboard />
     </div>
+
   );
 }
 
