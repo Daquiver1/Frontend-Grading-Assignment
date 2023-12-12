e926bc71391751b314846f4aba0263ab0a6850cd
Maxwellkorsah <maxwellenninful@gmail.com>
2023-12-12 01:41:28 +0000

    addition of herosection.jsx

--- my-react/src/components/Home Page/Homepage.jsx
+++ my-react/src/components/Home Page/Homepage.jsx
@@ -1,7 +1,19 @@
+import Footer from "../Footer"
+import Navbar from "../Navbar"
+import hero from "../hero"
+
+
+
+
+
 const Homepage = () => {
     return (
         <div className="">
-        
+            {/*navbar*/}
+            <header>
+                <Navbar/>
+            </header>
+         
         </div>
     );
 
