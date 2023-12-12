248b2b71d90059e6e248f723d5405fbc6e63c462
muizbello <bellomuiz78+1@gmail.com>
2023-12-12 10:58:42 +0000

    Landing page fdescription card component

--- src/Components/landing-cards.jsx
+++ src/Components/landing-cards.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Landing_cards = () => {
     return (
-        <div className="flex" mt-9>
-            <div class="max-w-sm mr-3 ml-4 mt-9 bg-white rounded overflow-hidden shadow-lg">
-                <img class="w-full h-48 object-cover" src="https://placekitten.com/640/360" alt="Card Image" />
+        <div className="flex flex-wrap mt-3">
+            <div class="max-w-sm mr-3 ml-4 mt-9 bg-white rounded overflow-hidden shadow-lg hover:shadow-2xl">
+                <img class="w-full h-48 object-cover" src="/Media_Files/gradereport.jpg" alt="Card Image" />
                 <div class="px-5 py-3">
                 <div class="font-bold text-xl mb-2">Grade Report Page</div>
                  <p class="text-gray-700 text-base">
@@ -12,8 +12,27 @@ const Landing_cards = () => {
                  </p>
                 </div>
             </div>
-            <div class="max-w-sm mr-4 ml-5 mt-9 bg-white rounded overflow-hidden shadow-lg">
-                <img class="w-full h-48 object-cover" src="https://placekitten.com/640/360" alt="Card Image" />
+            <div class="w-[400px] mt-9 mr-4 ml-5 bg-white rounded overflow-hidden shadow-lg hover:shadow-2xl">
+                <img class="w-full h-48 object-cover" src="/Media_Files/dashboard.jpg" alt="Card Image" />
+                <div class="px-5 py-3">
+                <div class="font-bold text-xl mb-2">Dashboard</div>
+                 <p class="text-gray-700 text-base">
+
+                 Welcome to your personalized dashboard! Here, you can access a comprehensive overview of your academic progress. Stay organized with timely alerts for any missing grades, ensuring that all coursework is accounted for. This feature helps you proactively address any gaps in your academic record.
+                 </p>
+                </div>
+            </div>
+            <div class="w-[400px] mt-9 mr-4 ml-[12px] bg-white rounded overflow-hidden shadow-lg hover:shadow-2xl">
+                <img class="w-full h-48 object-cover" src="/Media_Files/instructorcontact.jpg" alt="Card Image" />
+                <div class="px-5 py-3">
+                <div class="font-bold text-xl mb-2">Instructor Contact Page</div>
+                 <p class="text-gray-700 text-base">
+                    Whether you have questions, need clarifications, or seek guidance on resolving missing grade concerns,our Instructor Contact Page is your direct link to communicate with our dedicated teaching staff regarding any missing grades in your academic profile.
+                 </p>
+                </div>
+            </div>
+            <div class="w-[400px] mt-9 mr-4 ml-[220px] bg-white rounded overflow-hidden shadow-lg hover:shadow-2xl">
+                <img class="w-full h-48 object-cover" src="/Media_Files/missingform.jpg" alt="Card Image" />
                 <div class="px-5 py-3">
                 <div class="font-bold text-xl mb-2">Missing Grade Form</div>
                  <p class="text-gray-700 text-base">
@@ -21,12 +40,13 @@ const Landing_cards = () => {
                  </p>
                 </div>
             </div>
-            <div class="w-[400px] mt-9 mr-4 ml-5 bg-white rounded overflow-hidden shadow-lg">
-                <img class="w-full h-48 object-cover" src="https://placekitten.com/640/360" alt="Card Image" />
+            <div class="w-[400px] mt-9 mr-[10px] ml-[12px] bg-white rounded overflow-hidden shadow-lg hover:shadow-2xl">
+                <img class="w-full h-48 object-cover" src="/Media_Files/support.jpg" alt="Card Image" />
                 <div class="px-5 py-3">
-                <div class="font-bold text-xl mb-2">Instructor Contact Page</div>
+                <div class="font-bold text-xl mb-2">Help & Support</div>
                  <p class="text-gray-700 text-base">
-                    Whether you have questions, need clarifications, or seek guidance on resolving missing grade concerns,our Instructor Contact Page is your direct link to communicate with our dedicated teaching staff regarding any missing grades in your academic profile.
+
+                 Welcome to our Help and Support page! Our dedicated support team is ready to address your concerns and guide you through the process of resolving missing grade inquiries. Whether you need clarification on how to navigate the platform, or require technical assistance.
                  </p>
                 </div>
             </div>
