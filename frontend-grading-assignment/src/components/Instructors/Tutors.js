1f45c6b1819a9f6da019a6fd3f704733bdc8b7f4
bennydan7 <owusub243@gmail.com>
2023-12-12 12:12:28 +0000

    added instructor components

--- frontend-grading-assignment/src/components/Instructors/Tutors.js
+++ frontend-grading-assignment/src/components/Instructors/Tutors.js
@@ -11,7 +11,7 @@ import instructor7 from "../../assets/instructor6.jpg";
 class Team extends Component {
   render() {
     return (
-      <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div className="px-4 py-16 mx-auto  sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
         <div className="mx-auto mb-10 lg:max-w-xl sm:text-center">
           <p className="inline-block px-3 py-px mb-4 text-3xl font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
             Meet Our Instructors
