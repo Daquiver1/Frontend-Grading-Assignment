614c768ce17970d6ce8dbf310a959f640425f2c5
KekeliC-T <122158162+KekeliC-T@users.noreply.github.com>
2023-12-12 19:55:12 +0000

    last but one commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import LandingPage from './components/LandingPage';
+import LoginPage from './components/LoginPage';
+import DashboardPage from './components/DashboardPage';
+import GradeReportPage from './components/GradeReportPage';
+import MissingGradeFormPage from './components/MissingGradeFormPage';
+import HelpAndSupportPage from './components/HelpAndSupportPage';
+import InstructorContactPage from './components/InstructorContactPage';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/grade-report" element={<GradeReportPage />} />
+        <Route path="/missing-grade-form" element={<MissingGradeFormPage />} />
+        <Route path="/help-and-support" element={<HelpAndSupportPage />} />
+        <Route path="/instructor-contact" element={<InstructorContactPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
