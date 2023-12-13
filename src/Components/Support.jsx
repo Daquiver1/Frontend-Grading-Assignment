425fad6cc3e80655412195e6c791dafcb7e5d670
Joeboy77 <acheampongjoseph470@gmail.com>
2023-12-13 09:17:31 +0000

    Technical support component

---
+++ src/Components/Support.jsx
@@ -0,0 +1,19 @@
+
+function Support(){
+
+    return(
+        <>
+        <div className="general-head"><h2>Technical Support Form</h2></div>
+        <form action="#" className="technical">
+            <input type="text" placeholder="Name"/>
+            <input type="text" placeholder="Student ID" />
+            <input type="text" placeholder="Program" />
+            <input type="text" placeholder="Phone Number" />
+            <input type="email" name="#" id="email" placeholder="Student Email" />
+            <a><button>Submit</button></a>
+        </form><br /><br /><hr />
+        </>
+    );
+
+}
+export default Support
