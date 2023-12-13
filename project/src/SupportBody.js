8aadbec02afe4a7a7674282f8fed315e73121565
thesameakpalu <eakpalu008@st.ug.edu.gh>
2023-12-13 12:00:12 +0000

    modified and completed the help and support page

--- project/src/SupportBody.js
+++ project/src/SupportBody.js
@@ -20,10 +20,8 @@ const SupportBody = () =>{
                     <ReactBootStrap.Card.Body>
                         <ReactBootStrap.Card.Title>Getting Started</ReactBootStrap.Card.Title>
                         <ReactBootStrap.Card.Text>
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
+                        Get your help scout account in just 6 simple steps. <br /><a className="card-link">Read article</a>
                         </ReactBootStrap.Card.Text>
-                        <ReactBootStrap.Button variant="primary">Go somewhere</ReactBootStrap.Button>
                     </ReactBootStrap.Card.Body>
                     </ReactBootStrap.Card>
 
@@ -32,10 +30,9 @@ const SupportBody = () =>{
                     <ReactBootStrap.Card.Body>
                         <ReactBootStrap.Card.Title>Reset Password</ReactBootStrap.Card.Title>
                         <ReactBootStrap.Card.Text>
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
+                        Click the button below to initiate the password reset procedure. <br />
+                        <a className="card-link">Read article</a>
                         </ReactBootStrap.Card.Text>
-                        <ReactBootStrap.Button variant="primary">Go somewhere</ReactBootStrap.Button>
                     </ReactBootStrap.Card.Body>
                     </ReactBootStrap.Card>
 
@@ -45,57 +42,56 @@ const SupportBody = () =>{
                     <ReactBootStrap.Card.Body>
                         <ReactBootStrap.Card.Title> Describe Issue</ReactBootStrap.Card.Title>
                         <ReactBootStrap.Card.Text>
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
+                        Provide detailed information about the issue you're facing. <br />
+                        <a className="card-link">Read article</a> 
                         </ReactBootStrap.Card.Text>
-                        <ReactBootStrap.Button variant="primary">Go somewhere</ReactBootStrap.Button>
                     </ReactBootStrap.Card.Body>
                     </ReactBootStrap.Card>
             </div>
-
             <div className="accordion-container">
                 <div className="accord-holder">
-            <ReactBootStrap.Accordion flush>
+                  <p className="header">FAQS</p>
+            <ReactBootStrap.Accordion className="accordion" flush>
       <ReactBootStrap.Accordion.Item eventKey="0">
-        <ReactBootStrap.Accordion.Header>What is the purpose of the school grade reporting system?</ReactBootStrap.Accordion.Header>
+        <ReactBootStrap.Accordion.Header ><span className="accordion">What is the purpose of the school grade reporting system?</span></ReactBootStrap.Accordion.Header>
         <ReactBootStrap.Accordion.Body>
-        The system is designed to provide a transparent and efficient way for students, parents, and educators to access and understand academic performance data.
+        <span className="info">The system is designed to provide a transparent and efficient way for students, parents, and educators to access and understand academic performance data.</span>
         </ReactBootStrap.Accordion.Body>
       </ReactBootStrap.Accordion.Item>
       <ReactBootStrap.Accordion.Item eventKey="1">
-        <ReactBootStrap.Accordion.Header>Who has access to the grade reporting system?</ReactBootStrap.Accordion.Header>
+        <ReactBootStrap.Accordion.Header ><span className="accordion">Who has access to the grade reporting system?</span></ReactBootStrap.Accordion.Header>
         <ReactBootStrap.Accordion.Body>
-        Students and teachers will have access to the system. Each user type will have different levels of access based on their role.
+        <span className="info">Students and teachers will have access to the system. Each user type will have different levels of access based on their role.</span>
         </ReactBootStrap.Accordion.Body>
       </ReactBootStrap.Accordion.Item>
       <ReactBootStrap.Accordion.Item eventKey="2">
-        <ReactBootStrap.Accordion.Header>How do I create an account in the grade reporting system?</ReactBootStrap.Accordion.Header>
+        <ReactBootStrap.Accordion.Header ><span className="accordion">How do I create an account in the grade reporting system?</span></ReactBootStrap.Accordion.Header>
         <ReactBootStrap.Accordion.Body>
-          Accounts are automatically created fro new students once their admissions are approved. A student will need his or her ID and Pin provided by the Administration on the admission letters.
+        <span className="info">Accounts are automatically created fro new students once their admissions are approved. A student will need his or her ID and Pin provided by the Administration on the admission letters.</span>
         </ReactBootStrap.Accordion.Body>
       </ReactBootStrap.Accordion.Item>
       <ReactBootStrap.Accordion.Item eventKey="3">
-        <ReactBootStrap.Accordion.Header>Is there a way to communicate with teachers through the system?
+        <ReactBootStrap.Accordion.Header ><span className="accordion">Is there a way to communicate with teachers through the system?</span>
 
 </ReactBootStrap.Accordion.Header>
         <ReactBootStrap.Accordion.Body>
-          The grade reporting system includes Course instructor support feature that that students use to get the contact details of the course instructors for the respective courses.
+        <span className="info">The grade reporting system includes Course instructor support feature that that students use to get the contact details of the course instructors for the respective courses.</span>
         </ReactBootStrap.Accordion.Body>
       </ReactBootStrap.Accordion.Item>
       <ReactBootStrap.Accordion.Item eventKey="4">
-        <ReactBootStrap.Accordion.Header>What should I do if I notice an error in the grade report?
+        <ReactBootStrap.Accordion.Header ><span className="accordion">What should I do if I notice an error in the grade report?</span>
 
 </ReactBootStrap.Accordion.Header>
         <ReactBootStrap.Accordion.Body>
-        Users can contact the teacher or school administration to address any discrepancies or errors in the grade report.
+        <span className="info">Users can contact the teacher or school administration to address any discrepancies or errors in the grade report.</span>
         </ReactBootStrap.Accordion.Body>
       </ReactBootStrap.Accordion.Item>
       <ReactBootStrap.Accordion.Item eventKey="5">
-        <ReactBootStrap.Accordion.Header>I am having technical issues with the system. How can I get help?
+        <ReactBootStrap.Accordion.Header ><span className="accordion">I am having technical issues with the system. How can I get help?</span>
 
 </ReactBootStrap.Accordion.Header>
         <ReactBootStrap.Accordion.Body>
-        Users can reach out to the school's IT support or the system administrator for technical assistance.
+        <span className="info">Users can reach out to the school's IT support or the system administrator for technical assistance.</span>
         </ReactBootStrap.Accordion.Body>
       </ReactBootStrap.Accordion.Item>
     </ReactBootStrap.Accordion>
