0d2394865bd54cc8cc0bf6a5275a148f60dee493
JAYDEN2904 <osafojayden290405@gmail.com>
2023-12-13 11:57:18 +0000

    add

--- grade/src/Components/Navbar.jsx
+++ grade/src/Components/Navbar.jsx
@@ -3,12 +3,13 @@ import { Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import './Navbar.css'
 
 const AppHead = () => {
   return (
     <Navbar bg="primary" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand as={Link} to="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand as={Link} to="#home">Grade Book</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link as={Link} to="#home">Home</Nav.Link>
             <Nav.Link as={Link} to="#features">Features</Nav.Link>
