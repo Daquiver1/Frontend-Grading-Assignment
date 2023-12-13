b6be9e1acd64c2e40172d72fe675ea610b49f3cb
11194673_DCIT205_Assignment1 <aaninsin@st.ug.edu.gh>
2023-12-13 21:09:09 +0000

    finalised my navigation bar and applied some styling

--- src/App.tsx
+++ src/App.tsx
@@ -7,35 +7,40 @@ import InstructorContactPage from "./components/InstructorContactPage";
 import HelpSupportPage from "./components/HelpSupportPage";
 import React, { Component } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <div className="App">
-          <ul className="App-header">
-            <li>
+        <div>
+          <div className="App">
+            <div className="App-header">
               <Link to="/">Login</Link>
-            </li>
-            <li>
+            </div>
+            <div className="App-header">
               <Link to="/HomePage">Home page</Link>
-            </li>
-            <li>
+            </div>
+            <div className="App-header">
               <Link to="/DashboardPage">Dashboard</Link>
-            </li>
-            <li>
+            </div>
+            <div className="App-header">
               <Link to="/GradeReportPage">Grade Report Page</Link>
-            </li>
-            <li>
+            </div>
+            <div className="App-header">
+              {" "}
               <Link to="/MissingGradeForm">MissingGradeForm</Link>
-            </li>
-            <li>
+            </div>
+            <div className="App-header">
+              {" "}
               <Link to="/InstructorContactPage">Contact Instructor</Link>
-            </li>
-            <li>
+            </div>
+            <div className="App-header">
+              {" "}
               <Link to="/HelpSupportPage">Help and Support</Link>
-            </li>
-          </ul>
+            </div>
+          </div>
+
           <Routes>
             <Route exact path="/" element={<LoginPage />}></Route>
             <Route exact path="/HomePage" element={<HomePage />}></Route>
