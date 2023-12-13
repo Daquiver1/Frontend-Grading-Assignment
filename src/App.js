a9e60dbf27a042acac4f460743165ed2e923dbd1
muizbello <bellomuiz78+1@gmail.com>
2023-12-13 08:57:50 +0000

    Program file

--- src/App.js
+++ src/App.js
@@ -1,31 +1,57 @@
-import React from "react";
-import Landing_header from "./Components/landing-header";
-import Landing_cards from "./Components/landing-cards";
-import Landing_footer from "./Components/landing-footer";
-
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Routes, BrowserRouter } from 'react-router-dom';
+import Landingpage from './Components/Landingpage';
+import Login from './Components/Login';
+import Missing_gradeform from './Components/Missing_gradeform';
+import Gradereport from './Components/Gradereport';
+import Dashboard from './Components/Dashboard';
+import Help from './Components/Help';
+import Instructorcontact from './Components/Instructorcontact';
 
 
 function App() {
   return (
-    <div className="px-6 pt-1 shadow-lg">
-      <Landing_header/>
-      <div className="flex mt-5">
-        <img src="/Media_Files/landing-image.jpg" className="w-[620px] h-[390px] mt-4 rounded-lg shadow-lg" />
-        <div className="ml-5">
-          <p className="ml-3 -mt-[40px] font-bold text-[136px] text-amber-950" >Empower</p>
-          <p className="ml-3 -mt-[23px] font-bold text-7xl text-zinc-800 ">Your Academics.</p>
-          <p className="ml-3 mt-3 font-semibold text-xl">
-            Take control of your academic journey with our intuitive interface, ensuring that you have access to accurate and up-to-date information on your grades, as well as the capability to make complaints about perceived errors in your grades. Our system aims to address the common challenge of missing or unrecorded grades in a student's profile thus, empowering you to stay informed and proactive in their education.
-          </p>
-        </div>
+    <BrowserRouter>
+      <div id="route" className='grid'>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/Login">Log In</Link>
+            </li>
+            <li>
+              <Link to="/Landingpage">landingpage</Link>
+            </li>
+          </ul>
+        </nav>
+        <Link to = "/Missing_gradeform">
+          <button>Missing_gradeform</button>
+        </Link>
+        <Link to = "/Gradereport">
+          <button>gradereport</button>
+        </Link>
+        <Link to = "/Instructorcontact">
+          <button>Instructorcontact</button>
+        </Link>
+        <Link to = "/Dashboard">
+          <button>Dashboard</button>
+        </Link>
+        <Link to = "/Help">
+          <button>help</button>
+        </Link>
       </div>
-      <div>
-        <p className="text-6xl mt-7 -mb-2 text-center font-semibold">Our Features.</p>
-      </div>
-      <Landing_cards/>
-      <Landing_footer/>
-    </div>
+      <Routes>
+          <Route path="/Landingpage" element={<Landingpage />} />
+          <Route index element={<Landingpage/>} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Missing_gradeform" element={<Missing_gradeform />} />
+          <Route path="/Dashboard" element={<Dashboard />} />
+          <Route path="/Gradereport" element={<Gradereport />} />
+          <Route path="/Help" element={<Help/>} />
+          <Route path="/Instructorcontact" element={<Instructorcontact />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
