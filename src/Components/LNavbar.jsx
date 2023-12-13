c07b93ef00ca42bc927dff0d009e4553749c4ddd
muizbello <bellomuiz78+1@gmail.com>
2023-12-13 08:55:59 +0000

    Navbar component

---
+++ src/Components/LNavbar.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const LNavbar = () => {
+        return (
+            <div className="ml-[100px] mt-[6.7px]">
+                <nav className="flex justify-between">
+                <Link to= "/Dashboard"> 
+                    <div id="6667" className="ml-[10px] text-center text-[15pt] shadow-md p-1">
+                        <button>Dashboard</button>                      
+                    </div>
+                    </Link>
+                    <Link to= "/Gradereport">
+                    <div className="ml-[10px] text-[15pt] shadow-md p-1">
+                        <button>Grade Report</button>
+                    </div>
+                    </Link>
+                    <Link to = "/Instructorcontact">
+                    <div className="ml-[10px] text-[15pt] shadow-md p-1">
+                        <button>Contact Instructors</button> 
+                    </div>
+                    </Link>
+                    <Link to= "/Missing_gradeform">
+                    <div className="ml-[10px] text-[15pt] shadow-md p-1">
+                            <button>Missing Grade Form</button>  
+                    </div>
+                    </Link>
+                </nav>
+                
+            </div>
+        )
+        
+    }   
+export default LNavbar;
