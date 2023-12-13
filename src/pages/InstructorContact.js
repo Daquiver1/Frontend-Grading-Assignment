cbd8d8325a8d0eabaf2d516ba11792cb0e69f39b
Davelar1789 <dnalaryea003@st.ug.edu.gh>
2023-12-13 00:08:50 +0000

    time late

--- src/pages/InstructorContact.js
+++ src/pages/InstructorContact.js
@@ -33,9 +33,9 @@ function InstructorContact() {
 	</div>
 </div>
 <div className="card2-container2">
-	<span className="pro2">1</span>
+	<span className="pro2">2</span>
 	<img className="round2" src="https://randomuser.me/api/portraits/women/79.jpg" alt="user" />
-	<h3>Emily Johnson</h3>
+	<h3>Daniel Rodriguez</h3>
 	<h6>GHANA</h6>
 	<p>User interface designer and <br/> front-end developer</p>
 	<div className="buttons2">
@@ -47,6 +47,114 @@ function InstructorContact() {
 		</button>
 	</div>
 	<div className="skills2">
+		<h6>Skills</h6>
+		<ul>
+			<li>AJAX</li>
+			<li>Front End Development</li>
+			<li>HTML</li>
+			<li>CSS</li>
+			<li>JavaScript</li>
+			<li>React</li>
+			<li>Angular</li>
+		</ul>
+	</div>
+</div>
+<div className="card3-container3">
+	<span className="pro3">3</span>
+	<img className="round3" src="https://randomuser.me/api/portraits/women/79.jpg" alt="user" />
+	<h3>Emily Johnson</h3>
+	<h6>GHANA</h6>
+	<p>User interface designer and <br/> front-end developer</p>
+	<div className="buttons3">
+		<button className="primary3">
+			Send Email
+		</button>
+		<button className="primary3 ghost3">
+			Follow
+		</button>
+	</div>
+	<div className="skills3">
+		<h6>Skills</h6>
+		<ul>
+			<li>UI / UX</li>
+			<li>Front End Development</li>
+			<li>HTML</li>
+			<li>CSS</li>
+			<li>JavaScript</li>
+			<li>React</li>
+			<li>Node</li>
+		</ul>
+	</div>
+</div>
+<div className="card4-container4">
+	<span className="pro4">1</span>
+	<img className="round4" src="https://randomuser.me/api/portraits/women/79.jpg" alt="user" />
+	<h3>Emily Johnson</h3>
+	<h6>GHANA</h6>
+	<p>User interface designer and <br/> front-end developer</p>
+	<div className="buttons4">
+		<button className="primary4">
+			Send Email
+		</button>
+		<button className="primary4 ghost4">
+			Follow
+		</button>
+	</div>
+	<div className="skills4">
+		<h6>Skills</h6>
+		<ul>
+			<li>UI / UX</li>
+			<li>Front End Development</li>
+			<li>HTML</li>
+			<li>CSS</li>
+			<li>JavaScript</li>
+			<li>React</li>
+			<li>Node</li>
+		</ul>
+	</div>
+</div>
+<div className="card5-container5">
+	<span className="pro5">1</span>
+	<img className="round5" src="https://randomuser.me/api/portraits/women/79.jpg" alt="user" />
+	<h3>Emily Johnson</h3>
+	<h6>GHANA</h6>
+	<p>User interface designer and <br/> front-end developer</p>
+	<div className="buttons5">
+		<button className="primary5">
+			Send Email
+		</button>
+		<button className="primary5 ghost5">
+			Follow
+		</button>
+	</div>
+	<div className="skills5">
+		<h6>Skills</h6>
+		<ul>
+			<li>UI / UX</li>
+			<li>Front End Development</li>
+			<li>HTML</li>
+			<li>CSS</li>
+			<li>JavaScript</li>
+			<li>React</li>
+			<li>Node</li>
+		</ul>
+	</div>
+</div>
+<div className="card6-container6">
+	<span className="pro6">1</span>
+	<img className="round6" src="https://randomuser.me/api/portraits/women/79.jpg" alt="user" />
+	<h3>Emily Johnson</h3>
+	<h6>GHANA</h6>
+	<p>User interface designer and <br/> front-end developer</p>
+	<div className="buttons6">
+		<button className="primary6">
+			Send Email
+		</button>
+		<button className="primary6 ghost6">
+			Follow
+		</button>
+	</div>
+	<div className="skills6">
 		<h6>Skills</h6>
 		<ul>
 			<li>UI / UX</li>
