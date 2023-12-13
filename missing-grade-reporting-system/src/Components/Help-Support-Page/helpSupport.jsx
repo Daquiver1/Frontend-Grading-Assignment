7d53464c346e855fc25047e8d6032f430e1704a1
kev-69 <bismarkobuobi19@gmail.com>
2023-12-13 20:16:57 +0000

    Created the Help and Support page and added styling. also attached the nav bar and footer

--- missing-grade-reporting-system/src/Components/Help-Support-Page/helpSupport.jsx
+++ missing-grade-reporting-system/src/Components/Help-Support-Page/helpSupport.jsx
@@ -14,6 +14,24 @@ const HelpSupport = () => {
     studentId: '11146250',
     missingGradesCount: 3,
   });
+  const [showMoreContactHelp, setShowMoreContactHelp] = useState(false);
+  const [showMoreCourseHelp, setShowMoreCourseHelp] = useState(false);
+  const [showMoreGradesHelp, setShowMoreGradesHelp] = useState(false);
+
+  const handleSeeMoreContactHelp = (event) => {
+    event.preventDefault();
+    setShowMoreContactHelp(!showMoreContactHelp);
+  };
+
+  const handleSeeMoreCourseHelp = (event) => {
+    event.preventDefault();
+    setShowMoreCourseHelp(!showMoreCourseHelp);
+  };
+
+  const handleSeeMoreGradesHelp = (event) => {
+    event.preventDefault();
+    setShowMoreGradesHelp(!showMoreGradesHelp);
+  };
 
   useEffect(() => {
     document.title = 'Help & Support';
@@ -31,8 +49,64 @@ const HelpSupport = () => {
       />
 
       <section id='support-section'>
-        <div>
-          <h1>Hello my name is Kevlar</h1>
+        <div className='help-div'>
+          <h1>Find Help And Support</h1>
+          <div className="search-container">
+    <input type="text" placeholder="Search..." />
+    <span className="search-icon" role="img" aria-label="Search">&#128269;</span>
+  </div>
+        </div>
+        <div className='help-links'>
+          <h1><u>Suggested Help Links</u></h1>
+        </div>
+        <div className='quicklinks-div'>
+          <div>
+            <h3>Missing Grades</h3>
+            <p>How to spot a missing grade</p>
+            <p>How to report missing grades</p>
+            <p>Generals rules on how to report</p>
+            {showMoreGradesHelp && (
+              <div>
+                <p>Find lecturer's office</p>
+                <p>Not sure exam was submitted?</p>
+              </div>
+            )}
+            <p><a href="#" onClick={handleSeeMoreGradesHelp}>See more</a></p>
+          </div>
+          <div>
+            <h3>Contact Issues</h3>
+            <p>Problems on contacting instrustor</p>
+            <p>Find instructor's contact details</p>
+            <p>Not getting responds from instrustor after reporting</p>
+            
+            {showMoreContactHelp && (
+              <div>
+                <p>Alternatives</p>
+                <p>Schedule meeting with lecturer</p>
+              </div>
+            )}
+            <p><a href="#" onClick={handleSeeMoreContactHelp}>See more</a></p>
+          </div>
+          
+          <div>
+            <h3>Issues with Courses</h3>
+            <p>Missing course</p>
+            <p>Problems with registered courses</p>
+            {showMoreCourseHelp && (
+              <div>
+                <p>Missed course exam?</p>
+                <p>Resit Exam?</p>
+              </div>
+            )}
+            <p><a href="#" onClick={handleSeeMoreCourseHelp}>See more</a></p>
+          </div>
+        </div>
+
+        <div className='faqs-div'>
+        <h1>Frequently Asked Questions</h1>
+        <p>How long does my reported missing grades status change?</p>
+        <p>Does my report get to the lecturer/instructor directly?</p>
+        <p>What do I need to do after report a missing grade?</p>
         </div>
 
       </section>
