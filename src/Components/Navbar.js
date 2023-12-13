04d873f0def80571ec2b868fa2a328cf1edaf340
Naa Shidaa <rainaaaddo@gmail.com>
2023-12-13 10:30:36 +0000

    additional components

---
+++ src/Components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+
+      <Navbar color ="blue-gray" className='flex flex-wrap items-center justify-between gap-y-4 text-white'>
+          <ul className='flex flex-wrap items-center justify-between gap-y-4 text-white'  >
+              <li>Home</li> 
+              <li>Home</li>
+              <li>Home</li>
+              <li>Home</li>
+          </ul>
+  
+      </Navbar>
+    )
+  }
+  
+  export default Navbar;
