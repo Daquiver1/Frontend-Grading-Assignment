0dfa4e2ac60f44e21f0141873301502bd2e407c0
OseiEsther <Oseiamoakoesther2022@gmail.com>
2023-12-13 17:44:25 +0000

    Added dahboard layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
+import DashboardLayout from "./layout/dashboard";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="dashboard" element={<DashboardLayout />}></Route>
         <Route path="login" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
---
+++ src/layout/dashboard.jsx
@@ -0,0 +1,21 @@
+import SideBar from "../components/naviagtion/SideBar.jsx";
+
+function Dashboard() {
+  return (
+    <div className=" h-[100vh] w-full flex flex-row ">
+      <div className="sideBar_container active">
+        <SideBar  />
+      </div>
+      <div className="content_ px-3">
+        lewkfhweihfiohfoiwefjp9 ewfwefijweflijweoif ewifjewiofjowiejfoiewjf
+        ewifjoiwejfoiewjfoiewj lewkfhweihfiohfoiwefjp9 ewfwefijweflijweoif
+        ewifjewiofjowiejfoiewjf ewifjoiwejfoiewjfoiewj lewkfhweihfiohfoiwefjp9
+        ewfwefijweflijweoif ewifjewiofjowiejfoiewjf ewifjoiwejfoiewjfoiewj
+        lewkfhweihfiohfoiwefjp9 ewfwefijweflijweoif ewifjewiofjowiejfoiewjf
+        ewifjoiwejfoiewjfoiewj
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
