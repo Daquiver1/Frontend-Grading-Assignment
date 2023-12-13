71cc2545d14f821f9d9c8a486fc936c8fa78735c
Michael Perry Tettey <michaelperryt97@gmail.com>
2023-12-13 12:25:24 +0000

    Added student data

--- dcit-205-assignment/src/scripts/student-data.js
+++ dcit-205-assignment/src/scripts/student-data.js
@@ -1,8 +1,8 @@
+import studentImg from "../images/student.jpg";
 export const Student = {
   name: "Michael Perry Tettey",
   major: "Bsc.Computer Science",
   role: "Student",
-  image:
-    "https://pbs.twimg.com/profile_images/1728364037228933121/-yM3FBA5_400x400.jpg",
+  image: studentImg,
 };
 export default Student;
