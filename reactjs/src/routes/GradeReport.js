3c6ab4f221bfc40c6971b27204afe5907d74cf38
ErasKuKU <149342667+ErasKuKU@users.noreply.github.com>
2023-12-12 23:39:19 -0800

    added other pages

--- reactjs/src/routes/GradeReport.js
+++ reactjs/src/routes/GradeReport.js
@@ -1,19 +1,84 @@
-
-
-
-
-
-
+import React, { useState } from 'react';
+import './GradeReport.css';
 
 const GradeReport = () => {
-    return (
-        <div>
-        
-            
-                grade
-            
-        </div>
-        
-    );
-}
+  const mockupData = [
+    { semester: 'First Semester(Level 100)', courses: ['INTRODUCTION TO COMPUTER SCIENCE(DCIT 101)', 
+    'OFFICE PRODUCTIVITY TOOLS(DCIT 103)', 
+    'INTRODUCTION TO STATISTICS AND PROBABILITY(STAT 111)',
+    'ALGEBRA AND TRIGONOMETRY(MATH 121)',
+    'GEOMETRY I(MATH 123)', 
+    'CRITICAL THINKING(UGRC 250)'], 
+    grades: ['A', 'B', 'C', 'B', 'A', 'C'] },
+
+    
+    { semester: 'Second Semester(Level 100)', courses: ['PROGRAMMING I',
+     'DIGITAL AND LOGICAL CIRCUITS(DCIT 203)', 
+     'CALCULUS II(MATH 223)',
+     'COMPUTER DESIGN AND ARCHITECTURE(DCIT 207)',
+     'MULTIMEDIA AND WEB DESIGN(DCIT 205',
+     'ACADEMIC WRITING II(CBAS 210)'
+    ], 
+    grades: ['B', 'A', 'A', 'D', 'C', 'A'] },
+
+
+    { semester: 'Second Semester(Level 200)', courses: ['PROGRAMMING I',
+    'DIGITAL AND LOGICAL CIRCUITS(DCIT 203)', 
+    'CALCULUS II(MATH 223)',
+    'COMPUTER DESIGN AND ARCHITECTURE(DCIT 207)',
+    'MULTIMEDIA AND WEB DESIGN(DCIT 205',
+    'ACADEMIC WRITING II(CBAS 210)'
+   ], 
+   grades: ['B', 'A', 'A', 'D', 'C', 'A'] },
+
+
+  ];
+
+  const [selectedSemester, setSelectedSemester] = useState('');
+
+  const filteredData = selectedSemester
+    ? mockupData.filter((data) => data.semester === selectedSemester)
+    : mockupData;
+
+  return (
+    <div>
+
+<div className="grade-report-container">
+      <div className="filter-section">
+        <label>Semesters:</label>
+        <select
+          value={selectedSemester}
+          onChange={(e) => setSelectedSemester(e.target.value)}
+        >
+          <option value="">All Semesters</option>
+          <option value="First Semester(Level 100)">Semester 1</option>
+          <option value="Second Semester(Level 100)">Semester 2</option>
+          <option value="Second Semester(Level 200)">Semester 3</option>
+
+        </select>
+      </div>
+
+      <div className="grade-list">
+        {filteredData.map((data) => (
+          <div key={data.semester} className="semester-section">
+            <h3>{data.semester}</h3>
+            <ul>
+              {data.courses.map((course, index) => (
+                <li key={index}>
+                  <span className='course-name'>{course}:</span> <span className='grade'>{data.grades[index]}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
+    </div>
+
+    </div>
+    
+    
+   
+  );
+};
+
 export default GradeReport
--- reactjs/src/routes/MissingGrades.js
+++ reactjs/src/routes/MissingGrades.js
@@ -1,19 +1,84 @@
-
-
-
-
-
-
+import React, { useState } from 'react';
+import './MissingGrades.css'; // Import the CSS file for styling
 
 const MissingGrades = () => {
-    return (
-        <div>
-        
-            <div className="in-lineflex">
-                missing
-            </div>
+  const [formData, setFormData] = useState({
+    courseName: '',
+    instructorName: '',
+    expectedGrade: '',
+    explanation: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Simulate submission (you can replace this with actual API call)
+    console.log('Form Data Submitted:', formData);
+
+    // Display a confirmation message (you can replace this with a modal or redirect)
+    alert('Grade report submitted successfully!');
+  };
+
+  return (
+    <div className="missing-grade-form-container">
+      <h2>Missing Grade Report Form</h2>
+      <form onSubmit={handleSubmit} className="missing-grade-form">
+        <div className="form-group">
+          <label htmlFor="courseName">Course Name:</label>
+          <input
+            type="text"
+            id="courseName"
+            name="courseName"
+            value={formData.courseName}
+            onChange={handleChange}
+            required
+          />
         </div>
-        
-    );
-}
-export default MissingGrades
+
+        <div className="form-group">
+          <label htmlFor="instructorName">Instructor Name:</label>
+          <input
+            type="text"
+            id="instructorName"
+            name="instructorName"
+            value={formData.instructorName}
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="expectedGrade">Expected Grade:</label>
+          <input
+            type="text"
+            id="expectedGrade"
+            name="expectedGrade"
+            value={formData.expectedGrade}
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="explanation">Explanation:</label>
+          <textarea
+            id="explanation"
+            name="explanation"
+            value={formData.explanation}
+            onChange={handleChange}
+            required
+          ></textarea>
+        </div>
+
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default MissingGrades;
