d2feb6bac1fad5eb83ff16c70df73850d8df47c8
Albert Andrews <03jayteee@gmail.com>
2023-12-13 17:58:02 +0000

    update

--- src/component/Hero.js
+++ src/component/Hero.js
@@ -3,31 +3,35 @@ import { Link } from "react-router-dom";
 
 const Hero = () => {
   return (
-    <div className="flex flex-col xl:flex-row xl:h-screen items-center pt-[3rem] w-full p-[10rem] gap-[2rem]">
-      <div className="flex items-center justify-center flex-1   motion-safe:animate-bounce w-8 h-6 ">
-        <img src={require("./Data-rafiki.png")} alt="" />
+    <div className="flex flex-col items-center pt-8 px-4 xl:flex-row xl:h-screen xl:p-10 xl:pt-3 gap-4">
+      <div className="flex items-center justify-center flex-1 w-full xl:w-auto ">
+        <img
+          src={require("./Data-rafiki.png")}
+          alt=""
+          className="motion-reduce:animate-bounce  w-full h-auto "
+        />
       </div>
-      <div className="flex flex-col flex-1 h-full items-center xl:items-start justify-center text-center xl:text-start space-y-5 mb-[4rem]">
-        <h1 className=" font-bold  text-[1.5rem] w-full ">
+      <div className="flex flex-col flex-1 items-center xl:items-start justify-center text-center xl:text-start space-y-4 xl:mb-4">
+        <h1 className="font-bold text-lg xl:text-2xl">
           Welcome to the Missing Grade Reporting System
         </h1>
-        <p className="font-md font-thin">
+        <p className="font-normal text-sm xl:text-md">
           Explore academic clarity with our Missing Grade Reporting System.
           Quickly view grades, report missing ones, and communicate seamlessly.
           Get started by logging in or creating an account.
         </p>
 
-        <h2 className="font-bold text-[1rem] text-[#cfa460]">
+        <h2 className="font-bold text-md text-[#cfa460]">
           Ready to Take Control?
         </h2>
 
-        <p className="font-md font-thin">
+        <p className="font-normal text-sm xl:text-md">
           Join a community dedicated to maintaining accurate academic records.
           Login or create an account to get started on the path of academic
           empowerment.
         </p>
         <Link to="/login">
-          <button className="w-[8rem] font-bold text-[1rem] py-2 bg-gradient-to-tr from-[#232590] to-[#CCCCCC] text-white rounded-full">
+          <button className=" lg:w-[8rem] w-[6rem] font-bold text-[1rem] py-2 bg-gradient-to-tr from-[#232590] to-[#CCCCCC] text-white rounded-full">
             Log In
           </button>
         </Link>
