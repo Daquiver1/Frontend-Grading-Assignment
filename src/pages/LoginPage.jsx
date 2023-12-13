e40962e8a986fca79c20b5e4672d58bc2c61de22
OseiEsther <Oseiamoakoesther2022@gmail.com>
2023-12-13 15:39:09 +0000

    Refactor: Added padding to login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -3,8 +3,8 @@ import { LabelInput } from "../components/inputs/InputLabel";
 
 function LoginPage() {
   return (
-    <div className="w-full justify-center flex items-center h-screen py-20 overflow-y-auto bg-primary_blue">
-      <form className=" py-10 px-10 w-full 450px:w-[11cm] flex justify-center items-center flex-col bg-white rounded-md">
+    <div className="w-full justify-center px-3 flex items-center h-screen py-20 overflow-y-auto bg-primary_blue">
+      <form className=" py-10 px-2 450px:px-10 w-full 450px:w-[11cm] flex justify-center items-center flex-col bg-white rounded-md">
         <div className="h-[2.2cm] w-[2.2cm] border-2 border-secondary p-4 rounded-full">
           <img src={Logo} alt="logo" className="h-full w-full" />
         </div>
