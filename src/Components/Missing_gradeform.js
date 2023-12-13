7570ac10c3a30a1c04680f0a08ab398e15866817
muizbello <bellomuiz78+1@gmail.com>
2023-12-13 08:56:45 +0000

    Missing grade form page

---
+++ src/Components/Missing_gradeform.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Missing_gradeform(){
+    return (
+        <div>
+            <h1>hhbccjjb</h1>
+        </div>
+    )
+}
+
+export default Missing_gradeform;
