9575f432efc9c4e95c8459f3c1d5002d71263eed
NanaAmpomah <nanaampomaharhin@gmail.com>
2023-12-14 20:31:20 +0100

    imported the home.css file

--- grading-website/src/pages/Home.js
+++ grading-website/src/pages/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import HeroSection from "../components/HeroSection";
 import Footer from "../components/Footer";
-
+import "../components/Home.css";
 function Home() {
   const backgroundImage = (
     <>
