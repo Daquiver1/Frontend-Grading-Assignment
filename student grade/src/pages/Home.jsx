6605c1ad1fb8c5cac704e9b508fbd407a31743e5
Adipah Osei <adipahosei@gmail.com>
2023-12-14 23:22:58 +0000

    created and modified page layout

--- student grade/src/pages/Home.jsx
+++ student grade/src/pages/Home.jsx
@@ -1,43 +1,26 @@
-import React from 'react';
-import Header from '../components/Header';
+import React from "react";
+import backgroundImage from '../assets/images/home.jpg'
+import Header2 from "../components/header2";
+import logo from '../assets/images/logo.png'
 
-const HomePage = ({ backgroundImage }) => {
-  const backgroundStyle = {
-    backgroundImage: `url(${backgroundImage})`,
-  };
-
-  return (
-    <div className="min-h-screen bg-cover bg-center bg-gradient-to-b from-gray-900 to-purple-800" style={backgroundStyle}>
-      <Header />
-
-      <div className="mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8">
-        <div className="relative isolate overflow-hidden bg-gray-900 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
-          <div className="absolute inset-0 -z-10 h-full w-full object-cover">
-            {/* Background Image (if any) */}
-            <svg viewBox="0 0 1024 1024" className="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0" aria-hidden="true">
-              <circle cx="512" cy="512" r="512" fill="url(#759c1415-0410-454c-8f7c-9a820de03641)" fill-opacity="0.7" />
-              <defs>
-                <radialGradient id="759c1415-0410-454c-8f7c-9a820de03641">
-                  <stop stop-color="#7775D6" />
-                  <stop offset="1" stop-color="#E935C1" />
-                </radialGradient>
-              </defs>
-            </svg>
-          </div>
-
-          <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
-            <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">Boost your productivity.<br />Start using our app today.</h2>
-            <p className="mt-6 text-lg leading-8 text-gray-300">Ac euismod vel sit maecenas id pellentesque eu sed consectetur. Malesuada adipiscing sagittis vel nulla.</p>
-            <div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
-              <a href="#" className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white">Get started</a>
-              <a href="#" className="text-sm font-semibold leading-6 text-white">Learn more <span aria-hidden="true">→</span></a>
-            </div>
-          </div>
-
-        </div>
+const HomePage = () => {
+  return(
+    <div>
+      <Header2 logoPath={logo}/>
+      <div className="h-screen bg-cover bg-center bg-opacity-75"  style={{ backgroundImage: `url(${backgroundImage})` }}>
+          <main>
+          <div className="bg-cover bg-center  h-screen flex items-center justify-center">
+      <div className="bg-purple-900 max-w-2xl p-8 rounded-lg text-white">
+        {/* Your content goes here */}
+        <h1 className="text-3xl font-bold mb-4">School Grading System</h1>
+        <p>
+          This is an example of a centered container with a light purple background using Tailwind CSS.
+        </p>
       </div>
     </div>
-  );
-};
-
-export default HomePage;
+          </main>
+      </div>
+    </div>
+  )
+}
+export default HomePage
