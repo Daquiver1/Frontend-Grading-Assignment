3b94bdfe05f87ddc9ef3b308247d229058d91fb7
nerdfavour <favourackonu13@gmail.com>
2023-12-14 05:25:28 +0000

    added footer
    had to restart the whole thing because tailwind wasn't working for some reason. got it to work after restarting and just completed the footer

--- grade-report-system/tailwind.config.js
+++ grade-report-system/tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
