0b438364455203469afe8c6146ed66adda421650
Nana-Kay11 <knarthur002@st.ug.edu.gh>
2023-12-14 14:42:45 +0000

    Imported Home and Dashboard folders in the App.js folder

--- missing/src/App.js
+++ missing/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Navbar from "./Pages-And-Components/Navigation-Bar/navbar";
 import LoginForm from './Pages-And-Components/LoginForm/LoginForm';
+import Footer from './Pages-And-Components/Footer/Footer';
+import Home from './Pages-And-Components/Home/Home';
+import Dashboard from './Pages-And-Components/Dashboard/Dashboard';
 
-function App() {
+
+function App  () {
      return (
       <div>
-        <LoginForm />
+        <Dashboard />
       </div>
      );
 }
