ae725b8541c46d05858dd747f21dc0eb6ab747fb
Nana-Kay11 <knarthur002@st.ug.edu.gh>
2023-12-14 10:31:21 +0000

    Created dashboard.jsx and dashboard.css files in the Dashboard folder

---
+++ missing/src/Pages-And-Components/Dashboard/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from 'react';
+import './Dashboard.css';
+
+const Dashboard = () => {
+    const [grades, setGrades] =useState([]);
+    const [userData, setUserData] = useState({studentName: 'Keziah Nana Arthur',programOffering:'Bsc Information Technology', level: '200',studentId: '11091047', missingGradesCount: 2,})
+        
+     // Simulated data (you can replace this with actual data)
+  const mockGradesData = [
+    // ... your mock data
+  ];
+
+}
