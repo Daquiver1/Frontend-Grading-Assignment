15f4c4237b2ba3fe2d2fdce4dfd5ed1675621da2
NanaAmpomah <nanaampomaharhin@gmail.com>
2023-12-14 20:34:09 +0100

    imported the instructorContact.css file

--- grading-website/src/pages/InstructorContact.js
+++ grading-website/src/pages/InstructorContact.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import HeroSection from "../components/HeroSection";
 import Footer from "../components/Footer";
+import "../components/InstructorContact.css";
 
 function InstructorContact() {
   const backgroundImage = (
