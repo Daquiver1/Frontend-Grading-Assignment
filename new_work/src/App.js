133579970c450f71430cf7bfe1582e73da2a421a
kkkofi <kofiababio07@gmail.com>
2023-12-14 19:28:18 +0000

    created styles

---
+++ new_work/src/App.js
@@ -0,0 +1,23 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react'
+import './App.css';
+import  AppNavbar  from './components/Navbar';
+
+
+//import Login from './components/Login';
+//import Home from './components/Home';
+
+
+
+function App(){
+    return (
+      <>
+      <AppNavbar />
+      
+      </>
+      
+    );
+  }
+
+
+export default App;
