af4229d8340839ecf68020f5895b25c41825d95b
Gyaneee_awura <agbaah@st.ug.edu.gh>
2023-12-14 17:21:45 +0000

    Some fixes in the dashboard page

--- frontend-grading-assignment/src/App.jsx
+++ frontend-grading-assignment/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./routes/Home"
 import Login from "./routes/Login"
+import Dashboard from "./routes/Dashboard"
 
 function App() {
   
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
     </BrowserRouter>
   )
