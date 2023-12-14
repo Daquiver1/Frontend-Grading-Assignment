c1f11dc801623dde28aecf7627ea6b77d8c44aa7
Cweku-Siaw6620 <kksashong@st.ug.edu.gh>
2023-12-14 08:44:13 +0000

    Setup of my navigation bar.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,79 @@
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from "./components/HomePage";
 import LoginPage from "./components/LoginPage";
+import HomePage from "./components/HomePage";
 import DashboardPage from "./components/DashboardPage";
 import GradeReportPage from "./components/GradeReportPage";
 import MissingGradeForm from "./components/MissingGradeForm";
 import InstructorContactPage from "./components/InstructorContactPage";
 import HelpSupportPage from "./components/HelpSupportPage";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./App.css";
 
-function App() {
-  return (
-    <div>
-      <HomePage />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <div className="App">
+            <div className="App-header">
+              <Link to="/">Login</Link>
+            </div>
+            <div className="App-header">
+              <Link to="/HomePage">Home page</Link>
+            </div>
+            <div className="App-header">
+              <Link to="/DashboardPage">Dashboard</Link>
+            </div>
+            <div className="App-header">
+              <Link to="/GradeReportPage">Grade Report Page</Link>
+            </div>
+            <div className="App-header">
+              {" "}
+              <Link to="/MissingGradeForm">MissingGradeForm</Link>
+            </div>
+            <div className="App-header">
+              {" "}
+              <Link to="/InstructorContactPage">Contact Instructor</Link>
+            </div>
+            <div className="App-header">
+              {" "}
+              <Link to="/HelpSupportPage">Help and Support</Link>
+            </div>
+          </div>
+
+          <Routes>
+            <Route exact path="/" element={<LoginPage />}></Route>
+            <Route exact path="/HomePage" element={<HomePage />}></Route>
+            <Route
+              exact
+              path="/DashboardPage"
+              element={<DashboardPage />}
+            ></Route>
+            <Route
+              exact
+              path="/GradeReportPage"
+              element={<GradeReportPage />}
+            ></Route>
+            <Route
+              exact
+              path="/MissingGradeForm"
+              element={<MissingGradeForm />}
+            ></Route>
+            <Route
+              exact
+              path="/InstructorContactPage"
+              element={<InstructorContactPage />}
+            ></Route>
+            <Route
+              exact
+              path="/HelpSupportPage"
+              element={<HelpSupportPage />}
+            ></Route>
+          </Routes>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -1,6 +1,5 @@
 import "./component.css";
 import { useState } from "react";
-import { Link } from "react-router-dom";
 
 function LoginPage() {
   const [username, setUsername] = useState("");
