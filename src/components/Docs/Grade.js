aed7e525a5bdd6dedb34c633c1168a7f586274b1
Ewurafua Quansah <ewurafuaquansah02@gmail.com>
2023-12-14 12:34:22 +0000

    changes

--- src/components/Docs/Grade.js
+++ src/components/Docs/Grade.js
@@ -103,7 +103,7 @@ export default function Dashboard(){
                 <option value={"2023/2024"}>2023/2024</option>
               </select>
             </div>
-            </th>
+            </th> 
             <th colSpan={4}>
             <div className="mt-2">
                <button type="button" onClick={handRefresh} className="text-sm font-semibold leading-6 text-gray-900 ml-12">
