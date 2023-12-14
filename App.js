b61e45a4ad516ea71fc5796f8a785febd902e8f1
AMG MONI <150767131+AmgMoni-Fx@users.noreply.github.com>
2023-12-14 09:57:05 +0000

    Add files via upload
    New contents uploaded.

--- App.js
+++ App.js
@@ -3,17 +3,19 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './Components/Login';
 import Home from './Components/Home';
 import Transcripts from './Components/Transcripts';
-import Forms from './Components/Forms';
+import MissingGradeForms from './Components/Missing Grade Forms';
+import GradeBook from './Components/Grade Book';
 import Contacts from './Components/Contacts';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/transcripts" element={<Transcripts />} />
-        <Route path="/forms" element={<Forms />} />
+        <Route path="/forms" element={<MissingGradeForms />} />
+        <Route path="/Grade Book" element={<GradeBook />} />
         <Route path="/contacts" element={<Contacts />} />
       </Routes>
     </BrowserRouter>
