45586dd292f4ef876582fd49af34d809787aae81
Adjei Alexander <alexanderadjei1054@gmail.com>
2023-12-14 10:33:34 -1200

    Grading System

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import Home from "./components/pages/Landing-Page/Home";
+import Login from "./components/pages/Login/Loginform";
+import{
+  BrowerRouter,
+  BrowserRouter,
+  Route,
+  Routes,
+  route,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-     starting over!!
+     <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="Login" element={<Login />} />
+        </Route>
+      </Routes>
+     </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/pages/Login/Loginform.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import './Loginform.css';
+//import { FaUserAlt, FaLock  } from "react-icons/fa";
+
+
+const LoginForm = () => {
+    return(
+        <div className="wrapper">
+            <form action="">
+                <h1>Login</h1>
+                <div className="input-box">
+                    <input type="text" placeholder="Student ID" required />
+                </div>
+                <div className="input-box">
+                    <input type="password" placeholder="Password" required />
+                </div>
+
+                <div className="remember-forgot">
+                    <label><input type="checkbox" />Remember me</label>
+                    <a href="#">Forgot password?</a>
+                </div>
+
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
+};
+
+export default LoginForm;
