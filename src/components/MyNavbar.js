08d4a7e89b2fd8855e1f465ee5b4fc55c0864698
hodogbe <151807680+hodogbe@users.noreply.github.com>
2023-12-14 14:06:06 +0000

    Update MyNavbar.js

--- src/components/MyNavbar.js
+++ src/components/MyNavbar.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 function MyNavbar() {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
-      <Navbar.Brand as={Link} to="/">Missing Grade Reporting App</Navbar.Brand>
+      <Navbar.Brand as={Link} to="/">University of Ghana Grades Portal</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
