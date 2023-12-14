a73ce6b37b156d5a894fb777fbac0e6c20c7fe52
Albert Amoako <info.me.albert@gmail.com>
2023-12-14 11:25:10 +0000

    Included star students to the dash board

--- grade/src/dashboard.js
+++ grade/src/dashboard.js
@@ -26,60 +26,97 @@ const Dashboard = () => {
 
   const missingGrades = findMissingGrades();
 
+
+  const starStudents = [
+    { name: 'Young Seldon', grade: 'A', image: 'sheldon.jpeg' },
+    { name: 'Dr. Stone', grade: 'A', image: 'stone.jpeg' },
+    {name: 'Petyr Baelish', grade: 'A', image: 'got.jpeg' },
+  
+  ];
+
   return (
     <div>
       <MyNavbar />
-      
-      
+      <MyFooter />
 
-    
-    <div className="container main mt-5">
+      <div className="container main mt-5">
       <h1 className="mb-4">Dashboard</h1>
 
-      <div className="mb-4">
-        <h2>Current Grades Overview</h2>
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Course</th>
-              <th>Grade</th>
-              <th>Semester</th>
-              <th>Credit Hours</th>
-            </tr>
-          </thead>
-          <tbody>
-            {gradeReport.map((grade, index) => (
-              <tr key={index}>
-                <td>{grade.course}</td>
-                <td>{grade.grade}</td>
-                <td>{grade.semester}</td>
-                <td>{grade.creditHours}</td>
+<div className="mb-4">
+  <h2>Current Grades Overview</h2>
+  <table className="table">
+    <thead>
+      <tr>
+        <th>Course</th>
+        <th>Grade</th>
+        <th>Semester</th>
+        <th>Credit Hours</th>
+      </tr>
+    </thead>
+    <tbody>
+      {gradeReport.map((grade, index) => (
+        <tr key={index}>
+          <td>{grade.course}</td>
+          <td>{grade.grade}</td>
+          <td>{grade.semester}</td>
+          <td>{grade.creditHours}</td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+</div>
+
+<div>
+  <h2>Alerts</h2>
+  {missingGrades.length > 0 ? (
+    <div className="alert alert-warning" role="alert">
+      <p>You have missing grades for the following courses:</p>
+      <ul>
+        {missingGrades.map((missingGrade, index) => (
+          <li key={index}>{missingGrade.course}</li>
+        ))}
+      </ul>
+    </div>
+  ) : (
+    <div  className="alert alert-success" role="alert">
+      <p>No missing grades. Good job!</p>
+    </div>
+  )}
+</div>
+<div>
+          <h2>Star Students</h2>
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Image</th>
+                <th>Name</th>
+                <th>Grade</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {starStudents.map((student, index) => (
+                <tr key={index}>
+                  <td>
+                    <img
+                      src={`/images/${student.image}`}
+                      alt={student.name}
+                      style={{ maxWidth: '500px', maxHeight: '100px' }}
+                    />
+                  </td>
+                  <td>{student.name}</td>
+                  <td>{student.grade}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+</div>
+
+        
       </div>
 
-      <div>
-        <h2>Alerts</h2>
-        {missingGrades.length > 0 ? (
-          <div className="alert alert-warning" role="alert">
-            <p>You have missing grades for the following courses:</p>
-            <ul>
-              {missingGrades.map((missingGrade, index) => (
-                <li key={index}>{missingGrade.course}</li>
-              ))}
-            </ul>
-          </div>
-        ) : (
-          <div  className="alert alert-success" role="alert">
-            <p>No missing grades. Good job!</p>
-          </div>
-        )}
-      </div>
-    </div>
-    <MyFooter />
-    </div>
+    
+   
   );
 };
 
