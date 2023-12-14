6caaf27bf8e959f236796829e48d830172424f80
NanaAmpomah <nanaampomaharhin@gmail.com>
2023-12-14 20:39:49 +0100

    imported the missingGradeFormPage.css file

--- grading-website/src/pages/MissingGradeFormPage.js
+++ grading-website/src/pages/MissingGradeFormPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import HeroSection from "../components/HeroSection";
+import "../components/MissingGradeFormPage.css";
 
 function MissingGradeFormPage() {
   const handleClick = () => {
