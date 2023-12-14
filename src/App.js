2e6d3cb23ee4ed855768c47ab6fa8aefb6c40ac5
muizbello <bellomuiz78+1@gmail.com>
2023-12-14 17:41:40 +0000

    Program file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Routes, BrowserRouter } from 'react-router-dom';
-import Landingpage from './Components/Landingpage';
-import Login from './Components/Login';
-import Missing_gradeform from './Components/Missing_gradeform';
-import Gradereport from './Components/Gradereport';
-import Dashboard from './Components/Dashboard';
-import Help from './Components/Help';
-import Instructorcontact from './Components/Instructorcontact';
+import Landingpage from './Pages/Landingpage';
+import Login from './Pages/Login';
+import Missing_gradeform from './Pages/Missing_gradeform';
+import Gradereport from './Pages/Gradereport';
+import Dashboard from './Pages/Dashboard';
+import Help from './Pages/Help';
+import Instructorcontact from './Pages/Instructorcontact';
 
 
 function App() {
