76a331a449b1166743124ff051543cb9ac390205
Pappyjedu <148871618+Pappyjedu@users.noreply.github.com>
2023-12-14 12:43:05 +0000

    Update login page and app.js

--- frontend-grading-assignment/src/App.js
+++ frontend-grading-assignment/src/App.js
@@ -1,16 +1,28 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link, useLocation } from 'react-router-dom';
+import LoginPage from './components/LoginPage';
 import HomePage from './components/HomePage';
 import GradeReportPage from './components/GradeReportPage';
 import MissingGradeFormPage from './components/MissingGradeFormPage';
 import InstructorContactPage from './components/InstructorContactPage';
 import HelpSupportPage from './components/HelpSupportPage';
+
+const PrivateRoute = ({ children, ...rest }) => {
+ const location = useLocation();
+ const isAuthenticated = // add your authentication logic here
+ if (!isAuthenticated) {
+    return <Redirect to="/login" />;
+ }
+ return <Route {...rest}>{children}</Route>;
+};
+
 function App() {
  return (
     <Router>
       <nav>
         <ul>
           <li><Link to="/">Home</Link></li>
+          <li><Link to="/login">Login</Link></li>
           <li><Link to="/grade-report">Grade Report</Link></li>
           <li><Link to="/missing-grade-form">Missing Grade Form</Link></li>
           <li><Link to="/instructor-contact">Instructor Contact</Link></li>
@@ -19,10 +31,11 @@ function App() {
       </nav>
       <Routes>
         <Route path="/" exact element={<HomePage />} />
-        <Route path="/grade-report" element={<GradeReportPage />} />
-        <Route path="/missing-grade-form" element={<MissingGradeFormPage />} />
-        <Route path="/instructor-contact" element={<InstructorContactPage />} />
-        <Route path="/help-support" element={<HelpSupportPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <PrivateRoute path="/grade-report" element={<GradeReportPage />} />
+        <PrivateRoute path="/missing-grade-form" element={<MissingGradeFormPage />} />
+        <PrivateRoute path="/instructor-contact" element={<InstructorContactPage />} />
+        <PrivateRoute path="/help-support" element={<HelpSupportPage />} />
       </Routes>
     </Router>
  );
---
+++ frontend-grading-assignment/src/components/LoginPage.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useState } from 'react';
+
+function LoginPage() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('A form was submitted:', { email, password });
+    };
+
+    return (
+        <div>
+            <h1>Login</h1>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Email:
+                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                </label>
+                <br />
+                <label>
+                    Password:
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                </label>
+                <br />
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
+}
+
+export default LoginPage;
