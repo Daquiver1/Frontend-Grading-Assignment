75b8b2bb80363ef9d134436d59c52c16b7ce470c
feckomana <pkaforve@gmail.com>
2023-12-14 15:48:19 +0000

    passwrod updated

--- myassignment/src/Login.js
+++ myassignment/src/Login.js
@@ -6,7 +6,7 @@ const Login = () => {
             </h1>
             <form action="POST">
                 <input type="text" placeholder="Student ID" className="student" required/>
-                <input type="text" placeholder="Pin" className="pin" required/>
+                <input type="password" placeholder="Pin" className="pin" required/>
                 <input type="submit" className="submit"/>
             </form>
         </div>
