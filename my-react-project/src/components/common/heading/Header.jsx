74eb4d8bdb1460f982809d71784b25f80aef0f00
Joshua Danso <jdanso2104@gmail.com>
2023-12-14 19:00:29 +0000

    add

--- my-react-project/src/components/common/heading/Header.jsx
+++ my-react-project/src/components/common/heading/Header.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Head from './Head'
 import './header.css'
 import { Link } from "react-router-dom"
 
 const Header = () => {
+  const [click, setClick] = useState(false)
   return (
     <>
       <Head />
       <header>
         <nav className='flexSB'>
-          <ul className='flexSB'>
+          <ul className={click ? "mobile-nav" : "flexSB"} onClick={() => setClick(false)}>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/dashboard">Dashboard</Link></li>
             <li><Link to="/missing_grades">Missing Grades</Link></li>
@@ -21,6 +22,9 @@ const Header = () => {
           <div className="start">
             <div className="button">Get To Know More</div>
           </div>
+          <button className='toggle' onClick={() => setClick(!click)}>
+            {click ? <i className='fa fa-times'></i> : <i className='fa fa-bars'></i>}
+          </button>
         </nav>
       </header>
     </>
