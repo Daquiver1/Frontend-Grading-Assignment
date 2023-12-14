7a5944181a9717a6d3b3c1e0ad6778c2e66cb2bb
Nana-Kay11 <knarthur002@st.ug.edu.gh>
2023-12-14 14:41:26 +0000

    Deleted toggle icon in navbar.css and navbar.jsx

--- missing/src/Pages-And-Components/Navigation-Bar/navbar.jsx
+++ missing/src/Pages-And-Components/Navigation-Bar/navbar.jsx
@@ -1,62 +1,49 @@
-import React, {useState} from 'react'
+import React from "react";
 import './navbar.css'
 
 function Navbar() {
-       const [active, setActive] = useState("nav__menu");
-
-              active === "nav__menu" 
-              ? setActive('nav__menu nav__active')
-              : setActive ("nav__menu")
-
-
-
-     return (
-      <nav className='nav'>
-        <a href="#" className="nav__brand">
-          herdoy
+  return (
+   <nav className="nav">
+    <a href="#" className="nav__brand">Missing Grade System</a>
+    <ul className="nav__menu">
+    <li className="nav__items">
+      <a href="#" className="nav__link">
+        Home
         </a>
-        <ul className={active}>
-          <li className="nav__item">
-            <a href="#" className="nav__link">
-              Home
-            </a>
-          </li>
-          <li className="nav__item">
-            <a href="#" className="nav__link">
-              Dashboard
-            </a>
-          </li>
-          <li className="nav__item">
-            <a href="#" className="nav__link">
-              Login
-            </a>
-         </li>
-          <li className="nav__item">
-            <a href="#" className="nav__link">
-              Grades Report
-            </a>
-          </li>
-          <li className="nav__item">
-            <a href="#" className="nav__link">
-              Instructor Contacts
-            </a>
-          </li>
-          <li className="nav__item">
-            <a href="#" className="nav__link">
-              Missing Grades Form
-            </a>
-          </li>
-          <li className="nav__item">
-            <a href="#" className="nav__link">
-              Help and Support
-            </a>
-          </li>
-        </ul>
-        <div onClick = {navToggle} className={toggleIcon}>
-          <div className="line1"></div>
-          <div className="line2"></div>
-          <div className="line3"></div>
-        </div>
-      </nav>
-     );
-}
+        </li>
+    <li className="nav__items">
+      <a href="#" className="nav__link">
+        Dashboard
+        </a>
+        </li>
+    <li className="nav__items">
+      <a href="#" className="nav__link">
+        Login
+        </a>
+        </li>
+    <li className="nav__items">
+      <a href="#" className="nav__link">
+        Grade Report
+        </a>
+        </li>
+    <li className="nav__items">
+      <a href="#" className="nav__link">
+        Missing Grade Form
+        </a>
+        </li>
+    <li className="nav__items">
+      <a href="#" className="nav__link">
+        Contact
+        </a>
+        </li>
+    <li className="nav__items">
+      <a href="#" className="nav__link">
+        Help and Support
+        </a>
+        </li>
+    </ul>
+   </nav>
+  );
+};
+
+export default Navbar;
