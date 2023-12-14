265762a053565e4c8a4a38130233cded97f68b6e
Joshua Danso <jdanso2104@gmail.com>
2023-12-14 14:20:43 +0000

    add

--- my-react-project/src/App.js
+++ my-react-project/src/App.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import "./App.css"
+import Header from "./components/common/heading/Header"
+import { BrowserRouter as Router, Switch } from "react-router-dom"
 
-const App = () => {
+function App() {
   return (
-    <div>
-      <h1>Hello React</h1>
-    </div>
+    <>
+      <Router>
+        <Header />
+        <Switch>
+          
+        </Switch>
+
+      </Router>
+      
+    </>
   )
 }
 
---
+++ my-react-project/src/components/common/heading/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Head from './Head'
+import './header.css'
+
+const Header = () => {
+  return (
+    <>
+      <Head />
+    </>
+  )
+}
+
+export default Header
