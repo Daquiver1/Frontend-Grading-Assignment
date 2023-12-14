7f9ac84348030ebb92733f30898c1cfc8402ae53
Ewurafua Quansah <ewurafuaquansah02@gmail.com>
2023-12-14 10:17:00 +0000

    additional changes

--- src/components/Dashboard/dashboard.js
+++ src/components/Dashboard/dashboard.js
@@ -1,1 +1,27 @@
- 
+import React from "react";
+import Layout from '../Layout/layout';
+import ReactApexChart from "react-apexcharts";
+
+
+
+
+const state = {
+  series: [44, 55, 41, 17, 15],
+  options: {
+    chart: {
+      type: 'donut',
+    },
+    responsive: [{
+      breakpoint: 480,
+      options: {
+        chart: {
+          width: 100,
+          height:100
+        },
+        legend: {
+          position: 'bottom'
+        }
+      }
+    }]
+  },
+};
