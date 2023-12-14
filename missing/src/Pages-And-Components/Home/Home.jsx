7c942af06f8ad59412169d93984034e3f696dd72
Nana-Kay11 <knarthur002@st.ug.edu.gh>
2023-12-14 10:28:04 +0000

    Added footer and home to app.js file, modified footer component in footer.jsx and footer.css

---
+++ missing/src/Pages-And-Components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+  return (
+    <div className='container'>
+      <h1>Welcome to Our Website</h1>
+      <button className='btn'>Login</button>
+    </div>
+  )
+};
+
+export default Home;
