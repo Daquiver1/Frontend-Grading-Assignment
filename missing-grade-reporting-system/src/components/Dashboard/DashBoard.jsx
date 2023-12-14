83c70ab7ee28f1ebd92247e3a77bf025403e6cc0
kofidell4545 <151035682+Kofidell4545@users.noreply.github.com>
2023-12-14 16:17:35 +0000

    modified dashboard .jsx

--- missing-grade-reporting-system/src/components/Dashboard/DashBoard.jsx
+++ missing-grade-reporting-system/src/components/Dashboard/DashBoard.jsx
@@ -3,17 +3,22 @@
 import React from 'react';
 import NavBar from '../Navbar/NavBar'; // Adjust the path based on your folder structure
 import './DashBoard.css';
-import UserInfo from '../UserInfo/UserInfo';
+import MissingGradeDashboard from '../MissingGradeDashboard/MissingGradeDashboard';
+import UserInfo from '../UserInfo/UserInfo'; // Add this line
 
 const Dashboard = () => {
   return (
+    
     <div className="dashboard">
-      <NavBar />
       <div className="dashboard-content">
         {/* Add content for the Dashboard */}
         <h1>Your Dashboard</h1>
         {/* Include overview of current grades and alerts for missing grades */}
       </div>
+      <MissingGradeDashboard/>
+      <NavBar />
+      <UserInfo/>
+      
     </div>
   );
 };
