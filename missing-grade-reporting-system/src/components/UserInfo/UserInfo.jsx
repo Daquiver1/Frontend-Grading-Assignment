8200cef97bc655c411b815a276f643810cdf9940
kofidell4545 <151035682+Kofidell4545@users.noreply.github.com>
2023-12-14 16:27:24 +0000

    added userinfo.jsx

--- missing-grade-reporting-system/src/components/UserInfo/UserInfo.jsx
+++ missing-grade-reporting-system/src/components/UserInfo/UserInfo.jsx
@@ -1,15 +1,20 @@
-// components/UserInfo/UserInfo.jsx
+
 
 import React from 'react';
 import './UserInfo.css';
 
-const UserInfo = ({ studentName, level, missingGrades }) => {
+const UserInfo = ({ studentName, academicLevel, course, missingGrades }) => {
   return (
     <div className="user-info-container">
-      <div className="user-info">
-        <p>Student Name: {studentName}</p>
-        <p>Level: {level}</p>
-        <p>Missing Grades: {missingGrades}</p>
+      <div className="user-info-header">
+        <h2>User Info</h2>
+      </div>
+      <div className="user-info-content">
+        <p><strong>Name:</strong>NHYIRABA DAVID KOFI {studentName}</p>
+        <p><strong>Level:</strong> 200{academicLevel}</p>
+        <p><strong>Semester:</strong> FIRST SEMESTER{academicLevel}</p>
+        <p><strong>Course:</strong> INFORMATION TECHNOLOGY{course}</p>
+        <p><strong>Missing Grades:</strong> 4{missingGrades}</p>
       </div>
     </div>
   );
