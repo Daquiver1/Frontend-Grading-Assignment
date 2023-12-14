daf86ed1a18b5f95c63a1b701cb0523dcf431740
Ewurafua Quansah <ewurafuaquansah02@gmail.com>
2023-12-14 10:29:40 +0000

    changes

--- src/components/Docs/Grade.js
+++ src/components/Docs/Grade.js
@@ -3,11 +3,11 @@ import Layout from '../Layout/layout';
 
 const grade = [
    {subject:"Course One",credit:4, assessment:29,term:50,total:79,result:"Pass",grade:"B", semester:"Semester One", date:"2021/2022"},
-   {subject:"Course One",credit:4, assessment:30,term:60,total:90,result:"Pass",grade:"A+",semester:"Semester One",date:"2021/2022"},
-   {subject:"Course two",credit:4, assessment:25,term:60,total:85,result:"Pass",grade:"A",semester:"Semester Two",date:"2022/2023"},
-   {subject:"Course three",credit:4, assessment:20,term:50,total:70,result:"Pass",grade:"B",semester:"Semester Two",date:"2022/2023"},
-   {subject:"Course four",credit:4, assessment:28,term:55,total:83,result:"Pass",grade:"A",semester:"Semester Three",date:"2023/2024"},
-   {subject:"Course five",credit:4, assessment:30,term:60,total:90,result:"Pass",grade:"A+",semester:"Semester Three",date:"2023/2024"},
+   {subject:"Course Two",credit:4, assessment:30,term:60,total:90,result:"Pass",grade:"A+",semester:"Semester One",date:"2021/2022"},
+   {subject:"Course Three",credit:4, assessment:25,term:60,total:85,result:"Pass",grade:"A",semester:"Semester Two",date:"2022/2023"},
+   {subject:"Course Four",credit:4, assessment:20,term:50,total:70,result:"Pass",grade:"B",semester:"Semester Two",date:"2022/2023"},
+   {subject:"Course Five",credit:4, assessment:28,term:55,total:83,result:"Pass",grade:"A",semester:"Semester Three",date:"2023/2024"},
+   {subject:"Course Six",credit:4, assessment:30,term:60,total:90,result:"Pass",grade:"A+",semester:"Semester Three",date:"2023/2024"},
 ];
 
 export default function Dashboard(){
