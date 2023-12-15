231e121af014056a2dcae62115495ad0cd3a92f6
Gyaneee_awura <agbaah@st.ug.edu.gh>
2023-12-15 16:11:10 +0000

    some fixes in the footer page

--- frontend-grading-assignment/src/components/Footer.jsx
+++ frontend-grading-assignment/src/components/Footer.jsx
@@ -3,9 +3,9 @@ const Footer = () => {
         <div>
             <footer className="py-5 bg-dark">
                 <div className="row">
-                    <div className="col-md-4"></div>
-                    <div className="col-md-4"></div>
-                    <div className="col-md-4"></div>
+                    <h6 className="text-white text-center">
+                        copyright &#169; 2023
+                    </h6>
                 </div>
             </footer>
         </div>
