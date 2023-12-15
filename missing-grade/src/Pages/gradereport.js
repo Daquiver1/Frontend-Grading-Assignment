b6fdad52eb3e6fe996f1bde8d47d0d03a1857c81
kddjan <joelkddjan@gmail.com>
2023-12-15 10:32:54 +0000

    edited the gradereport

--- missing-grade/src/Pages/gradereport.js
+++ missing-grade/src/Pages/gradereport.js
@@ -128,6 +128,125 @@ function GradeReport() {
         </tbody>
     </table>
 </div>
+        
+
+<div class="relative overflow-x-auto shadow-md sm:rounded-lg mt-10">
+    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-blue-100 ">
+            <tr>
+                <th scope="col" class="px-6 py-3">
+                    Course name
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Grade
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Score
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    GPA
+                </th>
+               
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="bg-white border-b ">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap ">
+                    DCIT 304
+                </th>
+                <td class="px-6 py-4">
+                    A
+                </td>
+                <td class="px-6 py-4">
+                    85%
+                </td>
+                <td class="px-6 py-4">
+                  3.5
+                </td>
+                
+            </tr>
+            <tr class="bg-white border-b">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap ">
+                    DCIT 303
+                </th>
+                <td class="px-6 py-4">
+                    B
+                </td>
+                <td class="px-6 py-4">
+                    72%
+                </td>
+                <td class="px-6 py-4">
+                   2.55
+                </td>
+               
+            </tr>
+            <tr class="bg-white border-b">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                    DCIT 305
+                </th>
+                <td class="px-6 py-4">
+                    A
+                </td>
+                <td class="px-6 py-4">
+                   85%
+                </td>
+                <td class="px-6 py-4">
+                   3.9
+                </td>
+              
+            </tr>
+
+
+            <tr class="bg-white border-b">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                    DCIT 307
+                </th>
+                <td class="px-6 py-4">
+                    A
+                </td>
+                <td class="px-6 py-4">
+                    83%
+                </td>
+                <td class="px-6 py-4">
+                    3.6
+                </td>
+              
+            </tr>
+
+            <tr class="bg-white border-b">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                    DCIT 309
+                </th>
+                <td class="px-6 py-4">
+                    C
+                </td>
+                <td class="px-6 py-4">
+                    60%
+                </td>
+                <td class="px-6 py-4">
+                   2.2
+                </td>
+              
+            </tr>
+
+            <tr class="bg-white border-b">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                    Aca 301
+                </th>
+                <td class="px-6 py-4">
+                    A
+                </td>
+                <td class="px-6 py-4">
+                   84%
+                </td>
+                <td class="px-6 py-4">
+                    3.8
+                </td>
+              
+            </tr>
+        </tbody>
+    </table>
+</div>
 
     </div>
 </div>
