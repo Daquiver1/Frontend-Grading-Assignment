c7382c408f9466331b719546672c79fd5be403fb
Ralph-hue269 <120757645+Ralph-hue269@users.noreply.github.com>
2023-12-15 08:35:37 +0000

    Added Footer

---
+++ Footer.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import './footer.css';
+
+const MyFooter = () => {
+  return (
+    <Navbar className="navi" fixed="bottom" bg="warning" variant="dark">
+      <Container className='contain'>
+        <Navbar.Text className="text-muted">
+          © {new Date().getFullYear()} University of Ghana
+        </Navbar.Text>
+        <Nav className="ml-auto">
+          <Nav.Link
+            href="https://www.facebook.com/"
+            className="text-muted"
+            style={{ marginRight: '10px' }}
+          >
+            <i className="fab fa-facebook-f"></i>
+          </Nav.Link>
+          <Nav.Link
+            href="https://twitter.com/"
+            className="text-muted"
+            style={{ marginRight: '10px' }}
+          >
+            <i className="fab fa-twitter"></i>
+          </Nav.Link>
+          <Nav.Link
+            href="https://www.linkedin.com/"
+            className="text-muted"
+            style={{ marginRight: '10px' }}
+          >
+            <i className="fab fa-linkedin-in"></i>
+          </Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default MyFooter;
