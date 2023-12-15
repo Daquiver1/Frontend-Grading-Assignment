bc46a44be20833760843241f77667a3a468e3cc2
death-inspires-me <nana.esseney@gmail.com>
2023-12-15 16:45:28 +0000

    Updated app.js and index.js to render the pages

--- grade-report-sys/src/App.js
+++ grade-report-sys/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import Navbar from "./components/Navbar/Navbar";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+} from "react-router-dom";
+import Home from "./pages/home";
+import Contact from "./pages/contact";
+import Dashboard from "./pages/dashboard";
+import Hnsupport from "./pages/hnsupport"; 
+import Login from "./pages/login";
+import Report from "./pages/report";
+import Complain from "./pages/complain";
+import Loginform from "./components/loginform/loginform";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route path="/home" element={<Home />} />
+                <Route
+                    path="/login"
+                    element={<Login />}
+                />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route
+                    path="/report"
+                    element={<Report />}
+                />
+                <Route path="/complain" element={<Complain />} />
+                <Route path="/hnsupport" element={<Hnsupport />} />
+            </Routes>
+        </Router>
+    );
 }
-
+ 
 export default App;
