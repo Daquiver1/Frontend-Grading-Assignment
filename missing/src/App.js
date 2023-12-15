3b4c0c6b89e04e2fd8334a13133e836ebbd54f02
Divine <awuahasaredivine9@gmail.com>
2023-12-15 15:29:52 +0000

    missing grade

--- missing/src/App.js
+++ missing/src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import LoginPage from "./components/LoginPage/LoginPage/LoginPage.jsx";
 import Dashboard from "./components/Dashboard/Dashboard.jsx";
+import GradeReport from "./components/GradeReport/GradeReport.jsx";
 
 function App() {
   return (
     <div>
-      <Dashboard />
+      <GradeReport />
     </div>
   );
 }
