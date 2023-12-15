e471c4f16449e9d3a5357bc6119e9b04b784ba65
iijfgadfhgiuaehger9tiuh3489 <camoateng002@st.ug.edu.gh>
2023-12-15 14:10:55 -0800

    Worked done

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import HomePage from './Page/homepage';
+import Login from './Page/login';
+import GradeReport from './Page/graderepoty';
+import Dashboard from './Page/Dashboard';
+
+function App() {
+  return (
+    <>
+      <HomePage/>
+      <Login/>
+      <GradeReport/>
+      <Dashboard/>
+    </>
+  );
+}
+
+export default App;
