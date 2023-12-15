07bb91427ae08730b98fb6ca04059786ce7995e0
Pappyjedu <148871618+Pappyjedu@users.noreply.github.com>
2023-12-15 15:59:04 +0000

    Update dashboard page

--- frontend-grading-assignment/src/App.js
+++ frontend-grading-assignment/src/App.js
@@ -11,26 +11,13 @@ function App() {
     <Router>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/">
-            Navbar
-          </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
+          <h1>MISSING GRADE REPORT SYSTEM</h1>
+         
+            
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
               <li className="nav-item">
-                <Link className="nav-link" to="/login">
-                 Login
-                </Link>
+                
               </li>
             </ul>
           </div>
