9f6a17434728c8d46a271b3d02fbd5f88038a74b
fafakofi <sfkbenson@st.ug.edu.gh>
2023-12-15 16:09:53 +0000

    help page updated

--- myassignment/src/Help.js
+++ myassignment/src/Help.js
@@ -1,7 +1,30 @@
+import './CSS/help.css'
 const Help = () => {
     return ( 
         <div className="help">
-            <h1>Help</h1>
+            <h1>FINDING SOMETHING DIFFICULT?</h1>
+
+            <h3>How can I access my missing grade report?</h3>
+            <p>
+                You can access your missing grade report by logging into your account on our website. Navigate to the "Grade Report" section to view your current and past grades.
+            </p>
+            <h3>What should I do if a grade is missing from my report?</h3>
+            <p>
+            If a grade is missing, please check with your instructor first to ensure there isn't a delay in grade submission. If the issue persists, you can file a complaint by clicking on the "missing grade form" button in your account dashboard.
+            </p>
+
+            <h3>How long does it take to process a grade retrieval complaint?</h3>
+            <p>
+            A: The processing time for grade retrieval complaints varies. We aim to resolve complaints as quickly as possible, but it may depend on the nature of the issue. You will receive updates on the status of your complaint through email or your account dashboard.
+            </p>
+            <h3>
+            How do I contact support if I have additional questions?
+            </h3>
+
+            <p>You can contact our support team through the "Contact Us" page on our website. Our team will assist you with any additional questions or concerns regarding your missing grades</p>
+
+            <h3>Are there any specific requirements for filing a grade retrieval complaint?</h3>
+            <p>Make sure to provide accurate and detailed information in your complaint. Additionally, check the complaint submission guidelines to ensure that you meet any specific requirements outlined for the process.</p>
         </div>
      );
 }
