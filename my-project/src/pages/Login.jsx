15b914f5d2e38a701052bc2894afdbe690fa2f4c
Afful Nana Kwasi <affulnana04@gmail.com>
2023-12-15 22:35:46 +0000

    updated

--- my-project/src/pages/Login.jsx
+++ my-project/src/pages/Login.jsx
@@ -5,11 +5,11 @@ const Login = () => {
         <h1 className="text-4xl text-whitefont-bold text-center mb-6 ">Login</h1>
         <form action="">
         <div className="relative my-4">
-          <input type="email" className="block-w-72 py-2.3 px-0 text sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:blue-500 "  name="email" />
+          <input type="email" className="block-w-72 py-2.3 px-0 text sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus-text-white focus:border-blue-600 peer "  name="email" />
           <label htmlFor="">Your Email</label>
         </div>
         <div className="relative my-4">
-          <input type="password"  className="block-w-72 py-2.3 px-0 text sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:blue-500 " name="password" />
+          <input type="password"  className="block-w-72 py-2.3 px-0 text sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus-text-white focus:border-blue-600 peer " name="password" />
           <label htmlFor="">Your Password</label>
         </div>
         <div>
