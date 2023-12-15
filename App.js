3f9dd8af6081956bbe2225bc75a23cf9f54d7493
LouisaEco <147488916+LouisaEco@users.noreply.github.com>
2023-12-15 11:57:08 +0000

    Update App.js

--- App.js
+++ App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import Landingpage from './pages/Landingpage';
-import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
-import InstructorsContact from './pages/InstructorsContact';
-import HelpAndSupport from './pages/HelpAndSupport';
+import Dashboard from './pages/Dashboard';
 import Gradereportpage from './pages/Gradereportpage';
 import MissingGradeForm from './pages/MissingGradeForm';
+import InstructorsContact from './pages/InstructorsContact';
+import HelpAndSupport from './pages/HelpAndSupport';
 import './App.css';
 import './components/Sidebar.css'; 
 
@@ -20,12 +20,14 @@ function App() {
           <div className="main-container">
             <Routes>
               <Route path="/" element={<Landingpage />} />
-              <Route path="/Dashboard" element={<Dashboard />} />
               <Route path="/Login" element={<Login />} />
-              <Route path="/InstructorsContact" element={<InstructorsContact />} />
-              <Route path="/HelpAndSupport" element={<HelpAndSupport />} />
+              <Route path="/Dashboard" element={<Dashboard />} />
               <Route path="/Gradereportpage" element={<Gradereportpage />} />
               <Route path="/MissingGradeForm" element={<MissingGradeForm />} />
+              <Route path="/InstructorsContact" element={<InstructorsContact />} />
+              <Route path="/HelpAndSupport" element={<HelpAndSupport />} />
+             
+
             </Routes>
           </div>
         </div>
