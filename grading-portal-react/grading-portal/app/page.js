2424d4b855d68ffbe4363902e497617c3900cf90
the-cute-programmer <roduronyarko@gmail.com>
2023-12-15 22:49:05 +0000

    Added files

---
+++ grading-portal-react/grading-portal/app/page.js
@@ -0,0 +1,14 @@
+import Nav from "@/components/Nav";
+import Welcome from "@/components/Welcome";
+
+export default function Home() {
+  return (
+    <main>
+      <Nav/>
+      <Welcome/>
+    </main>
+  );
+}
+
+
+// Home, login, dashboard,  grade reportError, missing grade, instructors page, help
