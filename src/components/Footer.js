38de05a3f2c5698483301a2fc6870f4f593dcba8
Frederick Ofori <fredofori306@gmail.com>
2023-12-15 18:57:46 +0000

    Added footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer> 
+      <p>
+      </p> 
+          <strong>Disclaimer:</strong> GradeVista is intended for informational purposes only. It may not reflect real-time data and should not be considered as an official report. For accurate and updated information regarding missing grades or academic records, please contact the appropriate administrative department or faculty advisor.
+    </footer>
+  )
+}
+
+export default Footer
