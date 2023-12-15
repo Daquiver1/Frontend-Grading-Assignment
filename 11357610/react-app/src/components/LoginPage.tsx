0cc9aff8bfdcc814da2891232a78d221e6fefd9a
Ryan Brown <niiakweibrown1@gmail.com>
2023-12-15 05:14:08 -0800

    Login Page added

---
+++ 11357610/react-app/src/components/LoginPage.tsx
@@ -0,0 +1,52 @@
+// LoginPage.tsx
+
+import React, { useState } from "react";
+import "./LoginPage.css";
+
+const LoginPage: React.FC = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // Here you can perform authentication logic (e.g., send login data to a server)
+    console.log("Username:", username);
+    console.log("Password:", password);
+    // Reset the form after handling login
+    setUsername("");
+    setPassword("");
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <button type="submit">Login</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
