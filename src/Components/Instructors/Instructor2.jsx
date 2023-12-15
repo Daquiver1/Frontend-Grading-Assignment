7e7bb9437b42915f1a23ff631239d2601e267d19
Mensah <mensaherskine@gmail.com>
2023-12-15 07:44:48 +0000

    Modified some code in footer.css and added Instructors componenets to be rendered on the contact page.

---
+++ src/Components/Instructors/Instructor2.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import './Instructor2.css'
+
+const Instructor2 = () => {
+  return (
+    <div className="mainDiv">
+      <div className="lecturerInfo">
+        <h2>Lecturer.</h2>
+        <p><strong>Name:</strong> Mr. Mr. Dwumfour Abdullai Abdul-Aziz</p>
+        <p><strong>Email:</strong>  adwumfour@ug.edu.gh</p>
+      </div>
+      <div className="courseList">
+        <h2>Course Codes:</h2>
+        <ul>
+          <li>DCIT 203</li>
+          <br/>
+          <li>DCIT 102</li>
+        </ul>
+      </div>
+      <div className="lecturerInfo">
+        <h2>Lecturer.</h2>
+        <p><strong>Name:</strong> Mr. Paul Nii Tackie Ammah</p>
+        <p><strong>Email:</strong>  pammah@ug.edu.gh</p>
+      </div>
+      <div className="courseList">
+        <h2>Course Codes:</h2>
+        <ul>
+          <li>DCIT 201</li>
+          <br/>
+          <li>DCIT 202</li>
+        </ul>
+      </div>
+    </div>  
+  )
+}
+
+export default Instructor2
--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import Instructor1 from '../Components/Instructors/Instructor1'
+import Instructor2 from '../Components/Instructors/Instructor2'
+import Instructor3 from '../Components/Instructors/Instructor3'
+
 
 const Contact = () => {
   return (
     <div>
-      
+      <Instructor1/>
+      <Instructor2/>
+      <Instructor3/>
     </div>
   )
 }
