452cac4c626ba1242a2712b69ed563ef8b88c476
hayat717 <hayatibrahim717@gmail.com>
2023-12-15 22:02:07 +0000

    changes on App and Login files

--- my-react-assignment/src/App.jsx
+++ my-react-assignment/src/App.jsx
@@ -14,7 +14,6 @@ function App() {
   return (
         <BrowserRouter>
         <Navbar />
-       
           <Routes>
             {/* <Route index element={<Home />} /> */}
             
--- my-react-assignment/src/Pages/Login.jsx
+++ my-react-assignment/src/Pages/Login.jsx
@@ -1,76 +1,73 @@
-import React, { useState } from 'react';
+// import Navbar from "../components/Navbar";
+import React, { useState } from "react";
 
 const Login = () => {
-  const [formData, setFormData] = useState({
-    studentId: '',
-    pin: '',
-  });
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const handleLogin = () => {
+    // Implement your login logic here (e.g., send data to a backend)
+    console.log("Username:", username);
+    console.log("Password:", password);
 
-  const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  const handleLogin = (e) => {
-    e.preventDefault();
-    // Simulated authentication logic
-    if (formData.studentId === '11564738' && formData.pin === '6789') {
-      setIsAuthenticated(true);
-    } else {
-      setIsAuthenticated(false);
-      alert('Invalid Student ID or PIN. Please try again.');
-    }
+    // Reset form fields
+    setUsername("");
+    setPassword("");
   };
 
   return (
-    <div>
-      <div className="bg-white p-8 rounded-lg shadow-md w-full sm:w-96 justify-center">
-        <h2 className="text-3xl font-extrabold mb-6 text-center">Student Login</h2>
-
-        {isAuthenticated ? (
-          <div className="text-green-600 font-semibold mb-6 text-center">Login Successful!</div>
-        ) : (
-          <form onSubmit={handleLogin}>
-            <div className="mb-4">
-              <label htmlFor="studentId" className="block text-sm font-semibold text-gray-600">Student ID:</label>
-              <input
-                type="text"
-                id="studentId"
-                name="studentId"
-                value={formData.studentId}
-                onChange={handleChange}
-                className="mt-1 p-2 border border-gray-300 rounded-md w-full"
-                required
-              />
-            </div>
-
-            <div className="mb-4">
-              <label htmlFor="pin" className="block text-sm font-semibold text-gray-600">PIN:</label>
-              <input
-                type="password"
-                id="pin"
-                name="pin"
-                value={formData.pin}
-                onChange={handleChange}
-                className="mt-1 p-2 border border-gray-300 rounded-md w-full"
-                required
-              />
-            </div>
-
+    //  <>
+    //  <Navbar />
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-md w-full space-y-8 p-4 bg-white rounded-lg shadow-md">
+        <h2 className="text-3xl font-extrabold text-blue-800 text-center">
+          Login
+        </h2>
+        <form className="mt-8 space-y-6" onSubmit={handleLogin}>
+          <div>
+            <label htmlFor="username" className="sr-only">
+              Username
+            </label>
+            <input
+              id="username"
+              name="username"
+              type="text"
+              autoComplete="username"
+              required
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              placeholder="Username"
+            />
+          </div>
+          <div>
+            <label htmlFor="password" className="sr-only">
+              Password
+            </label>
+            <input
+              id="password"
+              name="password"
+              type="password"
+              autoComplete="current-password"
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              placeholder="Password"
+            />
+          </div>
+          <div>
             <button
               type="submit"
-              className="bg-blue-700 text-white py-2 px-4 rounded-md w-full hover:bg-blue-600 focus:outline-none focus:border-blue-800 focus:ring focus:ring-blue-200"
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
               Login
             </button>
-          </form>
-        )}
+          </div>
+        </form>
       </div>
     </div>
+    // </>
   );
 };
 
