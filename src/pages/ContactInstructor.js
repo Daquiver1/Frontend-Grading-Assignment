8ecaafa03ab8ae32e3d65055716f9330ec5498a7
selormbb <mskbebli@st.ug.edu.gh>
2023-12-15 07:32:37 +0000

    work2

--- src/pages/ContactInstructor.js
+++ src/pages/ContactInstructor.js
@@ -6,8 +6,8 @@ const InstructorContactPage = () => {
         <main>
             <h1>Instructor Contact</h1>
             <p>Contact your instructor using the information below:</p>
-            <p><strong>Name:</strong> John Doe</p>
-            <p><strong>Email:</strong> john.doe@example.com</p>
+            <p><strong>Name:</strong> Yaya KK</p>
+            <p><strong>Email:</strong> kkrasta@gmail.com</p>
             <p><strong>Phone:</strong> 123-456-7890</p>
         </main>
     );
