5f888ead7166d59ba3663660dd131b11f32e2d43
Adipah Osei <adipahosei@gmail.com>
2023-12-15 07:40:08 +0000

    re-structured the log in page

--- student grade/src/pages/LoginPage.jsx
+++ student grade/src/pages/LoginPage.jsx
@@ -1,4 +1,5 @@
-
+import React from "react";
+import logo from '../assets/images/logo.png'
 
 const LoginPage = () => {
   return (
@@ -6,11 +7,11 @@ const LoginPage = () => {
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <img
             className="mx-auto h-10 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-            alt="Your Company"
+            src={logo}
+            alt="Grading System"
           />
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Sign in to your account
+            Login in to your account
           </h2>
         </div>
 
@@ -18,14 +19,14 @@ const LoginPage = () => {
           <form className="space-y-6" action="#" method="POST">
             <div>
               <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                Email address
+                Student ID
               </label>
               <div className="mt-2">
                 <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
+                  id="st-id"
+                  name="st-id"
+                  type="st-id"
+                  autoComplete="st-id"
                   required
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
@@ -34,21 +35,21 @@ const LoginPage = () => {
 
             <div>
               <div className="flex items-center justify-between">
-                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                  Password
+                <label htmlFor="pin" className="block text-sm font-medium leading-6 text-gray-900">
+                  PIN
                 </label>
                 <div className="text-sm">
                   <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                    Forgot password?
+                    Forgot pin?
                   </a>
                 </div>
               </div>
               <div className="mt-2">
                 <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="current-password"
+                  id="pin"
+                  name="pin"
+                  type="pin"
+                  autoComplete="pin"
                   required
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
@@ -60,7 +61,7 @@ const LoginPage = () => {
                 type="submit"
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                Sign in
+                Log in
               </button>
             </div>
           </form>
