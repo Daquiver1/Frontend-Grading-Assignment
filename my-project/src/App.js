461c52523dd5273f7a358c3e4ae5b5f9c19730eb
Afful Nana Kwasi <affulnana04@gmail.com>
2023-12-15 11:45:27 +0000

    changed to react

---
+++ my-project/src/App.js
@@ -0,0 +1,26 @@
+// import { useState } from 'react'
+import React from 'react'
+import Header from './components/header'
+import About from './components/About'
+import Faq from './components/Faq'
+import Footer from './components/Footer'
+// import HeroSection from './components/hero'
+// import './App.css'
+// import NavBar from './nav'
+
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <Header/>
+      <About/>
+      <Faq/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
