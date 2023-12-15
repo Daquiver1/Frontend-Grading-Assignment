fa62821f9dad9cef29aa625a3b7d95a4ecacda9a
Adipah Osei <adipahosei@gmail.com>
2023-12-15 10:43:34 +0000

    Changed pages layout

--- student grade/src/pages/GradeReport.jsx
+++ student grade/src/pages/GradeReport.jsx
@@ -5,29 +5,20 @@ const GradeReport = () => {
   return (
     <Layout>
         <main>
+        <hr className="col-span-2" style={{marginLeft:'30px', marginRight: '30px'}}/>
         <div className="mx-8 mt-8">
-        <div className="grid grid-cols-2 gap-4 mt-4">
-            <div className="col-span-2 mb-4 ">
-            <p className="mb-2">Student ID: 11019725</p>
-            <p className="mb-2 flex items-center justify-end">
-                <button type="button" className="bg-blue-500 text-white py-2 px-4 rounded">
-                Print Report Page
-                </button>
-            </p>
-            <form>
-                <label htmlFor="year" className="mr-2">
-                Year:
-                </label>
-                <select className="border rounded p-2">
-                <option></option>  
-                <option>Year 1</option>
-                <option>Year 2</option>
-                <option>Year 3</option>
-                <option>Year 4</option>
+        <div className="grid grid-cols-2 gap-4 mt-4" style={{marginBottom: '30px'}}>
+                <p> Student ID : 1104658</p>
+                <button type='print' className='bg-blue-500 hover:bg-blue-700 py-1 w-30 px-1 rounded transition duration-300'>Print Grade Report</button>
+                <select className="p-2 w-20">
+                    <option value="" disabled selected> Year</option>
+                    <option>Year 1</option>
+                    <option>Year 2</option>
+                    <option>Year 3</option>
+                    <option>Year 4</option>
                 </select>
-            </form>
-            <p className="mt-2">Date: 12-12-24</p>
-            </div>
+                <p className='text-right'> Date : 12-12-32</p>
+        </div>
             <hr className="col-span-2" />
             <div className="col-span-2">
             <div className="grid grid-cols-3">
@@ -35,7 +26,7 @@ const GradeReport = () => {
                 <p className="col-span-1">CGPA:3.56</p>
             </div>
             <hr />
-            <div className="grid grid-cols-4 font-semibold text-gray-800">
+            <div className="grid grid-cols-4 font-semibold gap-4 text-gray-800">
                 <h4>Course</h4>
                 <h4>IA</h4>
                 <h4>Exams</h4>
@@ -66,14 +57,14 @@ const GradeReport = () => {
                 <p>N/A</p>
             </div>
             </div>
-            <hr className="col-span-2" />
+            <hr className="col-span-2" style={{marginTop: '30px'}} />
             <div className="col-span-2">
             <div className="grid grid-cols-3">
                 <p className="col-span-2">Second Semester</p>
                 <p className="col-span-1">CGPA:3.4</p>
             </div>
-            <hr />
-            <div className="grid grid-cols-4 font-semibold text-gray-800">
+            <hr/>
+            <div className="grid grid-cols-4 font-semibold text-gray-800 gap-4">
             <h4>Course</h4>
                 <h4>IA</h4>
                 <h4>Exams</h4>
@@ -104,7 +95,6 @@ const GradeReport = () => {
                 <p>N/A</p>
             </div>
             </div>
-        </div>
         </div>
         </main>
     </Layout>
