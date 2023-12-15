06b347391f2612774d57bb7a2c618c69cc6076c4
NanaOji <akpaabe@gmail.com>
2023-12-14 17:50:38 -0800

    added a dashboard page

--- my-react/src/Components/Dashboard/Dashboard.js
+++ my-react/src/Components/Dashboard/Dashboard.js
@@ -1,7 +1,11 @@
+import Page from "./Page";
+import Sidebar from "./Sidebar";
+
 const Dashboard = () => {
     return ( 
-        <div className="">
-            dashboard page here
+        <div className="flex mt-24">
+            <Sidebar/>
+            <Page/>
         </div>
      );
 }
