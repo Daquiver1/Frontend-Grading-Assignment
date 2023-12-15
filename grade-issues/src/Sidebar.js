d1e21d3ee9fd221a178533b08549790c061bcbf2
dem0saic <changeinculture4lyf@proton.me>
2023-12-15 22:52:43 +0000

    update  sidebar icons

--- grade-issues/src/Sidebar.js
+++ grade-issues/src/Sidebar.js
@@ -1,13 +1,11 @@
 import React from "react";
-import {
-  BsCart3,
-  BsGrid1X2Fill,
-  BsFillArchiveFill,
-  BsFillGrid3X3GapFill,
-  BsPeopleFill,
-  BsListCheck,
-  BsMenuButtonWideFill,
-} from "react-icons/bs";
+import { IoHomeOutline } from "react-icons/io5";
+import { IoIosLogIn } from "react-icons/io";
+import { MdOutlineSpaceDashboard } from "react-icons/md";
+import { MdOutlineReportGmailerrorred } from "react-icons/md";
+import { SiReacthookform } from "react-icons/si";
+import { FaHandsHelping } from "react-icons/fa";
+import { MdOutlineContactMail } from "react-icons/md";
 
 function Sidebar({ openSidebarToggle, OpenSidebar }) {
   return (
@@ -16,9 +14,7 @@ function Sidebar({ openSidebarToggle, OpenSidebar }) {
       className={openSidebarToggle ? "sidebar-responsive" : ""}
     >
       <div className="sidebar-title">
-        <div className="sidebar-brand">
-          <BsCart3 className="icon_header" /> UG | DEPARTMENT OF SCIENCE
-        </div>
+        <div className="sidebar-brand">UG | DEPARTMENT OF SCIENCE</div>
         <span className="icon close_icon" onClick={OpenSidebar}>
           X
         </span>
@@ -27,37 +23,37 @@ function Sidebar({ openSidebarToggle, OpenSidebar }) {
       <ul className="sidebar-list">
         <li className="sidebar-list-item">
           <a href="./Pages/home.js">
-            <BsGrid1X2Fill className="icon" /> Home
+            <IoHomeOutline className="icon" /> Home
           </a>
         </li>
         <li className="sidebar-list-item">
           <a href="./Pages/login.js">
-            <BsFillArchiveFill className="icon" /> Login
+            <IoIosLogIn className="icon" /> Login
           </a>
         </li>
         <li className="sidebar-list-item">
           <a href="./Pages/dashboard.js">
-            <BsFillGrid3X3GapFill className="icon" /> Dashboard
+            <MdOutlineSpaceDashboard className="icon" /> Dashboard
           </a>
         </li>
         <li className="sidebar-list-item">
           <a href="./Pages/gradeReport.js">
-            <BsPeopleFill className="icon" /> Grade Report
+            <MdOutlineReportGmailerrorred className="icon" /> Grade Report
           </a>
         </li>
         <li className="sidebar-list-item">
           <a href="./Pages/missingGrade.js">
-            <BsListCheck className="icon" /> Missing Grade Form
+            <SiReacthookform className="icon" /> Missing Grade Form
           </a>
         </li>
         <li className="sidebar-list-item">
           <a href="./Pages/contact.js">
-            <BsMenuButtonWideFill className="icon" /> Instructor Contact
+            <MdOutlineContactMail className="icon" /> Instructor Contact
           </a>
         </li>
         <li className="sidebar-list-item">
           <a href="./Pages/help.js">
-            <BsMenuButtonWideFill className="icon" /> Help and Support
+            <FaHandsHelping className="icon" /> Help and Support
           </a>
         </li>
       </ul>
