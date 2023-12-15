2758a0edf85aa0cb082044b8f862fa0064cfa0f4
Mensah <mensaherskine@gmail.com>
2023-12-15 17:41:53 +0000

    Created the Homepart component folder, added a picture and imported the rest of the homepage components to be displayed on the homepage or landing page.

---
+++ src/Components/Homepart/Homepart1.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Homepart1.css'
+
+const Homepart1 = () => {
+  return (      
+        <div className="main-container">
+
+            <div className="text-container">
+              <p>
+              Welcome to the Missing Grade Reporting System, your dedicated platform for reporting 
+              and tracking missing grades. We understand the importance of accurate and timely grade 
+              information, and our system is designed to streamline the process of reporting missing grades 
+              while providing transparency about the resolution status.
+              </p>
+            </div>
+          
+        </div>  
+  )
+}
+
+export default Homepart1
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,12 +2,20 @@ import React from 'react'
 import Newsletter from '../Components/Newsletter/Newsletter'
 import Help1 from '../Components/Helphome/Help1'
 import Faq from '../Components/Faqhome/Faq'
+import Homepart2 from '../Components/Homepart/Homepart2'
+import Homepart1 from '../Components/Homepart/Homepart1'
+import Hometext1 from '../Components/Homepart/Hometext1'
+import Hometext2 from '../Components/Homepart/Hometext2'
 
 const Home = () => {
   return (
     <div>
+      <Homepart2/>
+      <Homepart1/>
       <Help1/>
+      <Hometext1/>
       <Faq/>
+      <Hometext2/>
       <Newsletter/>
     </div>
   )
