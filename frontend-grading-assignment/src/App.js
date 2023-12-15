66f36d383b3dd43eba7e58d5f691de95c5919233
Pappyjedu <148871618+Pappyjedu@users.noreply.github.com>
2023-12-15 16:28:01 +0000

    Update background color

--- frontend-grading-assignment/src/App.js
+++ frontend-grading-assignment/src/App.js
@@ -4,6 +4,7 @@ import LoginPage from './components/LoginPage';
 import GradeReportPage from './components/GradeReportPage';
 import MissingGradeFormPage from './components/MissingGradeFormPage';
 import DashboardPage from './components/DashboardPage';
+import HelpAndSupportPage from './components/HelpSupportPage';
 import './App.css';
 
 function App() {
@@ -12,8 +13,11 @@ function App() {
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
           <h1>MISSING GRADE REPORT SYSTEM</h1>
-         
-            
+          <ul>
+          <li><Link to="/dashboard-page">Dashboard</Link></li>
+          <li><Link to="/login">Login</Link></li>
+          <li><Link to="/help-and-support">HelpAndSupportPage</Link></li>
+        </ul>
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
               <li className="nav-item">
@@ -27,8 +31,10 @@ function App() {
         <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/grade-report" element={<GradeReportPage />} />
+        <Route path="/dashboard-page" element={<DashboardPage />} />
         <Route path="/missing-grade-form" element={<MissingGradeFormPage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/help-and-support" element={<HelpAndSupportPage />} />
         </Routes>
       </div>
     </Router>
