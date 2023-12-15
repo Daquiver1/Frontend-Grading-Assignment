482136d56dd5225cb29b77989ef06c68bf98ec91
Joshua Danso <jdanso2104@gmail.com>
2023-12-15 04:25:59 +0000

    add

--- my-react-project/src/App.js
+++ my-react-project/src/App.js
@@ -2,6 +2,7 @@ import "./App.css"
 import Header from "./components/common/heading/Header"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import Home from "./components/home/Home"
+import Login from "./components/login/Login"
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Header />
         <Switch>
           <Route path='/' exact component={Home} />
+          <Route path='/login' exact component={Login} />
         </Switch>
 
       </Router>
---
+++ my-react-project/src/components/login/Login.jsx
@@ -0,0 +1,32 @@
+ import React, { useState } from "react"
+
+ export const Login = () => {
+  const [email, setEmail] = useState('');
+  const [pass, setPass] = useState('');
+  const [name, setName] = useState('');
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email);
+  }
+
+
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='name'>Full name</label>
+        <input value={name} name='name' id='name' placeholder='full Name' />
+        <label for='email'>email</label>
+        <input value={email} type='email' placeholder='youremail@gmail.com' id='email' name='email'/>
+        <label for='password'>password</label>
+        <input value={email} type='password' placeholder='********' id='password' name='password'/>
+        <button type='submit'>Login</button>
+      </form>
+      <button>Don't have an account? Register here.</button>
+    </>
+  )
+}
+
+export default Login
