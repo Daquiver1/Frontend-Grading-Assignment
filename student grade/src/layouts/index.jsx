b7340d17777abf374c2a36145d9eb3844aacd0d9
Adipah Osei <adipahosei@gmail.com>
2023-12-15 05:54:24 +0000

    modified the structure of the layout

--- student grade/src/layouts/index.jsx
+++ student grade/src/layouts/index.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/footer";
 
-const Layout = () => {
+const Layout = ({children}) => {
     return(
         <div className="flex flex-col justify-between h-screen">
             <Header />
-                    <div className="flex-1 my-5 mx-auto w-[80%]"></div>
+                    <main>{children}</main>
             <Footer/>
         </div>
     )
