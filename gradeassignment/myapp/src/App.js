b9cb8d01945718dc3bfc4e0b09e7d25a995926ce
Julia-Naa <juliamensah05@gmail.com>
2023-12-15 09:19:51 +0000

    new changes

--- gradeassignment/myapp/src/App.js
+++ gradeassignment/myapp/src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
-import LandingPage from './pages/LandingPage';
-import Navbar from './Navbar';
-
+import Navbar from './Components/Navbar';
+import "./App.css"
 
 function App() {
  return(
-  <>
-  <Navbar />
-  <Router>
-    <div className='app'>
-      <Routes>
-      <Route path='/' exact>
-        <LandingPage />
-        </Route>
-      </Routes>
-    </div>
-  </Router>
-  </>
+  <React.Fragment>
+    <Navbar />
+  </React.Fragment>
  )
 }
 
---
+++ gradeassignment/myapp/src/Components/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css'
+import { Fabars, FaTimes} from "react-icons"
+ function Navbar() {
+  return (
+    <header>
+      <nav>
+        <a href='/#'>Home</a>
+        <a href='/#'>Home</a>
+        <a href='/#'>Home</a>
+        <a href='/#'>Home</a>
+        <a href='/#'>Home</a>
+        <button>
+        <FaTimes/>
+        </button>
+      </nav>
+      <button>
+        <Fabars/>
+      </button>
+    </header>
+  )
+  ;
+ 
+  </nav>
+}
+
