6729bbfd25ab96b65359ce6d92cdb9f09b7ad4db
Mensah <mensaherskine@gmail.com>
2023-12-15 14:52:59 +0000

    Created the Msgform component folder and added code. Imported it in the Missinggardeform page to be displayed .

---
+++ src/Components/Msgform/Msgform.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Msgform.css'
+
+const Msgform = () => {
+  return (
+    <div className="contact-col">
+        <h2>MISSING GRADE REPORT FORM </h2>
+      <form method="post" action="">
+        <input type="text" name="code" placeholder="Course Code" required />
+        <input type="text" name="name" placeholder="Course name" required />
+        <input type="text" name="Instructor name" placeholder="Enter name of instructor" required />
+        <input type="text" name="grade" placeholder="Enter Expected Grade" required />
+        <textarea rows="8" name="message" placeholder="Explanation field" required></textarea>
+      </form>
+        <p><button className='btns'>Submit</button></p>
+    </div>
+
+  )
+}
+
+export default Msgform
--- src/Pages/Missinggradeform.jsx
+++ src/Pages/Missinggradeform.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Msgform from '../Components/Msgform/Msgform'
 
 const Missinggradeform = () => {
   return (
     <div>
-      
+      <Msgform/>
     </div>
   )
 }
