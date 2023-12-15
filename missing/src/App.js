cafc19bdd17c6db2d801e39c204c1fe1912a827c
Nana-Kay11 <knarthur002@st.ug.edu.gh>
2023-12-15 09:05:39 +0000

    Added a new jpg file to the Assets folder

--- missing/src/App.js
+++ missing/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Navbar from "./Pages-And-Components/Navigation-Bar/navbar";
 import LoginForm from './Pages-And-Components/LoginForm/LoginForm';
-import Footer from './Pages-And-Components/Footer/Footer';
 import Home from './Pages-And-Components/Home/Home';
 import Dashboard from './Pages-And-Components/Dashboard/Dashboard';
 
