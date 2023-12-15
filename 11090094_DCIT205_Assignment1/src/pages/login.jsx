2425a5526458b3a1b65ec5af4d134645e05befc9
Emmanuella Animwaa Kumah <emmanuellaanimwaa17@icloud.com>
2023-12-15 13:51:25 +0000

    final changes

--- 11090094_DCIT205_Assignment1/src/pages/login.jsx
+++ 11090094_DCIT205_Assignment1/src/pages/login.jsx
@@ -1,41 +1,40 @@
 import React from "react";
 import FooterComponent from "../components/FooterComponent";
-import { Outlet, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
+
 export const Login = () => {
   return (
     <>
-      <div className=" w-full">
-        <div className="bg-gray-800 h-screen flex flex-col justify-center">
-          <form className="max-w-[400px]  w-full mx-auto mt-40 py-20 px-10 rounded-lg">
-            <h2 className="text-4xl text-white font-bold text-center">LOGIN</h2>
+      <div className="bg-gray-800 h-screen flex flex-col justify-center">
+        <form className="max-w-[400px] w-full mx-auto mt-40 py-20 px-10 rounded-lg">
+          <h2 className="text-4xl text-white font-bold text-center">LOGIN</h2>
 
-            <div className="flex flex-col text-gray-400 py-4 ">
-              <label>Student ID</label>
-              <input
-                className="rounded-lg bg-gray-700 mt-2 p-2 focus:border-blue-500 focus:bg-gray-800 focus:outline-none"
-                type="text"
-              />
-            </div>
+          <div className="flex flex-col text-gray-400 py-4">
+            <label htmlFor="studentID">Student ID</label>
+            <input
+              id="studentID"
+              className="rounded-lg bg-gray-700 mt-2 p-2 focus:border-blue-500 focus:bg-gray-800 focus:outline-none"
+              type="text"
+            />
+          </div>
 
-            <div className="flex flex-col text-gray-400">
-              <label>Student Pin</label>
-              <input
-                className="rounded-lg bg-gray-700 mt-2 p-2 focus:border-blue-500 focus:bg-gray-800 focus:outline-none"
-                type="password"
-              />
-            </div>
+          <div className="flex flex-col text-gray-400">
+            <label htmlFor="studentPin">Student Pin</label>
+            <input
+              id="studentPin"
+              className="rounded-lg bg-gray-700 mt-2 p-2 focus:border-blue-500 focus:bg-gray-800 focus:outline-none"
+              type="password"
+            />
+          </div>
 
-            <Link to="/dashboard">
-              <button className="w-full my-5 py-2 bg-teal-500 shadow-lg shadow-teal-500/50 hover:shadow-teal-500/40 text-white font-semibold rounded-lg">
-                Login
-              </button>
-            </Link>
-          </form>
-        </div>
+          <Link to="/dashboard">
+            <button className="w-full my-5 py-2 bg-teal-500 hover:bg-teal-600 text-white font-semibold rounded-lg focus:outline-none">
+              Login
+            </button>
+          </Link>
+        </form>
       </div>
-      <div>
-        <FooterComponent />
-      </div>
+      <FooterComponent />
     </>
   );
 };
--- 11090094_DCIT205_Assignment1/src/pages/missing grade form.jsx
+++ 11090094_DCIT205_Assignment1/src/pages/missing grade form.jsx
@@ -1,99 +1,101 @@
 import React from "react";
 import FooterComponent from "../components/FooterComponent";
+
 export const Missing_Grade_Form = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
-
     alert("Grade report submitted successfully!");
   };
 
   return (
-    <>
-      <div className="container mx-auto mt-12">
-        <form onSubmit={handleSubmit} className="max-w-md mx-auto">
-          <h2 className="text-4xl font-bold text-black text-center mb-6">
-            MISSING GRADES?
-          </h2>
-          <p>
-            <small className="text-xl">
-              <em>Fill out the form below to report missing grades</em>
-            </small>
-          </p>
-          <div className="mt-16 space-y-4 ">
-            <div>
-              <label
-                htmlFor="courses"
-                className="block text-sm font-semibold mb-2">
-                Select a Course
-              </label>
-              <select id="courses" className="w-full border p-2 rounded-lg">
-                <option>Select a course</option>
-                <option value="01">DCIT 201</option>
-                <option value="03">DCIT 203</option>
-                <option value="05">DCIT 205</option>
-                <option value="07">DCIT 207</option>
-                <option value="09">DCIT 209</option>
-                <option value="10">CBAS 210</option>
-              </select>
-            </div>
-            <div>
-              <label
-                htmlFor="instructor"
-                className="block text-sm font-semibold mb-2">
-                Select your Instructor
-              </label>
-              <select id="instructor" className="w-full border p-2 rounded-lg">
-                <option>Select your Instructor</option>
-                <option value="DB">David Berko</option>
-                <option value="CB">Charis Boateng</option>
-                <option value="SB">Seth Baidoo</option>
-                <option value="ND">Nyla Darkoo</option>
-                <option value="KA">Kofi Asare</option>
-              </select>
-            </div>
-            <div>
-              <label
-                htmlFor="expectedGrade"
-                className="block text-sm font-semibold mb-2">
-                Your Expected Grade
-              </label>
-              <select
-                id="expectedGrade"
-                className="w-full border p-2 rounded-lg">
-                <option>Your Expected Grade</option>
-                <option value="A">A</option>
-                <option value="B">B</option>
-                <option value="C">C</option>
-                <option value="D">D</option>
-                <option value="E">E</option>
-                <option value="F">F</option>
-              </select>
-            </div>
-            <div>
-              <label
-                htmlFor="explanation"
-                className="block text-sm font-semibold mb-2">
-                Explanation
-              </label>
-              <textarea
-                id="explanation"
-                rows="4"
-                className="w-full border p-2 rounded-lg"
-                placeholder="Elaborate..."></textarea>
-            </div>
-            <div>
-              <button
-                type="submit"
-                className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-teal-500 focus:outline-none focus:shadow-outline-blue active:bg-teal-500">
-                Submit
-              </button>
-            </div>
+    <div className="container mx-auto mt-12">
+      <form onSubmit={handleSubmit} className="max-w-md mx-auto">
+        <h2 className="text-4xl font-bold text-teal-500 text-center mb-6">
+          MISSING GRADES?
+        </h2>
+        <p>
+          <small className="text-xl">
+            <em>Fill out the form below to report missing grades</em>
+          </small>
+        </p>
+        <div className="mt-16 space-y-4">
+          <div>
+            <label
+              htmlFor="courses"
+              className="block text-sm font-semibold mb-2">
+              Select a Course
+            </label>
+            <select
+              id="courses"
+              className="w-full border p-2 rounded-lg focus:outline-none focus:ring focus:border-teal-500">
+              <option>Select a course</option>
+              <option value="01">DCIT 201</option>
+              <option value="03">DCIT 203</option>
+              <option value="05">DCIT 205</option>
+              <option value="07">DCIT 207</option>
+              <option value="09">DCIT 209</option>
+              <option value="10">CBAS 210</option>
+            </select>
           </div>
-        </form>
-      </div>
+          <div>
+            <label
+              htmlFor="instructor"
+              className="block text-sm font-semibold mb-2">
+              Select your Instructor
+            </label>
+            <select
+              id="instructor"
+              className="w-full border p-2 rounded-lg focus:outline-none focus:ring focus:border-teal-500">
+              <option>Select your Instructor</option>
+              <option value="DB">David Berko</option>
+              <option value="CB">Charis Boateng</option>
+              <option value="SB">Seth Baidoo</option>
+              <option value="ND">Nyla Darkoo</option>
+              <option value="KA">Kofi Asare</option>
+            </select>
+          </div>
+          <div>
+            <label
+              htmlFor="expectedGrade"
+              className="block text-sm font-semibold mb-2">
+              Your Expected Grade
+            </label>
+            <select
+              id="expectedGrade"
+              className="w-full border p-2 rounded-lg focus:outline-none focus:ring focus:border-teal-500">
+              <option>Your Expected Grade</option>
+              <option value="A">A</option>
+              <option value="B">B</option>
+              <option value="C">C</option>
+              <option value="D">D</option>
+              <option value="E">E</option>
+              <option value="F">F</option>
+            </select>
+          </div>
+          <div>
+            <label
+              htmlFor="explanation"
+              className="block text-sm font-semibold mb-2">
+              Explanation
+            </label>
+            <textarea
+              id="explanation"
+              rows="4"
+              className="w-full border p-2 rounded-lg focus:outline-none focus:ring focus:border-teal-500"
+              placeholder="Elaborate..."></textarea>
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="bg-teal-500 text-white py-2 px-4 rounded-md hover:bg-teal-600 focus:outline-none focus:shadow-outline-blue active:bg-teal-700">
+              Submit
+            </button>
+          </div>
+        </div>
+      </form>
       <div className="mt-32">
         <FooterComponent />
       </div>
-    </>
+    </div>
   );
 };
