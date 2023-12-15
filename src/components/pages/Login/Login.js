6b6003e655813d8d3e6d92ee147d5e5e5e6eb686
Klenam <cedeegbe@gmail.com>
2023-12-15 14:50:05 +0000

    login page

--- src/components/pages/Login/Login.js
+++ src/components/pages/Login/Login.js
@@ -1,11 +1,50 @@
-import React from 'react'
+import React from "react";
+import "./Login.css"
+import { Link } from "react-router-dom";
+import { IoPerson } from "react-icons/io5";
+import { MdLock } from "react-icons/md";
 
 const Login = () => {
-  return (
-    <>
-    <h1>Login</h1>
-    </>
-  )
-}
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    const username = event.target.elements.username.value;
+    const password = event.target.elements.password.value;
 
-export default Login
+    // Add your authentication logic here
+    console.log("Form submitted with:", { username, password });
+
+    // Optionally, you can clear the form fields after submission
+    event.target.reset();
+  };
+
+  return (
+      <div className="background">
+    <div className="login-container ">
+      <h2>Login</h2>
+
+      <form onSubmit={handleFormSubmit}>
+      <div class="input-box">
+        <IoPerson />
+        <input type="text" name="username" placeholder="Username" required />
+        </div>
+
+        <div class="input-box">
+          <MdLock />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          required
+        />
+        </div>
+        <Link to="/Dashboard">
+
+        <button type="submit">Login</button>
+        </Link>
+      </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- src/router/MyRoutes.js
+++ src/router/MyRoutes.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
 import Landing from '../components/pages/Landing/Landing'
 import Login from '../components/pages/Login/Login'
+import Dashboard from '../components/pages/Dashboard/Dashboard'
 
 const MyRoutes = () => {
   return (
@@ -10,6 +11,7 @@ const MyRoutes = () => {
       <Routes>
       <Route path="/" exact Component={Landing}/>
       <Route path="/login" exact Component={Login}/>
+      <Route path="/Dashboard" exact Component={Dashboard}/>
 
       </Routes>
       
