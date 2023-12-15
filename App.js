e5bb80ad347132d875c2ed69c6ef1f9fa95aab0f
AMG MONI <150767131+AmgMoni-Fx@users.noreply.github.com>
2023-12-15 21:59:25 +0000

    Add files via upload
    updated

--- App.js
+++ App.js
@@ -4,19 +4,24 @@ import Login from './Components/Login';
 import Home from './Components/Home';
 import Transcripts from './Components/Transcripts';
 import MissingGradeForms from './Components/Missing Grade Forms';
-import GradeBook from './Components/Grade Book';
+import GradeReport from './Components/Grade Report';
 import Contacts from './Components/Contacts';
+import FAQ from './Components/FAQ';
+//import Readme from './Components/Readme';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/transcripts" element={<Transcripts />} />
-        <Route path="/forms" element={<MissingGradeForms />} />
-        <Route path="/Grade Book" element={<GradeBook />} />
-        <Route path="/contacts" element={<Contacts />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Transcripts" element={<Transcripts />} />
+        <Route path="/MissingGradeForms" element={<MissingGradeForms />} />
+        <Route path="/GradeReport" element={<GradeReport />} />
+        <Route path="/ Contacts" element={< Contacts />} />
+        <Route path="/ FAQ" element={< FAQ />} />
+        
       </Routes>
     </BrowserRouter>
   );
