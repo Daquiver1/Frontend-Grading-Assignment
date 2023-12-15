0468586fbfbbc3993913169fc7d68feaef5302a4
nerdfavour <favourackonu13@gmail.com>
2023-12-15 23:51:51 +0000

    changed bg

--- grade-report-system/src/components/HeroSection.jsx
+++ grade-report-system/src/components/HeroSection.jsx
@@ -1,20 +1,36 @@
-import React from 'react'
-import image from '../assets/bg-image-2.jpg';
-import './herosection.css'
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import image1 from '../assets/bg-image-4.jpg';
+import image2 from '../assets/bg-image-2.jpg';
+import './herosection.css';
 
-function HeroSection(){
-    return(
-        <>
-        <div id = "imgHero">
-           <img src = {image} alt = "Hero HEADER image">
-            </img>    
-            </div>
-        <div id = "imageText">
-            GRADING SYSTEM
-        </div>
-        </>
-        
-    )
+const images = [image1, image2];
+
+function HeroSection() {
+  const sliderSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000, // Adjust the speed of the slideshow
+  };
+
+  return (
+    <div id="heroSection">
+      <Slider {...sliderSettings}>
+        {images.map((img, index) => (
+          <div key={index}>
+            <img src={img} alt={`Slide ${index + 1}`} />
+          </div>
+        ))}
+      </Slider>
+      <div id="imageText">GRADING SYSTEM</div>
+    </div>
+  );
 }
 
-export default HeroSection
+export default HeroSection;
