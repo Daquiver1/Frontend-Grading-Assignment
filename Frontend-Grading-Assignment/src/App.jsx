4d3427ec11233b3947b8f80a2579fe02b2408e3b
Nana Osei Akoto <dr.akotoosei@gmail.com>
2023-12-15 21:49:42 +0000

    removed vite dependencies

--- Frontend-Grading-Assignment/src/App.jsx
+++ Frontend-Grading-Assignment/src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import GradeReport from './Component/GradeReport'
+import Navigation from './Component/Navigation'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div><GradeReport /></div>
+    <div><Navigation /></div>
   )
 }
 
