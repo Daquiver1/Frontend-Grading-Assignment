f8609ecb618f1d1ac82abff3ce308527579b193b
NanaAmpomah <nanaampomaharhin@gmail.com>
2023-12-15 03:07:46 +0100

    imported the student dashboard page

--- grading-website/src/App.js
+++ grading-website/src/App.js
@@ -7,6 +7,29 @@ import HelpAndSupport from "./pages/HelpAndSupport";
 import MissingGradeFormPage from "./pages/MissingGradeFormPage";
 import InstructorContact from "./pages/InstructorContact";
 import GradeReport from "./pages/GradeReport";
+import StudentDashboard from "./pages/StudentDashboard";
+
+const gradesData = [
+  { course: "UGRC 110 Academic Writing I", grade: "A" },
+  { course: "UGRC 131-136 Understanding Human Society", grade: "C" },
+  { course: "MATH 122 Calculus I", grade: "C+" },
+  {
+    course: "STAT 112 Introduction to Statistics and Probability II",
+    grade: "A",
+  },
+  { course: "DCIT 104 Programming Fundamentals", grade: "B" },
+  {
+    course: "DCIT 102 Computer Hardware Fund. and Circuits",
+    grade: "C",
+  },
+];
+
+const missingGradesData = [
+  "UGRC 210 Academic Writing II",
+  "DCIT 201 Programming I",
+  "DCIT 203 Digital and Logic Systems DesignI",
+  "DCIT 205 Multi Media and Web Design",
+];
 
 function App() {
   return (
@@ -18,6 +41,15 @@ function App() {
         <Route path="/missing" element={<MissingGradeFormPage />} />
         <Route path="/instructors" element={<InstructorContact />} />
         <Route path="/report" element={<GradeReport />} />
+        <Route
+          path="/dashboard"
+          element={
+            <StudentDashboard
+              grades={gradesData}
+              missingGrades={missingGradesData}
+            />
+          }
+        />
       </Routes>
     </Router>
   );
