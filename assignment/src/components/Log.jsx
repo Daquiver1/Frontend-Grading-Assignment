4d6a02d7c6f1286f1d51b233d315004279998917
Nana-king-netizen <otengnanayaw354@gmail.com>
2023-12-15 04:13:34 +0000

    Authentication for homepage

--- assignment/src/components/Log.jsx
+++ assignment/src/components/Log.jsx
@@ -1,14 +1,45 @@
 import {useState} from "react"
+import {useNavigate} from "react-router-dom"
 
 const Access = (props) =>{
-    const [email,setEmail] = useState('');
+    const [id,setID] = useState('');
     const [pass,setPass] = useState('');
+    const [error,setError] = useState('');
+    const navigate = useNavigate();
+    
 
-    const handleSubmit = (e) => {
+    const simulateLogin = async() => {
+        const simulatedUserData = {
+            validID:"11038553",
+            validPassword:"Koalais54"
+        };
+
+        return new Promise((resolve,reject) => {
+            setTimeout(()=>{
+                if(id===simulatedUserData.validID && pass ===simulatedUserData.validPassword) {
+                    resolve({id});
+                } else {
+                    reject(new Error("Invalid credentials"));
+                }
+            },1000
+            )
+        }
+        );
+    };
+
+    const handleSubmit = async(e) =>{
         e.preventDefault();
-        console.log(email);
-    }
+        try{
+            const user = await simulateLogin();
+            console.log("Logged in as:",user.id);
+            setError("");
+            navigate("/")
+        } catch (error) {
+            setError("Invalid credentials");
+        }
+    };
 
+ 
 
     return(
        <>
@@ -16,11 +47,12 @@ const Access = (props) =>{
 </div>
        <div className="flex flex-col items-center py-5 h-screen bg-gradient-to-b from-blue-100 to to-blue-500">
         <h1 className="font-bold text-3xl text-blue-700 mb-10">Login Page</h1>
+        {error && <p className="text-red-600">{error}</p>}
         <form action="" onSubmit={handleSubmit}>
 
             <label className="flex flex-col  text-xl font-sans font-semibold  " htmlFor="email">Student ID</label>
-            <input className= "rounded bg-slate-200 font-extralight font-serif text-center h-[45px] w-[400px] mb-10" type="number" value={email}  onChange={(e) =>
-            setEmail(e.target.value)} placeholder="Enter your ID" id="number" name="number" required/>
+            <input className= "rounded bg-slate-200 font-extralight font-serif text-center h-[45px] w-[400px] mb-10" type="number" value={id}  onChange={(e) =>
+            setID(e.target.value)} placeholder="Enter your ID" id="number" name="number" required/>
 
             <label className="flex flex-col font-sans font-semibold text-xl" htmlFor="password">Password</label>
             <input className="rounded bg-slate-200 font-extralight font-serif text-center h-[45px] w-[400px]" type="password" value={pass} onChange={(e) =>
