1761ef0e149ba67a2d3142902355c4c88109fe63
AlbertNoble123 <kobi8583@gmail.com>
2023-12-15 22:59:01 +0000

    login feature

---
+++ src/component/LoginPage.js
@@ -0,0 +1,43 @@
+// Login.js
+
+import React, { useState } from 'react';
+import './LoginPage.css';
+
+export const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Implement your login logic here
+    console.log('Login clicked. Implement your login logic.');
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Login</h2>
+      <form>
+        <label>
+          Username:
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <button type="button" onClick={handleLogin}>
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
