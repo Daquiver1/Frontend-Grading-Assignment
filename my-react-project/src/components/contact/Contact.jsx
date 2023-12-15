b950b7a5af21fbcc386d8aacc7bbac5118e8eb39
Joshua Danso <jdanso2104@gmail.com>
2023-12-15 14:28:39 +0000

    add

--- my-react-project/src/components/contact/Contact.jsx
+++ my-react-project/src/components/contact/Contact.jsx
@@ -4,7 +4,11 @@ export const Contact = () => {
   return (
     <form>
       <h1>Contact Us <span>Here</span></h1>
-    <input type='text' name='name' id=''/>
+    <input type='text' name='name' id='' placeholder='Enter name'/>
+    <input type='email' name='email' id='' placeholder='example@gmail.com'/>
+    <input type='phone' name='phone' id='' placeholder='+233 '/>
+    <textarea name="message" id="" cols="30" rows="10" placeholder='type here...'/>
+    <button type='submit'>send</button>
     </form>
   )
 }
--- my-react-project/src/components/dashboard/dashboard.jsx
+++ my-react-project/src/components/dashboard/dashboard.jsx
@@ -21,50 +21,50 @@ function Dashboard() {
 
   return (
     <div className="dash">    
-    <div className="bg-white p-4">
+      <div className="bg-white p-4">
       
 
-      <div className="max-w-4xl mx-auto mt-6">
-        <h2 className="text-2xl font-bold mb-4">DASHBOARD</h2>
-        <div className="grid grid-cols-2 gap-4">
-          <div className="bg-gray-100 p-4 rounded">
-            <h3 className="text-lg font-semibold mb-2">Grades for Current Semester</h3>
-            <table className="w-full">
-              <thead>
-                <tr>
-                  <th className="border-b">Subject</th>
-                  <th className="border-b">Grade</th>
-                </tr>
-              </thead>
-              <tbody>
-                {grades.map((grade, index) => (
-                  <tr key={index} className="border-b">
-                    <td className="py-2">{grade.subject}</td>
-                    <td className="py-2">{grade.grade}</td>
+        <div className="max-w-4xl mx-auto mt-6">
+          <h2 className="text-2xl font-bold mb-4">DASHBOARD</h2>
+          <div className="grid grid-cols-2 gap-4">
+            <div className="bg-gray-100 p-4 rounded">
+              <h3 className="text-lg font-semibold mb-2">Grades for Current Semester</h3>
+              <table className="w-full">
+                <thead>
+                  <tr>
+                   <th className="border-b">Subject</th>
+                   <th className="border-b">Grade</th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-          <div className="bg-gray-100 p-4 rounded">
-            {missingGrades.length > 0 && (
-              <div className="mb-4">
-                <p className="font-semibold mb-2">You have missing grades for the following subjects:</p>
-                <ul>
-                  {missingGrades.map((grade, index) => (
-                    <li key={index}>{grade.subject}</li>
+                </thead>
+                <tbody>
+                 {grades.map((grade, index) => (
+                    <tr key={index} className="border-b">
+                      <td className="py-2">{grade.subject}</td>
+                      <td className="py-2">{grade.grade}</td>
+                    </tr>
                   ))}
-                </ul>
-              </div>
-            )}
+                </tbody>
+              </table>
+            </div>
+            <div className="bg-gray-100 p-4 rounded">
+             {missingGrades.length > 0 && (
+                <div className="mb-4">
+                  <p className="font-semibold mb-2">You have missing grades for the following subjects:</p>
+                  <ul>
+                    {missingGrades.map((grade, index) => (
+                      <li key={index}>{grade.subject}</li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+            </div>
           </div>
-        </div>
 
-        <div className="mt-6">
-          <button className="bg-blue-500 text-white py-2 px-4 rounded">Go to Login</button>
+          <div className="mt-6">
+            <button className="bg-blue-500 text-white py-2 px-4 rounded">Go to Login</button>
+          </div>
         </div>
       </div>
-    </div>
     </div>
 
   );
