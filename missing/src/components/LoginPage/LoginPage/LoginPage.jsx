5a4c264ac99530133a30eac0ef777207050be043
Divine <awuahasaredivine9@gmail.com>
2023-12-15 12:45:01 +0000

    updated the files

---
+++ missing/src/components/LoginPage/LoginPage/LoginPage.jsx
@@ -0,0 +1,50 @@
+// LoginPage.js
+import React, { useState } from 'react';
+import './LoginPage.css'; // Import the CSS file
+
+const LoginPage = () => {
+  const [studentId, setStudentId] = useState('');
+  const [pin, setPin] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleLogin = () => {
+    // Simulated authentication (Replace with actual authentication logic)
+    const mockStudentId = '12345';
+    const mockPin = '6789';
+
+    if (studentId === mockStudentId && pin === mockPin) {
+      alert('Login Successful!');
+    } else {
+      setErrorMessage('Invalid Student ID or PIN. Please try again.');
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Student Login</h2>
+      <div className="login-form">
+        <label htmlFor="studentId">Student ID:</label>
+        <input
+          type="text"
+          id="studentId"
+          value={studentId}
+          onChange={(e) => setStudentId(e.target.value)}
+        />
+
+        <label htmlFor="pin">PIN:</label>
+        <input
+          type="password"
+          id="pin"
+          value={pin}
+          onChange={(e) => setPin(e.target.value)}
+        />
+
+        <button onClick={handleLogin}>Login</button>
+        {errorMessage && <p className="error-message">{errorMessage}</p>}
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
+
