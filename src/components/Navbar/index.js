0ebff939d4975bb50c677a4b35f9475827a18988
KOBBY-RICCH <148541461+KOBBY-RICCH@users.noreply.github.com>
2023-12-15 17:52:15 +0000

    Add files via upload

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,54 @@
+// components/Navbar/index.js
+
+import React from "react";
+import {
+	Nav,
+	NavLink,
+	Bars,
+	NavMenu,
+	NavBtn,
+	NavBtnLink,
+} from "./NavbarElements";
+
+const Navbar = () => {
+	return (
+		<>
+			<Nav>
+				<Bars />
+
+				<NavMenu>
+				 <img className="navbar-logo" src='https://dcs.ug.edu.gh/img/comScience_logo.png' alt=" logo" s/>
+					<NavLink to="/index" activeStyle> 
+						Home
+					</NavLink>
+					<NavLink to="/report" activeStyle> 
+						Grade
+					</NavLink>
+					<NavLink to="/dashboard" activeStyle>
+						Dashboard
+					</NavLink>
+					<NavLink to="/missinggrade" activeStyle>
+						Grade Form
+					</NavLink>
+					<NavLink to="/instructor" activeStyle>
+						Contact
+					</NavLink>
+					<NavLink to="/help" activeStyle>
+						Help
+					</NavLink>
+					
+					{/* Second Nav */}
+					{/* <NavBtnLink to='/log-in'activeStyle>login</NavBtnLink> */}
+				</NavMenu>
+				<NavBtn>
+					<NavBtnLink to="/login">
+						Login
+					</NavBtnLink>
+                    
+				</NavBtn>
+			</Nav>
+		</>
+	);
+};
+
+export default Navbar;
---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+// Footer.js
+import React from 'react';
+import './footer.css';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Copyright&copy;2023, All rights reserved. Powered By Grade Reporting System</p>
+    </footer>
+  );
+};
+
+export default Footer;
