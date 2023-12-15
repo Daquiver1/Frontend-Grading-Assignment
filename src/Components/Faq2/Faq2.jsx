28ce866b4e7960b7c3bd3951e2c6bd7325c93d0b
Mensah <mensaherskine@gmail.com>
2023-12-15 06:21:42 +0000

    Created four new components for usage in the help and home pages.

---
+++ src/Components/Faq2/Faq2.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+
+const Faq2 = () => {
+  return (
+  
+    <div className="main-container1">
+      <div className="inner-container">
+        <div className="text-container">
+           <p><strong>Q:</strong> How do instructors respond to missing grade reports?</p>
+           <p><strong>A:</strong> Instructors will receive notifications of missing grade 
+            reports and are prompted to address the issue promptly. They 
+            can provide explanations, updates, or submit the missing grades
+             through the system.
+            </p>
+        </div>
+      </div>
+      <div className="inner-container">
+        <div className="text-container">
+           <p><strong>Q:</strong> How do I report a missing grade? </p>
+           <p><strong>A:</strong> To report a missing grade, log in to the system, select 
+           the relevant course, and follow the prompts to submit a missing grade request. 
+           Provide necessary details, such as course name, instructor, and any supporting documentation.
+            </p>
+        </div>
+      </div>
+      <div className="inner-container">
+        <div className="text-container">
+           <p><strong>Q:</strong>  How long does it take for missing grades to be addressed? </p>
+           <p><strong>A:</strong>  The timeframe for resolving missing grades varies, but the system is 
+           designed to expedite the process. You will receive updates on the status of your report, and 
+           efforts will be made to resolve the issue promptly.
+            </p>
+        </div>
+      </div>
+      <div className="inner-container">
+        <div className="text-container">
+           <p><strong>Q:</strong> What information should I include when submitting a missing grade report? </p>
+           <p><strong>A:</strong> When submitting a missing grade report, include details such as your full name, 
+           student ID, course information, and any relevant documentation, such as emails or assignment receipts.
+           </p>
+        </div>
+      </div>
+      <div className="inner-container">
+        <div className="text-container">
+           <p><strong>Q:</strong> How do instructors respond to missing grade reports?  </p>
+           <p><strong>A:</strong> Instructors will receive notifications of missing grade reports
+            and are prompted to address the issue promptly. They can provide explanations, updates, 
+            or submit the missing grades through the system.
+            </p>
+        </div>
+      </div>
+      <div className="inner-container">
+        <div className="text-container">
+           <p><strong>Q:</strong>  Is the Missing Grade Reporting System available 24/7? </p>
+           <p><strong>A:</strong> Yes, the Missing Grade Reporting System is accessible 24/7, 
+           allowing you to submit reports and check the status of your requests at any time.
+            </p>
+        </div>
+      </div>
+    </div>
+    
+  )
+}
+
+export default Faq2
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Newsletter from '../Components/Newsletter/Newsletter'
-
+import Help1 from '../Components/Helphome/Help1'
+import Faq from '../Components/Faqhome/Faq'
 
 const Home = () => {
   return (
     <div>
+      <Help1/>
+      <Faq/>
       <Newsletter/>
     </div>
   )
