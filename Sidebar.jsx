cfa5af448b149bc6683b1348cf0cdf2873691431
Philomina Annan- 11347739 <150846223+ESIANNAN@users.noreply.github.com>
2023-12-15 21:02:36 +0000

    sidebar

---
+++ Sidebar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Sidebar.css';
+
+const Sidebar = () => {
+  return(
+   <main>
+    <header className="header">
+        <div className="header-toggle">
+          <i class="fas fa-bars"></i>  
+        </div>
+    </header>
+  </main>
+  );
+  
+};
+
+export default Sidebar;
