835125848421dc4f0a4315cd615d55c7c6416cd3
Adipah Osei <adipahosei@gmail.com>
2023-12-15 12:10:39 +0000

    added some styling to homepage

--- student grade/src/pages/Home.jsx
+++ student grade/src/pages/Home.jsx
@@ -11,11 +11,18 @@ const HomePage = () => {
           <main>
           <div className="bg-cover bg-center  h-screen flex items-center justify-center">
       <div className="bg-purple-900 max-w-2xl p-8 rounded-lg text-white">
-        {/* Your content goes here */}
-        <h1 className="text-3xl font-bold mb-4">School Grading System</h1>
+        
+        <h1 className="text-3xl font-bold mb-4">UG School Grading System</h1>
         <p>
-          This is an example of a centered container with a light purple background using Tailwind CSS.
+         Welcome to our Grading System – a dedicated platform committed to fostering academic excellence, providing insightful assessments, and empowering students on their educational journey by transforming grades into stepping stones for continuous growth and success
         </p>
+        <h4 style={{ fontStyle: 'italic', fontSize: '1.5rem', fontWeight: 'normal', marginTop:'20px' }}>
+        Grades That Reflect Your Potential
+        </h4>
+        <div class="mt-10 flex items-center justify-center gap-x-6">
+          <a href="#" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get started</a>
+          <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Learn more <span aria-hidden="true">→</span></a>
+        </div>
       </div>
     </div>
           </main>
