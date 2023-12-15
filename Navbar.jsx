6857ff455b57a6163518dd4a2a42b7bf7b6c60cb
Philomina Annan- 11347739 <150846223+ESIANNAN@users.noreply.github.com>
2023-12-15 00:07:19 +0000

    updated navbar

--- Navbar.jsx
+++ Navbar.jsx
@@ -1,25 +1,23 @@
 import React from 'react'
 import './Navbar.css'
-import { Link } from 'react-router-dom'
 import UG_logo from '../../assets/UG-logo.png'
 
-const Navbar = () => {
 
+const Navbar = () => {
   return (
-    <div className='navbar'>
-        <img src={UG_logo}alt='' className='logo'/>
+    <div className="navbar">
+        <img src={UG_logo}  alt=''className="logo"/>
         <ul>
             <li>HOME</li>
             <li>DASHBOARD</li>
             <li>GRADE REPORT</li>
             <li>HELP & SUPPORT</li>
         </ul>
-        <div className='search-box'>
-            <input type='text' placeholder='Search'/>
+        <div className="search-box">
+            <input type="text" placeholder="Search"/>
         </div>
         
     </div>
-    
   )
 }
 
