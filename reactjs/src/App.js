aa1a1f9bbc8d82046769aeb03b6b0037607dce95
John Doe <lawdjei77@gmail.com>
2023-12-15 15:23:03 +0000

    i have added the button file

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,41 +1,19 @@
-// App.js
-
 import React from 'react';
-import { Route, Switch, NavLink, Router } from 'react-router-dom';
-import LandingPage from './components/LandingPage';
-import LoginPage from './components/LoginPage';
-import DashboardPage from './components/DashboardPage';
-import GradeReportPage from './components/GradeReportPage';
-import MissingGradeFormPage from './components/MissingGradeFormPage';
-import InstructorContactPage from './components/InstructorContactPage';
-import HelpAndSupportPage from './components/HelpAndSupportPage';
+import Navbar from './components/NavBar';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import './App.css';
 
-const App = () => {
-  return (
-    <div>
-      <nav>
-        <ul>
-          <li><NavLink to="/" exact activeClassName="active">Home</NavLink></li>
-          <li><NavLink to="/login" activeClassName="active">Login</NavLink></li>
-          <li><NavLink to="/dashboard" activeClassName="active">Dashboard</NavLink></li>
-          <li><NavLink to="/grade-report" activeClassName="active">Grade Report</NavLink></li>
-          <li><NavLink to="/missing-grade-form" activeClassName="active">Report Missing Grade</NavLink></li>
-          <li><NavLink to="/instructor-contact" activeClassName="active">Instructor Contact</NavLink></li>
-          <li><NavLink to="/help-and-support" activeClassName="active">Help and Support</NavLink></li>
-        </ul>
-      </nav>
-
-      <Router>
-        <Route path="/" exact component={LandingPage} />
-        <Route path="/login" component={LoginPage} />
-        <Route path="/dashboard" component={DashboardPage} />
-        <Route path="/grade-report" component={GradeReportPage} />
-        <Route path="/missing-grade-form" component={MissingGradeFormPage} />
-        <Route path="/instructor-contact" component={InstructorContactPage} />
-        <Route path="/help-and-support" component={HelpAndSupportPage} />
+function App (){
+  return(
+   <>
+   <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' exact/>
+      </Routes>
       </Router>
-    </div>
+   </>
   );
-};
+}
 
 export default App;
--- reactjs/src/components/NavBar.js
+++ reactjs/src/components/NavBar.js
@@ -1,18 +1,65 @@
-import React, {useState} from 'react'
-import { Link } from 'react-router-dom'
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
   return (
     <>
     <nav className='navbar'>
         <div className='navbar-container'>
             <Link to="/" className='navbar-logo'>
-                TRVL
+                TRVL<i className='fab fa-typo3'/>
             </Link>
+           <div className='menu-icon' onClick={handleClick}>
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+           </div>
+           <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className='nav-item'>
+              <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/Login' className='nav-links' onClick={closeMobileMenu}>
+                Login
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/dashboard' className='nav-links' onClick={closeMobileMenu}>
+                Dashboard
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/grade-report' className='nav-links' onClick={closeMobileMenu}>
+                Grade Report
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/missing-grade' className='nav-links' onClick={closeMobileMenu}>
+                Missing Grade?
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/instructor-contact' className='nav-links' onClick={closeMobileMenu}>
+                Instructor Contact
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/help-support' className='nav-links' onClick={closeMobileMenu}>
+                Help and Support
+              </Link>
+            </li>
+            
+           </ul>
+           {button}
         </div>
     </nav>
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
