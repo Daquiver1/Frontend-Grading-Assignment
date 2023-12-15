28d28349c6cf126e888d3b4b69c55f71ff6dc643
Adipah Osei <adipahosei@gmail.com>
2023-12-15 16:49:02 +0000

    linked the pages together

--- student grade/src/App.jsx
+++ student grade/src/App.jsx
@@ -1,16 +1,31 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Footer from "./components/footer"
 import Header from "./components/Header"
-import DashboardPage from "./pages/Dashboard"
+import Dashboard from "./pages/Dashboard"
 import GradeReport from "./pages/GradeReport"
-import HomePage from "./pages/Home"
+import Grades from "./pages/Grades1"
+import Help from "./pages/Help"
+import Home from "./pages/Home"
+import Instructor from "./pages/Instructor"
 import LoginPage from "./pages/LoginPage"
 import MissingGrade from "./pages/MissingGrade"
 
 function App() {
   return(
-    <div>
-      <HomePage />
-    </div>
+    <Router>
+      <Routes>
+        <Route index element={<Home/>}/>
+        <Route path="/home" element={<Home/>}/>
+        <Route path="/help" element={<Help/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/instructor" element={<Instructor/>}/>
+        <Route path="/grade-report" element={<GradeReport/>}/>
+        <Route path="/missing-grade" element={<MissingGrade/>}/>
+        
+
+      </Routes>
+    </Router>
   )
 }
 
