555865b6abf768bd338713f4044326d53e3ea8f4
Kelvin Lin <Kelvinlinnaeus@gmail.com>
2023-12-15 00:53:23 +0000

    Modified Missing Grade Form

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -7,7 +7,7 @@ function Index() {
   const [width, setWidth] = useState(window.innerWidth)
   function SideBar(){
     return (
-      <div className='fixed left-0 top-0 w-[300px] border-r h-[690px] bg-transparent text-white ease-in-out duration-500 pt-[60px] '>
+      <div className='fixed left-0 top-[61px] w-[300px] border-r h-[600px] bg-transparent text-white ease-in-out duration-500 border'>
         <ul className='uppercase '>
           <li className='p-4 border-b border-gray-600'>Dashboard</li>
           <li className='p-4 border-b border-gray-600'>Grade Report</li>
@@ -30,8 +30,12 @@ function Index() {
 
   return (
     <div>
-      {window.innerWidth < 1000 ? <Navbar/> : <h1 className='w-full  flex text-3xl font-bold text-[#00df94] top-2 fixed left-0 right-0  justify-end pr-[5%]'>
-          <MdCastForEducation className='mr-3 flex self-center' />Grade Report Form</h1>}
+      {window.innerWidth < 1000 ? <Navbar/> : 
+      <nav className='w-full items-center flex text-3xl font-bold text-[#00df94]  top-0 h-[60px] fixed left-0 right-0  z-20 justify-end pr-[5%] py-[20px] bg-[white] '>
+        <h1 className='flex'>
+          <MdCastForEducation className='mr-3 flex self-center' />Grade Report Form
+        </h1>
+      </nav> }
       <MissingGradeForm />
 
       {window.innerWidth > 1000 && <SideBar/>}
--- src/components/MissingGradeForm.jsx
+++ src/components/MissingGradeForm.jsx
@@ -1,93 +1,131 @@
-import {useState,useEffect} from 'react'
+import { useState, useEffect } from 'react'
+import img1 from '../assets/img1.jpg'
+import img2 from '../assets/img2.jpg'
+import img3 from '../assets/img3.jpg'
+import Typed from 'react-typed'
 
 
 function MissingGradeForm() {
-  const [formData, setFormData] = useState({
-    courseName: '',
-    instructorName: '',
-    expectedGrade: '',
-    explanationField: ''
-  })
   const [width, setWidth] = useState(window.innerWidth)
-
-  function handleChange(event){
-    const {name, value} = event.target
-    setFormData(preFormData => {
-      return {
-        ...preFormData,
-        [name]: value
-      }
-    })
-  }
-
-  function handleSubmit(event){
-    event.preventDefault()
-    console.log(formData)
-  }
 
   useEffect(() => {
     function watchWidth() {
       setWidth(window.innerWidth)
       console.log(width)
     }
-    
+
     window.addEventListener("resize", watchWidth)
   }, [])
 
-  function Above1000(){
+  function Above1000() {
+    const [formData, setFormData] = useState({
+      courseName: '',
+      instructorName: '',
+      expectedGrade: '',
+      explanationField: ''
+    })
+
+    function handleChange(event) {
+      const { name, value } = event.target
+      setFormData(preFormData => {
+        return {
+          ...preFormData,
+          [name]: value
+        }
+      })
+    }
+
+    function handleSubmit(event) {
+      event.preventDefault()
+      console.log(formData)
+    }
+
+
+    const images = [img1, img2, img3]
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const nextSlide = () => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    };
+
+    useEffect(() => {
+      const intervalId = setInterval(nextSlide, 10000); // Change slide every 3 seconds
+
+      return () => clearInterval(intervalId); // Cleanup on component unmount
+    }, [currentIndex]);
+
+
+
+
     return (
-      <div  className='flex justify-center ml-[300px] pt-10'>
-        <form action="" className='flex bg-[#2c303a] min-h-[600px] p-20 mt-[43px] pb-[10%] w-[100%] justify-center align-center flex-col pt-10' onSubmit={handleSubmit}>
-          <input 
-            type="text" 
-            placeholder='Course Name'
-            className='mb-3 self-center p-3 rounded pr-[140px]'
-            name='courseName'
-            value={formData.courseName}
-            onChange={handleChange}
+      <div className='grid gap-x-[30px] p-10 grid-cols-2 grid-rows-2 h-[600px] ml-[300px] bg-[#2c303a] border-r mt-[61px] border mr-1'>
+        <div className="relative ml-3 ">
+          <img
+            className="w-full h-[500px] object-cover rounded-xl opacity-[0.6] relative"
+            src={images[currentIndex]}
+            alt={`img ${currentIndex + 1}`}
           />
-  
-          <input 
-            type="text" 
-            placeholder='Instructor Name'
-            className='mb-3 self-center p-3 rounded pr-[140px]'
-            name='instructorName'
-            value={formData.instructorName} 
-            onChange={handleChange}
+          <Typed
+            className='text-3xl font-bold md:pl-4 pl-2 absolute top-1/2 text-amber-200'
+            strings={['If a student believes there is an error in the grading of a specific assignment or exam.', 'If an exam was taken, but the grade has not been recorded., a student may submit a missing grade report to seek information on when the grades will be available.', 'Enter Expected Grade', 'Sometimes, an instructor might not have submitted grades for a specific assignment or exam., leading to an incomplete grade for the course.', 'The missing grade report serves as a formalized communication channel between students and instructors.',]}
+            typeSpeed={80} backSpeed={20}
+            loop
           />
-  
-          <select 
-            className='mb-2 self-center py-3 pr-[130px] rounded' 
-            name="expectedGrade"
-            id="expectedGrade"
-            value={formData.expectedGrade}
-            onChange={handleChange}
-          >
-            <option value="">--Select Expected Grade--</option>
-            <option value="A">A</option>
-            <option value="B+">B+</option>
-            <option value="B">B</option>
-            <option value="C+">C+</option>
-            <option value="C">C</option>
-            <option value="D+">D+</option>
-            <option value="D">D</option>
-            <option value="E">E</option>
-            <option value="F">F</option>
-          </select>
-  
-          <textarea 
-            className='mb-4 self-center p-3 rounded pr-40 text-black' 
-            name='explanationField'
-            value={formData.explanationField}
-            placeholder='Explanation Field'
-            onChange={handleChange}
-          />
-            
-  
-          <button className='bg-white text-black self-center px-[144px] py-3 rounded'>
-            Submit
-          </button>
-        </form>
+        </div>
+
+        <div className='mr-3 text-white'>
+          <form action="" className='grid' onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder='Course Name'
+              className='mb-3 self-center p-3 rounded pr-[140px] bg-[#282c2e] placeholder-white'
+              name='courseName'
+              value={formData.courseName}
+              onChange={handleChange}
+            />
+
+            <input
+              type="text"
+              placeholder='Instructor Name'
+              className='mb-3 self-center p-3 rounded pr-[140px] bg-[#282c2e] placeholder-white'
+              name='instructorName'
+              value={formData.instructorName}
+              onChange={handleChange}
+            />
+
+            <select
+              className='mb-2 self-center py-3 pr-[130px] rounded bg-[#282c2e] '
+              name="expectedGrade"
+              id="expectedGrade"
+              value={formData.expectedGrade}
+              onChange={handleChange}
+            >
+              <option value="" >--Select Expected Grade--</option>
+              <option value="A">A</option>
+              <option value="B+">B+</option>
+              <option value="B">B</option>
+              <option value="C+">C+</option>
+              <option value="C">C</option>
+              <option value="D+">D+</option>
+              <option value="D">D</option>
+              <option value="E">E</option>
+              <option value="F">F</option>
+            </select>
+
+            <textarea
+              className='mb-4 self-center p-3 rounded pr-40 text-white max-h-[250px] h-[250px] bg-[#282c2e] placeholder-white'
+              name='explanationField'
+              value={formData.explanationField}
+              placeholder='Explanation Field'
+              onChange={handleChange}
+            />
+
+
+            <button className='bg-white text-black self-center px-[144px] py-3 rounded'>
+              Submit
+            </button>
+          </form>
+        </div>
       </div>
     )
   }
