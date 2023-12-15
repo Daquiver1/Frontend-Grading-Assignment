7f0e72a999e74395acbac6e67a491fdcb2f708d5
Don191-gif <amofahwilliam18@gmail.com>
2023-12-15 23:39:24 +0000

    updated readMe

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -5,8 +5,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './components/Landingpage';
 import LoginPage from './components/LoginPage';
 import Header from './components/Header';
-import GradeReportPage from './components/GradeReportPage'; // Import GradeReportPage
-import MissingGradeFormPage from './components/MissingGradeFormPage';
+import GradeReportPage from './components/GradeReportPage';
+import MissingGradeFormPage from './components/MissingGradeFormPage'; // Import MissingGradeFormPage
 
 const App = () => {
   return (
@@ -15,8 +15,8 @@ const App = () => {
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/header" element={<Header />} />
-        <Route path="/grade-report" element={<GradeReportPage />} /> {/* Add this line */}
-        <Route path="/missing-grade-form" element={<MissingGradeFormPage />} />
+        <Route path="/grade-report" element={<GradeReportPage />} />
+        <Route path="/missing-grade-form" element={<MissingGradeFormPage />} /> {/* Add this line */}
       </Routes>
     </Router>
   );
--- my-react-app/src/components/Header.js
+++ my-react-app/src/components/Header.js
@@ -1,40 +1,30 @@
+// Header.js
 
-
-
+import React from 'react';
 import { Link } from 'react-router-dom';
-import "../CSS/Header.css";
 
-const Navbar = () => {
-    return (
-        <div>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/dashboard">Dashboard</Link>
-                    </li>
-                    <li>
-                        <Link to="/grade-report">Grade Report</Link>
-                    </li>
-                    <li>
-                        <Link to="/missing-grade">Missing Grade Form</Link>
-                    </li>
-                    <li>
-                        <Link to="/instructor-contact">Instructor Contact</Link>
-                    </li>
-                    <li>
-                        <Link to="/help-support">Help and Support</Link>
-                    </li>
-                    <li>
-                        <Link to="/login">Login</Link>
-                    </li>
-                </ul>
-            </nav>
-
-        </div>
-    );
+const Header = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/header">Header</Link>
+        </li>
+        <li>
+          <Link to="/grade-report">Grade Report</Link>
+        </li>
+        <li>
+          <Link to="/missing-grade-form">Missing Grade Form</Link> {/* Add this line */}
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
-export default Navbar;
+export default Header;
