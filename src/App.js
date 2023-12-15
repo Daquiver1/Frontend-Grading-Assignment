fb48a35718170aba73b65e4a5e6cd5978d6d9d74
saeeda <saeedatofa896@gmail.com>
2023-12-15 05:17:12 +0000

    instructor contact

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import NotFound from './pages/404';
 import Login from './pages/login';
 import GradeReportPage from './pages/GradeReportPage';
 import MissingGradeForm from './pages/MissingGradePage';
+import InstructorContact from './pages/InstructorContactPage';
 
 
 
@@ -19,6 +20,7 @@ function App() {
             <Route path="/*" element={<NotFound />} />
             <Route path="/dashboard/missing-grade-form" element={<MissingGradeForm />}/>
             <Route path="/dashboard/grade-report" element={<GradeReportPage />}/>
+            <Route path="/dashboard/instructor-contact" element={<InstructorContact />}/>
             
          </Routes>
       </>
---
+++ src/pages/InstructorContactPage/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import DashboardLayout from '../../components/shared/DashboardLayout'
+
+const InstructorContact = () => {
+  return (
+    <DashboardLayout title={"Dashboard / InstructorContact"}>
+    <p>Design me !</p>
+  </DashboardLayout>
+  )
+}
+
+export default InstructorContact
