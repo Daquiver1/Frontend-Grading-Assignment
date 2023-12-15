702eee0c70330c4e2810ce737c110505de7def8a
Ewurafua Quansah <ewurafuaquansah02@gmail.com>
2023-12-15 15:38:08 +0000

    changes

--- src/components/Docs/Grade-Form.js
+++ src/components/Docs/Grade-Form.js
@@ -45,7 +45,7 @@ export default function Dashboard(){
 
             <div className="sm:col-span-3">
               <label htmlFor="instructor" className="block text-sm font-medium leading-6 text-gray-900">
-              Instructor name
+              Instructor Name
               </label>
               <div className="">
               <select
@@ -55,9 +55,9 @@ export default function Dashboard(){
                   className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                   placeholder="Select Instructor"
                 >
-                  <option>Instructor one</option>
-                  <option>Instructor two</option>
-                  <option>Instructor three</option>
+                  <option>Instructor One</option>
+                  <option>Instructor Two</option>
+                  <option>Instructor Three</option>
                   <option>Instructor Four</option>
                   <option>Instructor Five</option>
                 </select>
