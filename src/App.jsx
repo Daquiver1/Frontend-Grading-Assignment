99542be6168f395fb2237070827a3edc3cc0f9ff
AlbertNoble123 <kobi8583@gmail.com>
2023-12-15 23:02:12 +0000

    Update App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,71 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import ' /LandingPage'
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import LoginPage from './component/LoginPage';
+import Dashboard from './component/Dashboard';
+import Navbar from './Navbar';
+import LandingPage from './component/LandingPage';
+import GradeReportPage from './component/GradeReportPage';
+import MissingGradeFormPage from './component/MissingGradeFormPage';
+import InstructorContactPage from './component/InstructorContactPage';
+import HelpAndSupportPage from './Component/HelpAndSupportPage';
 
-function App() {
-  const [count, setCount] = useState(0)
 
+
+const App = () => {
   return (
-    <>
-      
-        
-    </>
-  )
-}
+  
+    <Router>
+    <div>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={LandingPage} />
+        <Route path="/login" component={LoginPage} />
+        <Route path="/dashboard" component={DashboardPage} />
+        <Route path="/grade-report" component={GradeReportPage} />
+        <Route path="/missing-grade-form" component={MissingGradeFormPage} />
+        <Route path="/instructor-contact" component={InstructorContactPage} />
+        <Route path="/help-and-support" component={HelpAndSupportPage} />
+      </Switch>
+    </div>
+  </Router>
+   
+  );
+};
 
-export default App
+export default App;
+ // <div className="app-container">
+     // <LoginPage />
+    //  <Dashboard/>
+     // <Navbar/>
+      
+    //</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
