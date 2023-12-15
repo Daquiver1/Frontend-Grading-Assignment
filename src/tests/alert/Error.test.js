eae3308dd605d244f852f83f1486f5b5442af2d5
Mickeyb12 <michelbbiney12@gmail.com>
2023-12-15 16:54:11 -0500

    feat:adding test code

---
+++ src/tests/alert/Error.test.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { ReactDOM } from "react-dom";
+import Error from "../../components/alert/Error.js";
+
+import "jest-localstorage-mock";
+import renderer from "react-test-renderer";
+
+describe("Rendering Error alert", () => {
+  it("Renders Error without crashing", () => {
+    const div = document.createElement("div");
+
+    ReactDOM.render(<Error />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+});
+
+describe("Rendering Error alert with a message", () => {
+  it("Displays the correct message", () => {
+    const component = renderer.create(<Error message="This is a test"></Error>);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
---
+++ src/tests/alert/Success.test.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Success from "../../components/alert/Success.js";
+
+import "jest-localstorage-mock";
+import renderer from "react-test-renderer";
+
+describe("Rendering Success alert", () => {
+  it("Renders Success without crashing", () => {
+    const div = document.createElement("div");
+
+    ReactDOM.render(<Success />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+});
+
+describe("Rendering Unavailable alert with message", () => {
+  it("Displays the correct message", () => {
+    const component = renderer.create(
+      <Success message="This is a test"></Success>
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
