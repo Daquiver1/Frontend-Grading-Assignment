03c256b6c5f1f1329a2c7a977e3ca55d84b7b33d
elormApaloo <apalooelorm7@gmail.com>
2023-12-15 14:19:47 +0000

    Instructor Contact FAQ Contact Form

---
+++ gradesite/src/components/InstructorContactPage.js
@@ -0,0 +1,105 @@
+// src/components/InstructorContact.js
+import React, { useState } from 'react';
+import './InstructorContactPage.css'
+import Footer from './Footer';
+import Header from './Header';
+
+const instructorsData = [
+  { id: 1, name: 'Mr. Soli', email: 'mr.soli@gmail.com' },
+  { id: 2, name: 'Prof. Aziz', email: 'prof.aziz@gmail.com' },
+  { id: 3, name: 'Dr. Brown', email: 'dr.brown@yahoo.com' },
+  { id: 4, name: 'Prof. Mensah', email: 'prof.mensah@gmail.com' },
+  { id: 5, name: 'Dr. White', email: 'dr.white@yahoo.com' },
+  { id: 6, name: 'Prof. Mooxa', email: 'prof.mooxa@yahoo.com' },
+  { id: 7, name: 'Dr. Agyei', email: 'dr.agyei@yahoo.com' },
+  { id: 8, name: 'Prof. Danquah', email: 'prof.danquah@gmail.com' },
+  { id: 9, name: 'Dr. Ahenkorah', email: 'dr.ahenkorah@yahoo.com' },
+  { id: 10, name: 'Prof. Daniels', email: 'prof.daniels@yahoo.com' }
+];
+
+const InstructorContactPage = () => {
+  const [selectedInstructor, setSelectedInstructor] = useState(null);
+  const [emailSubject, setEmailSubject] = useState('');
+  const [emailBody, setEmailBody] = useState('');
+
+  const handleInstructorSelect = (instructor) => {
+    setSelectedInstructor(instructor);
+  };
+
+  const handleEmailSend = () => {
+    console.log(`Email sent to ${selectedInstructor.name}`);
+    console.log('Subject:', emailSubject);
+    console.log('Body:', emailBody);
+
+    setSelectedInstructor(null);
+    setEmailSubject('');
+    setEmailBody('');
+  };
+
+  return (
+    <div>
+        <Header/>
+        <div className='page-body'>
+            <div className="welcome">
+                Instructor Contact
+            </div>
+            <section>
+                <form className="instructor-contact">
+                    <div className='instructor-contact-info'>
+                        <h2>Instructors</h2>
+                        <ul>
+                        {instructorsData.map((instructor) => (
+                            <li key={instructor.id} onClick={() => handleInstructorSelect(instructor)}>
+                            {instructor.name} - {instructor.email}
+                            </li>
+                        ))}
+                        </ul>
+                    </div>
+                </form>
+
+                {selectedInstructor && (
+                    <div>
+                        <form className='contact'>
+                            <div className='subject-textbox'>
+                                <h2>Contact {selectedInstructor.name} By Email</h2>
+                                <label htmlFor="subject">Subject:</label>
+                                <input
+                                className='textbox'
+                                placeholder='Enter Subject'
+                                type="text"
+                                id="subject"
+                                value={emailSubject}
+                                onChange={(e) => setEmailSubject(e.target.value)}
+                                required
+                                />
+                            </div>
+                            
+                            <div className='body-textbox'>
+                                <label htmlFor="body">Body:</label>
+                                <textarea
+                                className='textarea'
+                                placeholder='Enter Subject'
+                                id="body"
+                                value={emailBody}
+                                onChange={(e) => setEmailBody(e.target.value)}
+                                required
+                                />
+                            </div>
+
+                            <div className='submit-button-area'>
+                                <button className="submit-button"type="button" onClick={handleEmailSend}>
+                                Send Email
+                                </button>
+                            </div>
+                            
+                        </form>
+                    </div>
+                )}
+            </section>    
+        </div>
+        <Footer/>
+    </div>
+  );
+};
+
+export default InstructorContactPage;
--- gradesite/src/index.js
+++ gradesite/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './components/MissingGradeForm';
+import App from './components/HelpAndSupport';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
