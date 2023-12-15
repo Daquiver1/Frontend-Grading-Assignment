62715cd6c97d0a6e0dc72dbfef3fb602ae2ba6c3
dem0saic <changeinculture4lyf@proton.me>
2023-12-15 23:19:33 +0000

    update  sidebar icons

--- grade-issues/src/Home.js
+++ grade-issues/src/Home.js
@@ -1,159 +1,32 @@
 import React from "react";
-import {
-  BsFillArchiveFill,
-  BsFillGrid3X3GapFill,
-  BsPeopleFill,
-  BsFillBellFill,
-} from "react-icons/bs";
-import {
-  BarChart,
-  Bar,
-  Cell,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend,
-  ResponsiveContainer,
-  LineChart,
-  Line,
-} from "recharts";
+import Header from "Header.js";
+import Footer from "./Footer.jsx";
 
-function Home() {
-  const data = [
-    {
-      name: "Page A",
-      uv: 4000,
-      pv: 2400,
-      amt: 2400,
-    },
-    {
-      name: "Page B",
-      uv: 3000,
-      pv: 1398,
-      amt: 2210,
-    },
-    {
-      name: "Page C",
-      uv: 2000,
-      pv: 9800,
-      amt: 2290,
-    },
-    {
-      name: "Page D",
-      uv: 2780,
-      pv: 3908,
-      amt: 2000,
-    },
-    {
-      name: "Page E",
-      uv: 1890,
-      pv: 4800,
-      amt: 2181,
-    },
-    {
-      name: "Page F",
-      uv: 2390,
-      pv: 3800,
-      amt: 2500,
-    },
-    {
-      name: "Page G",
-      uv: 3490,
-      pv: 4300,
-      amt: 2100,
-    },
-  ];
-
+const Home = () => {
   return (
-    <main className="main-container">
-      <div className="main-title">
-        <h3>DASHBOARD</h3>
+    <>
+      <Header />
+      <div className="container-class min-h-screen">
+        <p className="welcome-text text-4xl text-dark ml-[50%] translate-x-[-50%] file:font-bold italic">
+          Welcome To Our Home Page
+        </p>
+        <p className="text-dark max-w-fit ml-[50%] translate-x-[-50%] mt-5 text-2xl">
+          Welcome to the University of Ghana's Academic Affairs Department,
+          where we prioritize excellence in handling student results. Committed
+          to fostering a conducive learning environment, our dedicated team
+          ensures accurate and timely processing of academic outcomes. We employ
+          rigorous quality control measures to maintain the integrity of
+          assessments, providing students with reliable and transparent
+          feedback. Our goal is to support your academic journey, offering
+          assistance and guidance when needed. At the University of Ghana, we
+          take pride in upholding academic standards and promoting a culture of
+          continuous improvement, empowering students to thrive in their
+          educational pursuits.
+        </p>
+        <Footer></Footer>
       </div>
-
-      <div className="main-cards">
-        <div className="card">
-          <div className="card-inner">
-            <h3>PRODUCTS</h3>
-            <BsFillArchiveFill className="card_icon" />
-          </div>
-          <h1>300</h1>
-        </div>
-        <div className="card">
-          <div className="card-inner">
-            <h3>CATEGORIES</h3>
-            <BsFillGrid3X3GapFill className="card_icon" />
-          </div>
-          <h1>12</h1>
-        </div>
-        <div className="card">
-          <div className="card-inner">
-            <h3>CUSTOMERS</h3>
-            <BsPeopleFill className="card_icon" />
-          </div>
-          <h1>33</h1>
-        </div>
-        <div className="card">
-          <div className="card-inner">
-            <h3>ALERTS</h3>
-            <BsFillBellFill className="card_icon" />
-          </div>
-          <h1>42</h1>
-        </div>
-      </div>
-
-      <div className="charts">
-        <ResponsiveContainer width="100%" height="100%">
-          <BarChart
-            width={500}
-            height={300}
-            data={data}
-            margin={{
-              top: 5,
-              right: 30,
-              left: 20,
-              bottom: 5,
-            }}
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Bar dataKey="pv" fill="#8884d8" />
-            <Bar dataKey="uv" fill="#82ca9d" />
-          </BarChart>
-        </ResponsiveContainer>
-
-        <ResponsiveContainer width="100%" height="100%">
-          <LineChart
-            width={500}
-            height={300}
-            data={data}
-            margin={{
-              top: 5,
-              right: 30,
-              left: 20,
-              bottom: 5,
-            }}
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Line
-              type="monotone"
-              dataKey="pv"
-              stroke="#8884d8"
-              activeDot={{ r: 8 }}
-            />
-            <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-          </LineChart>
-        </ResponsiveContainer>
-      </div>
-    </main>
+    </>
   );
-}
+};
 
 export default Home;
