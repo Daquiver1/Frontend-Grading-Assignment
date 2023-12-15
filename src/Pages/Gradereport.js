76c97b49c1b466fb7103c1b1d94310fbf4ff6703
muizbello <bellomuiz78+1@gmail.com>
2023-12-15 22:05:21 +0000

    Grade report page

--- src/Pages/Gradereport.js
+++ src/Pages/Gradereport.js
@@ -3,19 +3,17 @@ import LandingFooter from "../Components/pagefooter";
 import LandingHeader from "../Components/pageheader";
 
 function Gradereport(){
-    return (
+    return(
         <div>
-                 <LandingHeader/>
+            <LandingHeader/>
             <div>
-                <p className="text-[23pt] text-[#191970] font-bold">Student Grade Report</p>
-                
+
             </div>
-            
-            <div className="mb-[-100px] ml-[15px]">
-            <LandingFooter/>
+            <div>
+                <LandingFooter/>
             </div>
         </div>
+     
     )
 }
-
 export default Gradereport;
