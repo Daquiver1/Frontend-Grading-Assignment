e8d27f8407f23651a644ab747794111c9e69e109
emkay3251 <jaymaxwell456@gmail.com>
2023-12-15 17:50:28 +0000

    midway through assignment

--- my-react-app/src/component/LandingPage.js
+++ my-react-app/src/component/LandingPage.js
@@ -1,20 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import NavBar from "./NavBar";
+import Footer from "./Footer";
 import "./LandingPage.css";
 
 const LandingPage = () => {
   return (
     <div className="landing-page-container">
+      <NavBar />
       <div className="landing-page-content">
         <h1 className="landing-page-title">Welcome to the Grade System</h1>
         <p className="landing-page-description">
           Your tool for tracking and reporting missing grades.
         </p>
         <Link to="/login">
-          {/* Corrected the path */}
-          <button className="landing-page-button">Get Started</button>
+          <button className="landing-page-button">Login To Start</button>
         </Link>
       </div>
+      <Footer />
     </div>
   );
 };
---
+++ my-react-app/src/component/NavBar.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./NavBar.css";
+
+const NavBar = () => {
+  return (
+    <header className="landing-page-header">
+      <div className="logo">
+        <p>GradeZone</p>
+      </div>
+      <nav className="navbar">
+        <Link to="/grade-report" className="nav-link">
+          Grade Report
+        </Link>
+        <Link to="/missing-grade-form" className="nav-link">
+          Missing Grade Form
+        </Link>
+        <Link to="/instructor-contact" className="nav-link">
+          Instructor Contact
+        </Link>
+        <Link to="/help-and-support" className="nav-link">
+          Help and Support
+        </Link>
+      </nav>
+    </header>
+  );
+};
+
+export default NavBar;
