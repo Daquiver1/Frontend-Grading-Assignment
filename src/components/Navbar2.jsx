da4e4835975684ee0affddf3c586a4060605fbed
Kelty Tech <Kelvinlinnaeus@gmail.com>
2024-01-07 08:22:24 +0000

    Working Alright

--- src/components/Navbar2.jsx
+++ src/components/Navbar2.jsx
@@ -4,20 +4,19 @@ import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai'
 import { MdCastForEducation } from "react-icons/md";
 
 
-function Navbar() {
+export default function Navbar2() {
   return (
     <div className='text-white flex justify-between items-center h-24 max-w-[1240px] px-4 mx-auto'>
-      <HandleTitle />
       <ul className='hidden md:flex'>
         <li className='p-4'>Home</li>
         <li className='p-4'>Dashboard</li>
         <li className='p-4 whitespace-nowrap'>Grade Report</li>
         <a href="./MissingGradeForm.jsx"><li className='p-4 whitespace-nowrap'>Missing Grade Form</li></a>
         <li className='p-4'>About</li>
-        <li className='p-4'>{width}</li>
+        {/* <li className='p-4'>{width}</li> */}
       </ul>
     </div>
   )
 }
 
-export default Navbar
+ 
