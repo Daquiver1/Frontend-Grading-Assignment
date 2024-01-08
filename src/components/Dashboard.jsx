ac9f8d59fab8fa254c95a215352a9cc45fcee96e
Kelty Tech <Kelvinlinnaeus@gmail.com>
2024-01-08 00:44:31 +0000

    Lin

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,6 +2,7 @@ import SideBar from "./Sidebar"
 import Chart from './Chart'
 import { useState, useEffect } from "react"
 import Gpa from "./GpaForDashboard"
+import { PerformanceForDashboard } from "./PerformanceForDashboard"
 
 function Init(){
   const [width, setWidth] = useState(window.innerWidth)
@@ -16,28 +17,7 @@ function Init(){
 
   return (
     <div>
-      <div className={' relative  mt-4 justify-center ' + (width > 1199 ? " flex space-x-3 mx-4": "space-y-4 mx-4") }>
-        <div className={"border p-3 " + (width < 1199 ? "flex justify-center" : "")}>
-          {width}
-          <Chart 
-            sem={"First Semester"}
-            subjects= {["" ,'MATH 123', 'DCIT103', 'DCIT102', 'MATH122', 'UGRC150',]}
-            marks={[ 80, 90, 87, 75, 90, 100]}
-            borderCol={'rgb(53, 162, 235)'}
-            backgroundCol={'rgba(53, 162, 235, 0.5)'}
-          /> 
-        </div> 
-
-        <div className={"border p-3 " + (width < 1199 ? "flex justify-center" : "")}>
-          <Chart 
-            sem={"Second Semester"}
-            subjects= {["",'MATH126', 'MATH122', 'DCIT104', 'DCIT102', 'UGRC150']}
-            marks={[ 70, 70, 97, 75, 80, 75]}
-            borderCol={'rgb(255, 99, 132)'}
-            backgroundCol={'rgba(255, 99, 132, 0.5)'}
-          />  
-        </div>   
-      </div>
+      <PerformanceForDashboard />
 
       <div className={"border p-3 " + (width < 1199 ? "flex justify-center" : "")}>
         <Gpa />
